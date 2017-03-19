c30a5b094320bb360c557ee1cb6dfa31db7ea4c9
Joachim Breitner <mail@joachim-breitner.de>
2017-03-19 15:57:41 -0400

    AST to Haskell: Use more native types

--- src/ast/Extract_Haskell_Ollvm_ast.v
+++ src/ast/Extract_Haskell_Ollvm_ast.v
@@ -3,7 +3,11 @@ Extraction Language Haskell.
 
 Require Import Vellvm.Ollvm_ast.
 
+Extract Inductive option => "Prelude.Maybe" [ "Nothing" "Just" ].
+Extract Inductive list => "[]" [ "[]" "(:)" ].
+Extract Inductive prod => "(,)" [ "(,)" ].
 Extract Inductive string => "Prelude.String" [ "[]" "(:)" ].
+Extract Inductive bool => "Prelude.Bool" [ "False" "True" ].
 Extract Inlined Constant int => "Prelude.Int".
 Extract Inlined Constant float => "Prelude.Float".
 
