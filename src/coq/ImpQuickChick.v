68f7670ceaf6003cfd3465da8b0f1685dc1a0da6
Nicolas <chkoh@seas.upenn.edu>
2017-05-27 01:23:19 +0800

    ImpQuickChick

---
+++ src/coq/ImpQuickChick.v
@@ -0,0 +1,8 @@
+Require Import Vellvm.Maps Vellvm.Imp.
+
+Require Import QuickChick.QuickChick.
+Import QcDefaultNotation. Open Scope qc_scope.
+Require Import List ZArith.
+Import ListNotations.
+Require Import String.
+
