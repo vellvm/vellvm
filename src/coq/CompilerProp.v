ca44d5ebca28f20e401bb97f052a4f63081a5ba6
Nicolas <chkoh@seas.upenn.edu>
2017-06-06 05:48:39 +0800

    Refactored QuickChick parts

--- src/coq/CompilerProp.v
+++ src/coq/CompilerProp.v
@@ -10,18 +10,17 @@
 
 Require Import Vellvm.Maps Vellvm.Imp.
 
-Require Import QuickChick.QuickChick. 
-Import QcDefaultNotation. Open Scope qc_scope. 
-
+Require Import QuickChick.QuickChick.
+Import QcDefaultNotation. Open Scope qc_scope.
 Require Import List ZArith.
 Import ListNotations.
 Require Import String.
-
 
 Require Import ZArith String Omega List Equalities MSets.
 
 (* Vellvm dependencies *)
 Require Import Vellvm.Ollvm_ast Vellvm.Compiler Vellvm.AstLib Vellvm.CFG Vellvm.StepSemantics Vellvm.Memory.
+Require Import Vellvm.Classes.
 
 (* Logical Foundations dependencies *)
 Require Import Vellvm.Imp Vellvm.Maps Vellvm.ImpCEvalFun.
@@ -61,9 +60,29 @@ Require Import List.
 
 Definition state_to_string (st : state) (fv : list id) : string :=
   fold_left (fun acc x => (match x with
-                        | Id ident => (ident ++ ": " ++ show (st x) ++ ", ")%string
+                        | Id ident => (ident ++ ": " ++ show_nat (st x) ++ ", ")%string
                         end)) fv "".
 
+Check fold_left.
+Check show_int.
+
+Definition mem_to_string (mem : mtype) : string :=
+  fold_left (fun s cell =>
+               match cell with
+               | DV (VALUE_Integer z) => (s ++ (string_of z) ++ "; ")%string
+               | _ => (s ++ "~VALUE_Integer; ")%string
+               end)
+            mem "".
+
+Definition elt_to_string (elem : IDSet.elt) :=
+  match elem with
+  | Id name => name
+  end.
+
+Definition elems_to_string (elements : list IDSet.elt) : string :=
+  fold_left (fun s elem => (s ++ ", " ++ (elt_to_string elem)))%string
+            elements
+            "".
 
 Definition imp_compiler_correct_aux (p:Imp.com) :=
   let fvs := IDSet.elements (fv p) in
@@ -78,44 +97,31 @@ Definition imp_compiler_correct_aux (p:Imp.com) :=
       | inl e => whenFail "init failed" false
       | inr initial_state =>
         let semantics := sem mcfg initial_state in
-        let llvm_final_state := MemDFin [] semantics 10 in
-        let imp_state := ceval_step empty_state p 10 in
+        let llvm_final_state := MemDFin [] semantics 1000 in
+        let imp_state := ceval_step empty_state p 10000 in
         match (llvm_final_state, imp_state) with
         | (None, None) => whenFail "both out of gas" true
         | (Some llvm_st, None) => whenFail "imp out of gas" false
         | (None, Some imp_st) => whenFail "llvm out of gas" false
         | (Some llvm_st, Some imp_st) => 
           let ans_state := List.map (fun x => dvalue_of_nat (imp_st x)) fvs in
-          checker (imp_memory_eqb llvm_st ans_state)
+          whenFail ("not equal: llvm: "
+                      ++ (mem_to_string llvm_st)
+                      ++ "; imp: "
+                      ++ (mem_to_string ans_state)
+                      ++ "; free vars: "
+                      ++ (elems_to_string fvs))
+                   (checker (imp_memory_eqb llvm_st ans_state))
         end        
       end
-    end
-  end.
-
-
-Definition another_imp_compiler_correct (p:Imp.com) : string * bool :=
-  let fvs := IDSet.elements (fv p) in
-  match compile p with
-  | inl e => ("Compilation failed", false)
-  | inr ll_prog =>
-    let m := modul_of_toplevel_entities ll_prog in
-    match mcfg_of_modul m with
-    | None => ("Compilation failed", false)
-    | Some mcfg =>
-      match init_state mcfg "imp_command" with
-      | inl e => ("init failed", false)
-      | inr initial_state =>
-        let semantics := sem mcfg initial_state in
-        ("Success", true)
-      end        
     end
   end.
 
 Example prog1 := W ::= AId W.
 Example prog2 := X ::= APlus (AId W) (AId W).
 
-
 Eval vm_compute in (compile prog2).
-Eval vm_compute in (another_imp_compiler_correct prog1).
+
+
 
 
