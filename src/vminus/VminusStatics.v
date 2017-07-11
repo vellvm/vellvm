2afe2436c636a9197641a4a906abfca4387b8334
Nicolas <chkoh@seas.upenn.edu>
2017-07-11 20:45:41 +0800

    Refactored out Vminus statics

--- src/vminus/VminusStatics.v
+++ src/vminus/VminusStatics.v
@@ -1,7 +1,6 @@
 Require Import Arith.
 Require Import List.
 Import ListNotations.
-Require Import String.
 
 Require Import Vminus.Classes.
 Require Import Vminus.Util.
@@ -10,283 +9,11 @@ Require Import Vminus.Vminus.
 Require Import Vminus.CFG.
 
 Require Import Vminus.Dom.
+Require Import Vminus.VminusOpSem.
 
-(** ** Vminus Operational Semantics *)
-
-Module Make (Cfg:CFG).
+Module MakeStatics (Cfg: CFG).
 Import Cfg.
 
-Module Opsem.
-  (** *** The state of a Vminus program: *)
-
-  Module Locals := Make_Env(Uid).
-  Module Memory := Make_Env(Addr).
-  Notation loc := (Locals.t (option nat)).
-  Notation mem := (Memory.t nat).
-  Notation update := Locals.update.
-
-  Record state := mkst { st_mem  : mem
-                       ; st_pc   : pc
-                       ; st_loc  : loc
-                       ; st_ppc  : pc     (* predecessor pc *)
-                       ; st_ploc : loc    (* predecessor locals *) 
-                       }.
-
-  (** *** Evaluating values. *)
-
-  (** Simply look up the uid in the locals state. (May fail!) *)
-  
-  Definition eval_val (loc:loc) (v:val) : option nat :=
-    match v with
-      | val_nat n => Some n
-      | val_uid i => loc i
-    end.
-
-  (** *** Evaluating binary operations. *)
-
-  Definition b2n (b:bool) := if b then 1 else 0.
-  Definition n2b (n:nat) := if n then false else true.
-
-  Definition bop_denote (bop:bop) : nat -> nat -> nat :=
-    match bop with
-      | bop_add => plus
-      | bop_sub => minus
-      | bop_mul => mult
-      | bop_eq => fun n m => b2n (beq_nat n m)
-      | bop_le => fun n m => b2n (leb n m)
-      | bop_and => fun n m => b2n (n2b n && n2b m)
-    end.
-
-  Definition eval_bop (loc:loc) (bop:bop) (v1 v2:val) : option nat :=
-    match eval_val loc v1, eval_val loc v2 with
-      | Some n1, Some n2 => Some (bop_denote bop n1 n2) 
-      | _, _ => None
-    end.
-
-  (** *** Phi nodes *)
-  (** Look up the predecessor label in the phi arg list. *)
-
-  Definition eval_phi (loc:loc) (l:lbl) (pas:list phiarg) 
-    : option nat :=
-    match assoc Lbl.eq_dec l pas with
-      | Some v => eval_val loc v
-      | None => None
-    end.
-
-  (** *** Control transfers. *)
-  
-  (** Given the current locals and the terminator, determine
-      the label of the next block to jump to. *)
-
-  Definition eval_tmn (loc:loc) (t:tmn) : option lbl :=
-    match t with
-      | tmn_jmp l => Some l
-      | tmn_cbr v l1 l2 =>
-          match eval_val loc v with
-            | Some n => Some (if n then l2 else l1)
-            | None => None
-          end
-      | _ => None
-    end.
-
-  (** *** Small-step, relational operational semantics *)
-
-  Inductive step (g:Cfg.t) : state -> state -> Prop :=
-
-  | step_bop : forall mem pc loc bop v1 v2 uid n ppc ploc,
-      insn_at_pc g pc (uid, cmd_bop bop v1 v2) ->
-      Some n = eval_bop loc bop v1 v2 ->
-      step g (mkst mem pc loc ppc ploc) 
-             (mkst mem (incr_pc pc) 
-                       (update loc uid (Some n)) ppc ploc)
-
-  (** Evaluate the right-hand side in the predecessor's 
-     local environment. [lbl_of ppc] is the control-flow 
-     edge that we jumped from. *)
-  | step_phi : forall mem pc loc pas uid n ppc ploc,
-      insn_at_pc g pc (uid, cmd_phi pas) ->
-      Some n = eval_phi ploc (lbl_of ppc) pas ->
-      step g (mkst mem pc loc ppc ploc)
-             (mkst mem (incr_pc pc) 
-                       (update loc uid (Some n)) ppc ploc)
-
-  (** Find the successor block (if any), update the pc. 
-      Record the current pc and locals as the "predecessor" state. *)
-  | step_tmn : forall mem pc l' loc tmn uid ppc ploc, 
-      insn_at_pc g pc (uid, cmd_tmn tmn) ->
-      Some l' = eval_tmn loc tmn ->
-      step g (mkst mem pc loc ppc ploc)
-             (mkst mem (block_entry l') loc pc loc)
-
-  (** Loads and stores **)
-  | step_load : forall mem pc loc uid addr ppc ploc,
-      insn_at_pc g pc (uid, cmd_load addr) ->
-      step g (mkst mem pc loc ppc ploc)
-             (mkst mem (incr_pc pc) 
-                       (update loc uid (Some (mem addr))) ppc ploc)
-
-  | step_store : forall mem pc loc uid addr v n ppc ploc,
-      insn_at_pc g pc (uid, cmd_store addr v) ->
-      Some n = eval_val loc v ->
-      step g (mkst mem pc loc ppc ploc)
-             (mkst (Memory.update mem addr n) 
-                   (incr_pc pc) loc ppc ploc).
-  
- (** Definition of the initial state. *)
-
-  Definition init_state (g:Cfg.t) (m: mem) : state :=
-    (mkst m
-          (block_entry (entry_block g))
-          (Locals.empty None)
-          (block_entry (entry_block g))
-          (Locals.empty None)).
-
-
-  (** *** Small-step, executable operational semantics *)
-
-  Fixpoint eval_step (g : Cfg.t) (s : state) : err state :=
-    let 'mkst mem pc loc ppc ploc := s in
-    match (Cfg.fetch g pc) with
-    | Some (uid, cmd_bop bop v1 v2) =>    
-      let result := eval_bop loc bop v1 v2 in
-      match result with
-      | Some n => inr (mkst mem (incr_pc pc) (update loc uid (Some n)) ppc ploc)
-      | None => inl "cannot evalute binop command"%string
-      end 
-    | Some (uid, cmd_phi pas) =>
-      let result := eval_phi ploc (lbl_of ppc) pas in
-      match result with
-      | Some n => inr (mkst mem (incr_pc pc) (update loc uid (Some n)) ppc ploc)
-      | None => inl "cannot evaluate phi"%string
-      end
-    | Some (uid, cmd_tmn tmn) =>
-      let result := eval_tmn loc tmn in
-      match result with
-      | Some l' => inr (mkst mem (block_entry l') loc pc loc)
-      | None => inl "cannot evaluate terminator"%string
-      end
-    | Some (uid, cmd_load addr) =>
-      inr (mkst mem (incr_pc pc) (update loc uid (Some (mem addr))) ppc ploc)
-    | Some (uid, cmd_store addr v) => 
-      let result := eval_val loc v in
-      match result with
-      (* | Some n => BUG HERE
-           inr (mkst (Memory.update mem addr n) (incr_pc pc) loc pc ploc) *)
-      | Some n => inr (mkst (Memory.update mem addr n) (incr_pc pc) loc ppc ploc)
-      | None => inl "cannot evaluate address to store to"%string
-      end
-    | None => inl "no instruction fetched"%string
-    end.
-
-  Fixpoint eval_until_pc (g : Cfg.t) (s : state)
-           (target_pc: pc)
-           (fuel : nat): err state :=
-    match fuel with
-    | 0 => inl "eval out of fuel"%string
-    | S n' =>
-      if (eq_dec_pc (st_pc s) target_pc) then inr s
-      else match eval_step g s with
-           | inl err => inl err
-           | inr s' => eval_until_pc g s' target_pc n'
-           end
-    end.
-
-  Definition eval_past_pc (g : Cfg.t) (s : state)
-             (target_pc : pc) (fuel : nat) : err state :=
-    match eval_until_pc g s target_pc fuel with
-    | inl err => inl err
-    | inr s' => eval_step g s'
-    end.
-
-  Definition eval_once_and_until_pc (g : Cfg.t) (s : state)
-             (target_pc: pc)
-             (fuel: nat) : err state :=
-    match eval_step g s with
-    | inl err => inl err
-    | inr s' => eval_until_pc g s' target_pc fuel
-    end.
-  
-  Ltac eval_step_with_step next_state constructor_rule
-       cfg_well_formed fetched :=
-    match goal with
-    | H: match ?X with
-         | Some _ => _
-         | None => _ end = inr ?next_state |-
-      step ?cfg ?curr_state next_state =>
-      destruct X eqn:Heval;
-      inversion H;
-      eapply constructor_rule;
-      try rewrite Cfg.insn_at_pc_fetch;
-      try apply fetched;
-      try apply cfg_well_formed;
-      rewrite Heval;
-      reflexivity
-    | H : _ = inr next_state |-
-      step ?cfg ?curr_state next_state =>
-      inversion H;
-      eapply constructor_rule;
-      try rewrite Cfg.insn_at_pc_fetch;
-      try apply fetched;
-      try apply cfg_well_formed;
-      reflexivity
-    end.
-
-  Ltac cases_on_eval_step_in_goal eval_insn :=
-    match goal with
-    | |- match ?X with
-        | Some _ => _
-        | None => _ end = inr _  =>
-      destruct X;
-      inversion eval_insn;
-      reflexivity
-    end.
-
-  Lemma evaluator_sound: forall g s s',
-      Cfg.wf_cfg g -> eval_step g s = inr s' -> step g s s'.
-  Proof.
-    intros g s s' wf_g; unfold eval_step;
-      destruct s as [mem curr_pc curr_loc previous_pc previous_loc];
-      destruct (Cfg.fetch g curr_pc) as 
-          [[uid [bop v1 v2 | pas | term | addr | addr v]] | ] eqn:fetched;
-      intros H;
-      [eval_step_with_step s' step_bop wf_g fetched |
-       eval_step_with_step s' step_phi wf_g fetched |
-       eval_step_with_step s' step_tmn wf_g fetched |
-       eval_step_with_step s' step_load wf_g fetched |
-       eval_step_with_step s' step_store wf_g fetched | idtac].
-    inversion H.
-  Qed.
-
-  Lemma evaluator_complete: forall g s s',
-      Cfg.wf_cfg g -> step g s s' -> eval_step g s = inr s'.
-  Proof.
-    intros g s s' wf_g step_rel; unfold eval_step;
-      inversion step_rel as
-        [mem pc loc bop v1 v2 uid n ppc ploc insn_at_pc_is eval_insn_ok s_eq s'_eq |
-         mem pc loc pas uid n ppc ploc insn_at_pc_is eval_insn_ok s_eq s'_eq |
-         mem pc label loc term uid ppc ploc insn_at_pc_is eval_insn_ok s_eq s'_eq |
-         mem pc loc uid addr ppc ploc insn_at_pc_is eval_insn_ok s'_eq |
-         mem pc loc uid addr v n ppc ploc insn_at_pc_is eval_insn_ok s_eq s'_eq];
-      simpl;
-      destruct (Cfg.fetch g pc) as [[curr_uid curr_cmd] | ] eqn:fetched; 
-      rewrite Cfg.insn_at_pc_fetch in insn_at_pc_is; try apply wf_g;
-        rewrite insn_at_pc_is in fetched;
-        inversion fetched;
-        try cases_on_eval_step_in_goal eval_insn_ok.
-    reflexivity.
-  Qed.
-
-  Theorem evaluator_correct: forall g s s',
-      Cfg.wf_cfg g -> eval_step g s = inr s' <-> step g s s'.
-  Proof.
-    intros g s s' wf_g; split;
-      revert g s s' wf_g.
-    apply evaluator_sound.
-    apply evaluator_complete.
-  Qed.  
-End Opsem.
-
-(** * Commented out 
 (** ** Typing *)
 (** FULL: The only ways in which a Vminus program can "go wrong" are
     by accessing a local variable that hasn't been defined or jumping
@@ -398,6 +125,7 @@ End Typing.
 (** ** Correctness *)
 
 Module OpsemCorrect.
+  Module Import V := Make (Cfg).  
   Import Typing Opsem.
 
 (** First, some utilities. *)
@@ -855,8 +583,4 @@ Qed.
 (* /FOLD *)
 (* /HIDE *)
 
-End OpsemCorrect. *)
-
-Export Opsem. (*  Typing OpsemCorrect. *)
-
-End Make.
+End OpsemCorrect.
