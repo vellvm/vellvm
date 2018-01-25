8b4cb41240656e4d15174b4edc87f2c11b72b43f
Emmett Neyman <eneyman@seas.upenn.edu>
2018-01-24 21:01:05 -0500

    started to implement block-based memory model

--- src/coq/Memory.v
+++ src/coq/Memory.v
@@ -2,15 +2,16 @@ Require Import ZArith List String Omega.
 Require Import  Vellvm.Ollvm_ast Vellvm.Classes Vellvm.Util.
 Require Import Vellvm.StepSemantics.
 Require Import Integers.
+Require Import ZMicromega.
 Import ListNotations.
 
 Set Implicit Arguments.
 Set Contextual Implicit.
 
-Module A : Vellvm.StepSemantics.ADDR with Definition addr := nat * nat.
-  Definition addr := nat * nat.
-  Definition null := 1000%nat.   (* TODO this is unsound if the memory has > 1000 values *)
-End A.  
+Module A : Vellvm.StepSemantics.ADDR with Definition addr := (nat * nat)%type.
+  Definition addr := (nat * nat)%type.
+  Definition null := (0%nat, 0%nat).   (* TODO this is unsound if the memory has > 1000 values *)
+End A.
 
 Module SS := StepSemantics.StepSemantics(A).
 Export SS.
@@ -19,28 +20,52 @@ Definition block := list byte.
 Definition memory := list block.
 Definition undef t := DVALUE_Undef t None.
 
+Fixpoint make_block (s: nat) : list byte :=
+  match s with
+  | 0%nat => []
+  | S n => Byte.zero::(make_block n)
+  end.
+
+Definition make_empty_block t :=
+  match t with
+  | TYPE_I sz =>
+    let x := match sz with
+            | 0 => 0%nat
+            | Z.pos n => BinPosDef.Pos.to_nat n
+            | Z.neg n => 0%nat (* this shouldn't happen *)
+            end in
+    make_block (if beq_nat (Nat.modulo x 8) 0%nat
+                then (x / 8)
+                else ((x / 8) + 1))
+  | TYPE_Pointer t => []
+  | TYPE_Struct f => []
+  | TYPE_Array s t => []
+  | TYPE_Vector s t => []
+  | _ => []
+  end.
+
 Definition mem_step {X} (e:effects X) (m:memory) :=
   match e with
   | Alloca t k =>
-    inr  ((m ++ [undef t])%list,
-          DVALUE_Addr (List.length m),
+    let new_block := make_empty_block t in
+    inr  ((m ++ [new_block])%list,
+          DVALUE_Addr (List.length m, 0%nat),
           k)
-  | Load t a k =>
-    inr (m,
+  | Load t a k => inl e
+    (*inr (m,
          nth_default (undef t) m a,
-         k)
+         k)*)
 
-  | Store a v k =>
-    inr (replace m a v,
+  | Store a v k => inl e
+    (*inr (replace m a v,
          DVALUE_None,
-         k)
+         k)*)
 
   | GEP t a vs k => inl e (* TODO: GEP semantics *)
 
   | ItoP t i k => inl e (* TODO: ItoP semantics *)
 
-  | PtoI t a k => inl e (* TODO: ItoP semantics *)                     
-                       
+  | PtoI t a k => inl e (* TODO: ItoP semantics *)
   | Call _ _ _ _ => inl e
   end.
 
@@ -58,7 +83,7 @@ CoFixpoint memD (m:memory) (d:Trace ()) : Trace () :=
     end
   | Vis x => Vis x
   end.
-                            
+
 Fixpoint MemDFin (m:memory) (d:Trace ()) (steps:nat) : option memory :=
   match steps with
   | O => None
