9977405a84a05b7aaef23813b2084ce5f1a44e2e
Steve Zdancewic <stevez@cis.upenn.edu>
2018-04-25 12:35:30 -0400

    memory address module

---
+++ src/coq/MemoryAddress.v
@@ -0,0 +1,18 @@
+(* -------------------------------------------------------------------------- *
+ *                     Vellvm - the Verified LLVM project                     *
+ *                                                                            *
+ *     Copyright (c) 2017 Steve Zdancewic <stevez@cis.upenn.edu>              *
+ *                                                                            *
+ *   This file is distributed under the terms of the GNU General Public       *
+ *   License as published by the Free Software Foundation, either version     *
+ *   3 of the License, or (at your option) any later version.                 *
+ ---------------------------------------------------------------------------- *)
+
+Require Import OrderedType OrderedTypeEx.
+
+Module Type ADDRESS.
+  Parameter addr : Set.
+  Parameter null : addr.
+  (* Include UsualOrderedType with Definition t:=addr. *)
+End ADDRESS.
+
