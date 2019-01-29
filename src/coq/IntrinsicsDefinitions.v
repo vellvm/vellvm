b237da36340bc8b10fbd65821147a4ada86420cd
Steve Zdancewic <stevez@cis.upenn.edu>
2019-01-29 13:40:31 -0500

    a bit of cleanup, and one more example

--- src/coq/IntrinsicsDefinitions.v
+++ src/coq/IntrinsicsDefinitions.v
@@ -47,13 +47,6 @@ Set Contextual Implicit.
    given by a function from a list of dynamic values to a dynamic value (or 
    possibly an error).  
 
-   - each intrinsic should "morally" be a total function: assuming the LLVM program 
-     is well formed, the intrisnic should always produce an LLVM value (which itself 
-     might be poison or undef)
-
-   - error should be returned only in the case that the LLVM program is ill-formed 
-     (e.g. if the wrong number and/or type of arguments is given to the intrinsic)
-
    NOTE: The intrinsics that can be defined at this layer of the semantics
    cannot affect the core interpreter state or the memory model.  This layer is
    useful for implementing "pure value" intrinsics like floating point
@@ -62,30 +55,52 @@ Set Contextual Implicit.
 
 *)
 Module Make(A:MemoryAddress.ADDRESS)(LLVMIO: LLVM_INTERACTIONS(A)).
+  Open Scope string_scope.
+
   Import LLVMIO.
   Import DV.
 
-  (* Each (pure) intrinsic is defined by a function of the following type. *)
+  (* Each (pure) intrinsic is defined by a function of the following type. 
+
+   - each intrinsic should "morally" be a total function: assuming the LLVM program 
+     is well formed, the intrisnic should always produce an LLVM value (which itself 
+     might be poison or undef)
+
+   - error should be returned only in the case that the LLVM program is ill-formed 
+     (e.g. if the wrong number and/or type of arguments is given to the intrinsic)
+
+  *)
   Definition semantic_function := (list dvalue) -> err dvalue.
 
   (* An association list mapping intrinsic names to their semantic definitions *)
   Definition intrinsic_definitions := list (string * semantic_function).
 
+
   (* Intrinsics semantic functions -------------------------------------------- *)
-  Open Scope string_scope.
+
+  (* Abosulute value for Float. *)
+  Definition llvm_fabs_f32 : semantic_function :=
+    fun args =>
+      match args with
+      | [DVALUE_Float d] => ret (DVALUE_Float (Float32.abs d))
+      | _ => raise "llvm_fabs_f64 got incorrect / ill-typed intputs"
+      end.
+
   
+  (* Abosulute value for Doubles. *)
   Definition llvm_fabs_f64 : semantic_function :=
     fun args =>
       match args with
       | [DVALUE_Double d] => ret (DVALUE_Double (Float.abs d))
       | _ => raise "llvm_fabs_f64 got incorrect / ill-typed intputs"
       end.
-  
 
   
-  (* Clients of Vellvm can add their own intrinsics definitions here. *)
+  (* Clients of Vellvm can register the names of their own intrinsics
+     definitions here. *)
   Definition defined_intrinsics : intrinsic_definitions :=
-    [("llvm.fabs.f64", llvm_fabs_f64)].  
+    [ ("llvm.fabs.f64", llvm_fabs_f64) ;
+      ("llvm.fabs.f32", llvm_fabs_f32) ].
 
   (* SAZ: TODO: it could be nice to provide a more general/modular way to "lift"
      primitive functions into intrinsics. *)
