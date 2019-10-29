c65068434eb717616451fe260e0853d8e98a7707
Yannick <y.zakowski@gmail.com>
2019-10-28 22:29:57 -0400

    Bit more complete traversal, specified the level of recursive instaces

--- src/coq/Transformations/Traversal.v
+++ src/coq/Transformations/Traversal.v
@@ -12,18 +12,18 @@ Section Traverse.
   (* Functors *)
   Global Instance endo_list {T: Set}
            `{endo T}
-    : endo (list T) :=
+    : endo (list T) | 50 :=
     List.map f_endo.
 
   Global Instance endo_option {T: Set}
            `{endo T}
-    : endo (option T) :=
+    : endo (option T) | 50 :=
     fun ot => match ot with None => None | Some t => Some (f_endo t) end.
 
   Global Instance endo_prod {T1 T2: Set}
            `{endo T1}
            `{endo T2}
-    : endo (T1 * T2) :=
+    : endo (T1 * T2) | 50 :=
     fun '(a,b) => (f_endo a, f_endo b).
 
   (* Abstract syntax *)
@@ -31,7 +31,7 @@ Section Traverse.
 
   Global Instance endo_ident
            `{endo raw_id}
-    : endo ident :=
+    : endo ident | 50 :=
     fun id =>
       match id with
       | ID_Global rid => ID_Global (f_endo rid)
@@ -40,11 +40,26 @@ Section Traverse.
 
   Global Instance endo_instr_id
            `{endo raw_id}
-    : endo instr_id :=
+    : endo instr_id | 50 :=
     fun id =>
       match id with
       | IId rid => IId (f_endo rid)
       | IVoid n => IVoid n
+      end.
+
+  Global Instance endo_typ
+         `{endo raw_id}
+    : endo typ | 50 :=
+    fix endo_typ t :=
+      match t with
+      | TYPE_Pointer t' => TYPE_Pointer (endo_typ t')
+      | TYPE_Array sz t' => TYPE_Array sz (endo_typ t')
+      | TYPE_Function ret args => TYPE_Function (endo_typ ret) (List.map endo_typ args)
+      | TYPE_Struct fields => TYPE_Struct (List.map endo_typ fields)
+      | TYPE_Packed_struct fields => TYPE_Packed_struct (List.map endo_typ fields)
+      | TYPE_Vector sz t' => TYPE_Vector sz (endo_typ t')
+      | TYPE_Identified id => TYPE_Identified (f_endo id)
+      | _ => t
       end.
 
   Global Instance endo_exp
@@ -54,7 +69,7 @@ Section Traverse.
            `{endo icmp}
            `{endo fbinop}
            `{endo fcmp}
-    : endo (exp T) :=
+    : endo (exp T) | 50 :=
     fix f_exp (e:exp T) :=
       match e with
       | EXP_Ident id => EXP_Ident (f_endo id)
@@ -115,13 +130,13 @@ Section Traverse.
   Global Instance endo_texp
            `{endo T}
            `{endo (exp T)}
-    : endo (texp T) :=
+    : endo (texp T) | 50 :=
     fun te => let '(t,e) := te in (f_endo t, f_endo e).
 
   Global Instance endo_instr
            `{endo T}
            `{endo (exp T)}
-    : endo (instr T) :=
+    : endo (instr T) | 50 :=
     fun ins =>
       match ins with
       | INSTR_Op op => INSTR_Op (f_endo op)
@@ -141,11 +156,11 @@ Section Traverse.
       | INSTR_LandingPad => ins
       end.
 
-  Definition endo_terminator
+  Global Instance endo_terminator
              `{endo T}
              `{endo raw_id}
              `{endo (exp T)}
-    : endo (terminator T) :=
+    : endo (terminator T) | 50 :=
     fun trm =>
       match trm with
       | TERM_Ret  v => TERM_Ret (f_endo v)
@@ -165,7 +180,7 @@ Section Traverse.
            `{endo T}
            `{endo raw_id}
            `{endo (exp T)}
-    : endo (phi T) :=
+    : endo (phi T) | 50 :=
     fun p => match p with
           | Phi t args => Phi (f_endo t) (f_endo args)
           end.
@@ -175,7 +190,7 @@ Section Traverse.
            `{endo (instr T)}
            `{endo (terminator T)}
            `{endo (phi T)}
-    : endo (block T) :=
+    : endo (block T) | 50 :=
     fun b =>
       mk_block (f_endo (blk_id b))
                (f_endo (blk_phis b))
@@ -183,15 +198,130 @@ Section Traverse.
                (f_endo (blk_term b))
                (blk_comments b).
 
+  Global Instance endo_global
+         `{endo raw_id}
+         `{endo T}
+         `{endo bool}
+         `{endo int}
+         `{endo string}
+         `{endo (exp T)}
+         `{endo linkage}
+         `{endo visibility}
+         `{endo dll_storage}
+         `{endo thread_local_storage}
+    : endo (global T) | 50 :=
+    fun g =>
+      mk_global
+        (f_endo (g_ident g))
+        (f_endo (g_typ g))
+        (f_endo (g_constant g))
+        (f_endo (g_exp g))
+        (f_endo (g_linkage g))
+        (f_endo (g_visibility g))
+        (f_endo (g_dll_storage g))
+        (f_endo (g_thread_local g))
+        (f_endo (g_unnamed_addr g))
+        (f_endo (g_addrspace g))
+        (f_endo (g_externally_initialized g))
+        (f_endo (g_section g))
+        (f_endo (g_align g)).
+
+  Global Instance endo_declaration
+         `{endo function_id}
+         `{endo T}
+         `{endo string}
+         `{endo int}
+         `{endo param_attr}
+         `{endo linkage}
+         `{endo visibility}
+         `{endo dll_storage}
+         `{endo cconv}
+         `{endo fn_attr}
+     : endo (declaration T) | 50 :=
+    fun d => mk_declaration
+            (f_endo (dc_name d))
+            (f_endo (dc_type d))
+            (f_endo (dc_param_attrs d))
+            (f_endo (dc_linkage d))
+            (f_endo (dc_visibility d))
+            (f_endo (dc_dll_storage d))
+            (f_endo (dc_cconv d))
+            (f_endo (dc_attrs d))
+            (f_endo (dc_section d))
+            (f_endo (dc_align d))
+            (f_endo (dc_gc d)).
+
+  Global Instance endo_definition
+         {FnBody:Set}
+         `{endo (declaration T)}
+         `{endo raw_id}
+         `{endo FnBody}
+    : endo (definition T FnBody) | 50 :=
+    fun d =>
+      mk_definition _
+                    (f_endo (df_prototype d))
+                    (f_endo (df_args d))
+                    (f_endo (df_instrs d)).
+
+  Global Instance endo_toplevel_entity
+         {FnBody:Set}
+         `{endo FnBody}
+         `{endo T}
+         `{endo (global T)}
+         `{endo raw_id}
+         `{endo (metadata T)}
+         `{endo (declaration T)}
+         `{endo (definition T FnBody)}
+         `{endo fn_attr}
+         `{endo int}
+         `{endo string}
+    : endo (toplevel_entity T FnBody) | 50 :=
+    fun tle =>
+      match tle with
+      | TLE_Comment msg => tle
+      | TLE_Target tgt => TLE_Target (f_endo tgt)
+      | TLE_Datalayout layout => TLE_Datalayout (f_endo layout)
+      | TLE_Declaration decl => TLE_Declaration (f_endo decl)
+      | TLE_Definition defn => TLE_Definition (f_endo defn)
+      | TLE_Type_decl id t => TLE_Type_decl (f_endo id) (f_endo t)
+      | TLE_Source_filename s => TLE_Source_filename (f_endo s)
+      | TLE_Global g => TLE_Global (f_endo g)
+      | TLE_Metadata id md => TLE_Metadata (f_endo id) (f_endo md)
+      | TLE_Attribute_group i attrs => TLE_Attribute_group (f_endo i) (f_endo attrs)
+      end.
+
+  Global Instance endo_modul
+         {FnBody:Set}
+         `{endo FnBody}
+         `{endo string}
+         `{endo T}
+         `{endo (global T)}
+         `{endo (declaration T)}
+         `{endo raw_id}
+    : endo (modul T FnBody) | 50 :=
+    fun m =>
+      mk_modul _
+               (f_endo (m_name m))
+               (f_endo (m_target m))
+               (f_endo (m_datalayout m))
+               (f_endo (m_type_defs m))
+               (f_endo (m_globals m))
+               (f_endo (m_declarations m))
+               (f_endo (m_definitions m)).
+
   Global Instance endo_cfg
            `{endo raw_id}
            `{endo (block T)}
-    : endo (cfg T) :=
+    : endo (cfg T) | 50 :=
     fun p => mkCFG _
                 (f_endo (init _ p))
                 (f_endo (blks _ p))
                 (f_endo (args _ p)).
 
+  (* By default, the solver can always pick the identity as an instance.
+     However both structural traversal from this section and local
+     instances should always have priority over this, hence the 100.
+   *)
   Global Instance endo_id (T: Set): endo T | 100 := fun x: T => x.
 
 End Traverse.
