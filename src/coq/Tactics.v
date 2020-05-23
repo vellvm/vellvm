b9ffa59a1808cd1154df7ec5c5459509b2c18b07
Yannick <y.zakowski@gmail.com>
2020-05-23 17:01:11 -0400

    Injectivity of naming scheme

---
+++ src/coq/Tactics.v
@@ -0,0 +1,121 @@
+Ltac flatten_goal :=
+  match goal with
+  | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
+  end.
+
+Ltac flatten_hyp h :=
+  match type of h with
+  | context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
+  end.
+
+Ltac flatten_all :=
+  match goal with
+  | h: context[match ?x with | _ => _ end] |- _ => let Heq := fresh "Heq" in destruct x eqn:Heq
+  | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
+  end.
+
+Ltac inv H := inversion H; try subst; clear H.
+
+Global Tactic Notation "intros !" := repeat intro.
+
+(* inv by name of the Inductive relation *)
+Ltac invn f :=
+    match goal with
+    | [ id: f |- _ ] => inv id
+    | [ id: f _ |- _ ] => inv id
+    | [ id: f _ _ |- _ ] => inv id
+    | [ id: f _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ _ _ _ |- _ ] => inv id
+    | [ id: f _ _ _ _ _ _ _ _ |- _ ] => inv id
+    end.
+
+(* destruct by name of the Inductive relation *)
+Ltac destructn f :=
+    match goal with
+    | [ id: f |- _ ] => destruct id
+    | [ id: f _ |- _ ] => destruct id
+    | [ id: f _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ _ _ _ |- _ ] => destruct id
+    | [ id: f _ _ _ _ _ _ _ _ |- _ ] => destruct id
+    end.
+
+(* apply by name of the Inductive relation *)
+Ltac appn f :=
+    match goal with
+    | [ id: f |- _ ] => apply id
+    | [ id: f _ |- _ ] => apply id
+    | [ id: f _ _ |- _ ] => apply id
+    | [ id: f _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ _ _ _ |- _ ] => apply id
+    | [ id: f _ _ _ _ _ _ _ _ |- _ ] => apply id
+    end.
+
+(* eapply by name of the Inductive relation *)
+Ltac eappn f :=
+    match goal with
+    | [ id: f |- _ ] => eapply id
+    | [ id: f _ |- _ ] => eapply id
+    | [ id: f _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ _ _ _ |- _ ] => eapply id
+    | [ id: f _ _ _ _ _ _ _ _ |- _ ] => eapply id
+    end.
+
+(** [break_match_hyp] looks for a [match] construct in some
+    hypothesis, and destructs the discriminee, while retaining the
+    information about the discriminee's value leading to the branch
+    being taken. *)
+Ltac break_match_hyp :=
+  match goal with
+    | [ H : context [ match ?X with _ => _ end ] |- _] =>
+      match type of X with
+        | sumbool _ _ => destruct X
+        | _ => destruct X eqn:?
+      end
+  end.
+
+(** [break_match_goal] looks for a [match] construct in your goal, and
+    destructs the discriminee, while retaining the information about
+    the discriminee's value leading to the branch being taken. *)
+Ltac break_match_goal :=
+  match goal with
+    | [ |- context [ match ?X with _ => _ end ] ] =>
+      match type of X with
+        | sumbool _ _ => destruct X
+        | _ => destruct X eqn:?
+      end
+  end.
+
+(** [break_match] breaks a match, either in a hypothesis or in your
+    goal. *)
+Ltac break_match := break_match_goal || break_match_hyp.
+
+Ltac inv_option :=
+  match goal with
+  | h: Some _ = Some _ |-  _ => inv h
+  | h: None   = Some _ |-  _ => inv h
+  | h: Some _ = None   |-  _ => inv h
+  | h: None   = None   |-  _ => inv h
+  end.
+
+Ltac inv_sum :=
+  match goal with
+  | h: inl _ = inl _ |-  _ => inv h
+  | h: inr _ = inr _ |-  _ => inv h
+  | h: inl _ = inr _ |-  _ => inv h
+  | h: inr _ = inl _ |-  _ => inv h
+  end.
+
--- src/coq/Util.v
+++ src/coq/Util.v
@@ -6,8 +6,410 @@
  *                                                                            *
  *   This file is distributed under the terms of the GNU General Public       *
  *   License as published by the Free Software Foundation, either version     *
-b *   3 of the License, or (at your option) any later version.                 *
+ *   3 of the License, or (at your option) any later version.                 *
  ---------------------------------------------------------------------------- *)
+
+From Vellvm Require Import Tactics.
+
+From Coq Require Import
+     Ascii
+     Strings.String
+     Arith.Arith
+     ZArith
+     Nat 
+     NPeano
+     Psatz.
+Require Import FunInd Recdef.
+
+Section nat_Show.
+  Local Open Scope string.
+  Local Open Scope nat.
+  Definition get_last_digit (n: nat): string :=
+    match n mod 10 with
+    | 0 => "0" | 1 => "1" | 2 => "2" | 3 => "3" | 4 => "4" | 5 => "5"
+    | 6 => "6" | 7 => "7" | 8 => "8" | _ => "9"
+    end.
+
+  Function string_of_nat_aux (acc: string) (n : nat) {measure id n}: string :=
+    let acc' := get_last_digit n ++ acc in
+    match n / 10 with
+    | 0 => acc'
+    | n' => string_of_nat_aux acc' n'
+    end.
+  intros _ n m ineq.
+  unfold id; simpl.
+  destruct n as [| n]; [simpl in *; easy |].
+  rewrite <- ineq; apply Nat.div_lt; lia.
+  Defined.
+
+  Definition string_of_nat := string_of_nat_aux "".
+
+  Lemma mod_div_eq: forall a b q,
+      q <> 0 ->
+      a mod q = b mod q ->
+      a / q = b / q ->
+      a = b.
+  Proof.
+    intros.
+    rewrite (NPeano.Nat.div_mod a q); auto.
+    rewrite (NPeano.Nat.div_mod b q); auto.
+  Qed.
+
+  Lemma get_last_digit_inj: forall n m,
+      n <> m ->
+      n/10 = m/10 ->
+      get_last_digit n <>
+      get_last_digit m.
+  Proof.
+    intros n m ineq eq.
+    unfold get_last_digit.
+    do 9 (flatten_goal; [repeat flatten_goal; try easy; exfalso; apply ineq,(mod_div_eq n m 10); lia |]).
+    subst.
+    do 9 (flatten_goal; [easy |]).
+    subst.
+    exfalso.
+    destruct n8.
+    2: generalize (NPeano.Nat.mod_upper_bound n 10 (ltac:(auto))); intros EQ; rewrite Heq in EQ; lia. 
+    destruct n9.
+    2: generalize (NPeano.Nat.mod_upper_bound m 10 (ltac:(auto))); intros EQ; rewrite Heq0 in EQ; lia. 
+    exfalso; apply ineq,(mod_div_eq n m 10); lia.
+  Qed.
+
+  Lemma append_EmptyString: forall s,
+      s ++ "" = s.
+  Proof.
+    induction s as [| c s IH]; simpl; auto.
+    rewrite IH; auto.
+  Qed.
+
+  Lemma append_String: forall s c s',
+      s ++ String c s' = (s ++ String c "") ++ s'.
+  Proof.
+    induction s as [| c' s IH]; simpl; intros c s'; auto.
+    f_equal.
+    apply IH.
+  Qed.
+
+  Lemma append_char_simplify_r: forall s s' c,
+      s ++ String c "" = s' ++ String c "" ->
+      s = s'.
+  Proof.
+    induction s as [| c s IH]; intros [| c' s']; simpl; intros tl eq; auto.
+    - inv eq.
+      destruct s'; inv H1.
+    - inv eq.
+      destruct s; inv H1.
+    - inv eq.
+      f_equal.
+      eapply IH; eauto.
+  Qed.
+
+  Lemma append_simplify_r: forall s2 s1 s1',
+      s1 ++ s2 = s1' ++ s2 ->
+      s1 = s1'.
+  Proof.
+    induction s2 as [| c s2 IH].
+    - intros s1 s1' H; rewrite 2 append_EmptyString in H; auto. 
+    - intros s1 s1' H.
+      rewrite append_String in H.
+      rewrite (append_String s1' c s2) in H.
+      apply IH,append_char_simplify_r in H.
+      assumption.
+  Qed.
+
+  Lemma append_simplify_l: forall s1 s2 s2',
+      s1 ++ s2 = s1 ++ s2' ->
+      s2 = s2'.
+  Proof.
+    induction s1 as [| c s1 IH]; simpl; intros s2 s2' eq; auto.
+    inv eq; apply IH; auto.
+  Qed.
+
+  Lemma append_assoc: forall s1 s2 s3,
+      s1 ++ s2 ++ s3 = (s1 ++ s2) ++ s3.
+  Proof.
+    induction s1 as [| c s1 IH]; simpl; intros; [reflexivity |].
+    rewrite IH; reflexivity.
+  Qed.
+
+  Lemma string_of_nat_aux_prepends:
+    forall n acc,
+    exists hd, hd <> "" /\ string_of_nat_aux acc n = hd ++ acc.
+  Proof.
+    induction n using (well_founded_induction lt_wf).
+    intros acc.
+    rewrite string_of_nat_aux_equation.
+    flatten_goal.
+    { eexists; split; [| reflexivity].
+      unfold get_last_digit.
+      do 9 (flatten_goal; [easy |]); easy.
+    }
+    edestruct (H (S n0)) as (hd & ineq & eq).
+    - rewrite <- Heq; apply Nat.div_lt.
+      { destruct n; [inv Heq| auto with arith]. }
+      auto with arith.
+    - rewrite eq.
+      rewrite append_assoc.
+      exists (hd ++ get_last_digit n); split; auto.
+      clear - ineq.
+      intros abs; apply ineq.
+      destruct hd; [reflexivity | inv abs].
+  Qed.
+
+  Lemma get_last_digit_len1: forall n,
+      String.length (get_last_digit n) = 1.
+  Proof.
+    intros n; unfold get_last_digit.
+    do 9 (flatten_goal; [reflexivity |]); reflexivity.
+  Qed.
+
+  Lemma length_append:
+    forall s s', String.length (s ++ s') = String.length s + String.length s'.
+  Proof.
+    induction s as [| c s IH]; intros s'; simpl; [reflexivity | rewrite IH; reflexivity].
+  Qed.
+
+  Lemma length_nonEmpty:
+    forall s, s <> "" -> String.length s > 0.
+  Proof.
+    intros []; [intros abs; easy | intros _; simpl; auto with arith].
+  Qed.
+
+  Lemma append_same_length_eq_l:
+    forall s1 s1' s2 s2',
+      s1 ++ s2 = s1' ++ s2' ->
+      String.length s1 = String.length s1' ->
+      s1 = s1'.
+  Proof.
+    induction s1 as [| c1 s1 IH]; simpl; intros.
+    - destruct s1'; [reflexivity | inv H0]. 
+    - destruct s1' as [| c1' s1']; [inv H0 |].
+      simpl in *.
+      inv H0.
+      inv H.
+      f_equal.
+      eapply IH; eauto.
+  Qed.
+
+  Lemma append_same_length_eq_r:
+    forall s1 s1' s2 s2',
+      s1 ++ s2 = s1' ++ s2' ->
+      String.length s2 = String.length s2' ->
+      s2 = s2'.
+  Proof.
+    intros.
+    assert (String.length s1 = String.length s1').
+    { generalize (length_append s1 s2).
+      intros ?.
+      rewrite H0,H, length_append in H1. 
+      lia.
+    }
+    eapply append_same_length_eq_l in H1; eauto.
+    subst.
+    eapply append_simplify_l; eauto. 
+  Qed.
+
+  Ltac eq_fun H f :=
+    match type of H with
+    | ?x1 = ?x2 => assert (f x1 = f x2) by (rewrite H; reflexivity)
+    end.
+
+  Lemma append_same_length_eq_r':
+    forall s1 s1' s2 s2',
+      s1 ++ s2 = s1' ++ s2' ->
+      String.length s2 = String.length s2' ->
+      s1 = s1'.
+  Proof.
+    intros.
+    generalize H; intros H'; apply append_same_length_eq_r in H'; auto; subst.
+    eapply append_same_length_eq_l; eauto.
+    eq_fun H String.length.
+    rewrite 2 length_append in H1.
+    lia.
+  Qed.
+
+  Lemma string_of_nat_aux_inj: forall n m acc,
+      n <> m ->
+      string_of_nat_aux acc n <>
+      string_of_nat_aux acc m.
+  Proof.
+    induction n using (well_founded_induction lt_wf).
+    intros m acc ineq.
+    rewrite 2 string_of_nat_aux_equation.
+    flatten_goal.
+    - flatten_goal.
+      + intros abs.
+        apply (get_last_digit_inj n m); try lia.
+        apply append_simplify_r in abs; auto.
+      + destruct (string_of_nat_aux_prepends (S n0) (get_last_digit m ++ acc)) as (hd & nonnil & ->). 
+        intros abs. 
+        match type of abs with
+        | ?s1 = ?s2 => assert (abs':String.length s1 = String.length s2) by (rewrite abs; reflexivity)
+        end.
+        rewrite 3 length_append, 2 get_last_digit_len1 in abs'.
+        clear - abs' nonnil.
+        apply length_nonEmpty in nonnil; simpl in *.
+        lia.
+    - flatten_goal.
+      + destruct (string_of_nat_aux_prepends (S n0) (get_last_digit n ++ acc)) as (hd & nonnil & ->). 
+        intros abs. 
+        match type of abs with
+        | ?s1 = ?s2 => assert (abs':String.length s1 = String.length s2) by (rewrite abs; reflexivity)
+        end.
+        rewrite 3 length_append, 2 get_last_digit_len1 in abs'.
+        clear - abs' nonnil.
+        apply length_nonEmpty in nonnil; simpl in *.
+        lia.
+      + destruct (Nat.eq_decidable (n mod 10) (m mod 10)).
+        * replace (get_last_digit m) with (get_last_digit n).
+          2:unfold get_last_digit; rewrite H0; reflexivity.
+          destruct (Nat.eq_decidable n0 n1).
+          { subst. exfalso.
+            eapply ineq, (mod_div_eq n m 10); lia.
+          }
+          apply H.
+          rewrite <- Heq; apply Nat.div_lt.
+          { destruct n; [inv Heq| auto with arith]. }
+          auto with arith.
+          auto.
+        * destruct (string_of_nat_aux_prepends (S n0) (get_last_digit n ++ acc)) as (hd & nonnil & ->).
+          destruct (string_of_nat_aux_prepends (S n1) (get_last_digit m ++ acc)) as (hd' & nonnil' & ->). 
+          intros abs.
+          rewrite 2 append_assoc in abs.
+          apply append_simplify_r in abs.
+          apply append_same_length_eq_r in abs; [| rewrite 2get_last_digit_len1; reflexivity].
+          clear -abs H0.
+          unfold get_last_digit in abs.
+          apply H0; clear H0.
+          do 9 (flatten_all; [do 9 (flatten_all; try easy) |]).
+          do 9 (flatten_all; try easy).         
+          subst.
+          assert (tmp:10 <> 0) by lia.
+          destruct n8; [| generalize (Nat.mod_upper_bound n 10 tmp); intros ineq; rewrite Heq in ineq; lia].
+          destruct n17; [easy| generalize (Nat.mod_upper_bound m 10 tmp); intros ineq; rewrite Heq8 in ineq; lia].
+  Qed.          
+
+  Lemma string_of_nat_inj: forall n m,
+      n <> m ->
+      string_of_nat n <>
+      string_of_nat m.
+  Proof.
+    intros n m; unfold string_of_nat; apply string_of_nat_aux_inj.
+  Qed.
+
+  Definition string_of_Z (n: Z) : string :=
+    match n with
+    | Z0 => "0"
+    | Zpos p => string_of_nat (Pos.to_nat p)
+    | Zneg p => "-" ++ string_of_nat (Pos.to_nat p)
+    end.
+
+  Lemma get_last_digit_0_iff:
+    forall n, get_last_digit n = "0" <-> n mod 10 =  0.
+  Proof.
+    intros n; split; intros H.
+    - unfold get_last_digit in H.
+      flatten_hyp H; [subst; reflexivity |].
+      do 8 (flatten_hyp H; [inv H |]); inv H.
+    - unfold get_last_digit; rewrite H; reflexivity.
+  Qed.
+
+  Lemma string_of_nat_0_iff:
+    forall n, string_of_nat n = "0" <-> n = 0.
+  Proof.
+    intros n; split; intros H.
+    - destruct n as [|n]; [reflexivity | exfalso].
+      unfold string_of_nat in H; rewrite string_of_nat_aux_equation in H.
+      flatten_hyp H.
+      + rewrite append_EmptyString in H.
+        rewrite get_last_digit_0_iff in H.
+        generalize (Nat.mod_eq (S n) 10 (ltac:(lia))).
+        rewrite H, Heq; simpl; intros abs; inv abs.
+      + rewrite append_EmptyString in H; destruct (string_of_nat_aux_prepends (S n0) (get_last_digit (S n))) as (hd & eq & eq'). 
+        rewrite H in eq'.
+        replace "0" with ("" ++ "0") in eq' by reflexivity.
+        apply append_same_length_eq_r' in eq' ; [| rewrite get_last_digit_len1; reflexivity].
+        subst; tauto.
+    - subst; reflexivity. 
+  Qed.
+
+  Lemma pos_to_nat_inj:
+    forall p p',
+      p <> p' ->
+      Pos.to_nat p <> Pos.to_nat p'.
+  Proof.
+    intros; intro abs; apply Pos2Nat.inj in abs; easy. 
+  Qed.
+
+  Lemma string_of_nat_aux_hd_get_last_digit:
+    forall n c s acc, string_of_nat_aux acc n = String c s -> exists n', String c "" = get_last_digit n'.
+  Proof.
+    intros n c.
+    induction n using (well_founded_induction lt_wf).
+    intros s acc EQ.
+    unfold string_of_nat in EQ.
+    rewrite string_of_nat_aux_equation in EQ.
+    flatten_hyp EQ.
+    - replace (String c s) with (String c "" ++ s) in EQ by reflexivity.
+      apply append_same_length_eq_l in EQ; [| rewrite get_last_digit_len1; reflexivity].
+      rewrite <- EQ; eauto.
+    - apply H in EQ; [assumption |].
+      rewrite <- Heq; apply Nat.div_lt.
+      { destruct n; [inv Heq| auto with arith]. }
+      auto with arith.
+  Qed.
+
+  Lemma string_of_nat_hd_get_last_digit:
+    forall n c s, string_of_nat n = String c s -> exists n', String c "" = get_last_digit n'.
+  Proof.
+    intros; eapply string_of_nat_aux_hd_get_last_digit; eauto.
+  Qed.
+
+  Lemma string_of_Z_inj: forall n m,
+      n <> m ->
+      string_of_Z n <>
+      string_of_Z m.
+  Proof.
+    intros n m ineq.
+    destruct n, m; simpl; try easy.
+    try (intros abs; symmetry in abs; rewrite string_of_nat_0_iff in abs; lia).
+    try (intros abs; rewrite string_of_nat_0_iff in abs; lia).
+    {apply string_of_nat_inj, pos_to_nat_inj; intros abs; apply ineq; rewrite abs; reflexivity. }
+    { destruct (string_of_nat (Pos.to_nat p)) as [| c s] eqn:EQ; [easy |].
+      apply string_of_nat_hd_get_last_digit in EQ; destruct EQ as [? EQ].
+      intros abs; inv abs.
+      unfold get_last_digit in EQ.
+      do 9 (flatten_hyp EQ; [inv EQ |]); inv EQ.
+    }
+    { destruct (string_of_nat (Pos.to_nat p0)) as [| c s] eqn:EQ; [easy |].
+      apply string_of_nat_hd_get_last_digit in EQ; destruct EQ as [? EQ].
+      intros abs; inv abs.
+      unfold get_last_digit in EQ.
+      do 9 (flatten_hyp EQ; [inv EQ |]); inv EQ.
+    }
+    {intros abs; inv abs.
+     generalize H0.
+     apply string_of_nat_inj, pos_to_nat_inj; intros abs; apply ineq; rewrite abs; reflexivity. }
+  Qed.
+
+  (* Things are much simpler otherwise with a binary representation... *)
+  Fixpoint string_of_nat_bin (n: nat): string :=
+    match n with
+    | O => ""
+    | S n => String (ascii_of_nat 49) (string_of_nat_bin n)
+    end.
+
+  Lemma string_of_nat_bin_inj:
+    forall (n m: nat), n <> m -> string_of_nat_bin n <> string_of_nat_bin m.
+  Proof.
+    induction n as [| n IH]; simpl; intros m ineq.
+    - destruct m as [| m]; [lia | intros abs; inversion abs].
+    - destruct m as [| m]; [intros abs; inversion abs |].
+      simpl; intros abs; inversion abs; subst; clear abs.
+      apply (IH m); auto.
+  Qed.
+
+End nat_Show.
 
 From Coq Require Import
      List
@@ -518,8 +920,6 @@ Proof.
   repeat rewrite Nat.sub_0_r.
   apply map_ext; intro; omega.
 Qed.
-
-
 
 Lemma flat_map_map : forall A B C (f : B -> list C) (g : A -> B) l,
   flat_map f (map g l) = flat_map (fun x => f (g x)) l.
