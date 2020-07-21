540240e604b982ee56e755820939a87daa405fe8
Yannick <y.zakowski@gmail.com>
2020-07-21 18:26:28 -0400

    Missing file

---
+++ src/coq/Denotation_Theory.v
@@ -0,0 +1,194 @@
+(* begin hide *)
+
+From Coq Require Import
+     List.
+
+From ITree Require Import
+     ITree
+     ITreeFacts
+     Events.State
+     Events.StateFacts
+     InterpFacts
+     KTreeFacts
+     Eq.Eq.
+
+From Vellvm Require Import
+     Util
+     PropT
+     DynamicTypes
+     CFG
+     LLVMAst
+     LLVMEvents
+     TopLevel
+     Tactics.
+
+Import ListNotations.
+Import D.
+Import Eq.
+(* Import CatNotations. *)
+
+Import MonadNotation.
+Open Scope monad_scope.
+
+
+(* end hide *)
+
+(** * Structural equations at the representation level 
+
+We prove here the equational theory that holds independently of the
+interpretation of events.
+These equations are expressed in terms of [eutt eq] over uninterpreted trees.
+They derive from the [itree] combinators used to compute the uninterpreted
+representation VIR's syntax.
+
+In particular, notice that since [interp] is a iterative-monad morphism that respects
+[eutt eq], these equations get transported at all levels of interpretations.
+
+*)
+
+(** [denote_code] *)
+
+Lemma denote_code_nil :
+  denote_code [] ≈ ret tt.
+Proof.
+  intros.
+  cbn. rewrite bind_ret_l.
+  reflexivity.
+Qed.
+
+Lemma denote_code_app :
+  forall a b,
+    denote_code (a ++ b)%list ≈ ITree.bind (denote_code a) (fun _ => denote_code b).
+Proof.
+  induction a; intros b.
+  - cbn. rewrite 2 bind_ret_l.
+    reflexivity.
+  - simpl.
+    unfold denote_code, map_monad_ in *.
+    simpl in *.
+    repeat rewrite bind_bind.
+    specialize (IHa b).
+    apply eutt_eq_bind; intros ().
+    rewrite bind_bind.
+    setoid_rewrite bind_ret_l.
+    setoid_rewrite IHa.
+    repeat rewrite bind_bind.
+    setoid_rewrite bind_ret_l.
+    reflexivity.
+Qed.
+
+Lemma denote_code_cons :
+  forall a l,
+    denote_code (a::l) ≈ ITree.bind (denote_instr a) (fun _ => denote_code l).
+Proof.
+  intros.
+  rewrite list_cons_app, denote_code_app.
+  cbn.
+  repeat rewrite bind_bind.
+  apply eutt_eq_bind; intros ().
+  rewrite !bind_bind, !bind_ret_l.
+  reflexivity.
+Qed.
+
+Lemma denote_code_singleton :
+  forall a,
+    denote_code [a] ≈ denote_instr a.
+Proof.
+  intros a.
+  rewrite denote_code_cons.
+  setoid_rewrite denote_code_nil.
+  cbn.
+
+  epose proof bind_ret_r.
+  specialize (H (denote_instr a)).
+
+  rewrite <- H.
+  rewrite bind_bind.
+  apply eutt_eq_bind; intros []; rewrite bind_ret_l; reflexivity. 
+Qed.
+
+(** denote_bks *)
+Lemma denote_bks_nil: forall s, denote_bks [] s ≈ ret (inl s).
+Proof.
+  intros []; unfold denote_bks.
+  match goal with
+  | |- CategoryOps.iter (C := ktree _) ?body ?s ≈ _ =>
+    rewrite (unfold_iter body s)
+  end.
+  repeat (cbn; (rewrite bind_bind || rewrite bind_ret_l)).
+  reflexivity.
+Qed.
+
+(* YZ: I am not sure that we have a good use for such a lemma.
+   In general, a singleton could loop on itself, so that the general
+   lemma for a singleton is exactly [denote_bks_unfold].
+   Ensuring statically that you do not loop requires very strong
+   restrictions on the expression used for the terminals.
+ *)
+(*
+Lemma denote_bks_singleton :
+  forall (bk : block dtyp) (bid_from : block_id),
+    eutt Logic.eq (denote_bks [bk] (bid_from, blk_id bk)) 
+         (bd <- denote_block bk bid_from;; 
+          match bd with
+          | inr dv => ret (inr dv)
+          | inl bid_target => ret (inl (blk_id bk,bid_target))
+          end).
+Proof.
+  intros *.
+  cbn.
+  unfold denote_bks.
+  rewrite KTreeFacts.unfold_iter_ktree.
+  cbn.
+  destruct (Eqv.eqv_dec_p (blk_id bk) (blk_id bk)) eqn:Heq'; [| contradiction n; reflexivity].
+  repeat rewrite bind_bind.
+  repeat setoid_rewrite bind_bind.
+  apply eutt_eq_bind; intros ?.
+  apply eutt_eq_bind; intros ?.
+  repeat setoid_rewrite bind_ret_l.
+  apply eutt_eq_bind; intros ?. 
+  apply eutt_eq_bind; intros ?. 
+  setoid_rewrite bind_ret_l.
+  rewrite Heqterm.
+  cbn.
+  apply eutt_eq_bind; intros ?; rewrite bind_ret_l.
+  cbn.
+  setoid_rewrite translate_ret.
+  setoid_rewrite bind_ret_l.
+  destruct (Eqv.eqv_dec_p (blk_id b) nextblock); try contradiction.
+  repeat setoid_rewrite bind_ret_l. unfold Datatypes.id.
+  reflexivity.
+Qed.
+*)
+
+Lemma denote_bks_unfold_in: forall bks bid_from bid_src bk,
+    find_block dtyp bks bid_src = Some bk ->
+    denote_bks bks (bid_from, bid_src) ≈
+    vob <- denote_block bk bid_from ;;
+    match vob with
+    | inr v => ret (inr v)
+    | inl bid_target => denote_bks bks (bid_src, bid_target)
+    end.
+Proof.
+  intros * GET_BK.
+  cbn. unfold denote_bks at 1.
+  rewrite KTreeFacts.unfold_iter_ktree. cbn.
+  rewrite GET_BK.
+  repeat setoid_rewrite bind_bind.
+  repeat (apply eutt_eq_bind; intros ?).
+  break_sum; rewrite bind_ret_l; [|reflexivity].
+  rewrite tau_eutt; reflexivity.
+Qed.
+
+Lemma denote_bks_unfold_not_in: forall bks bid_from bid_src, 
+    find_block dtyp bks bid_src = None ->
+    denote_bks bks (bid_from, bid_src) ≈ Ret (inl (bid_from,bid_src)).
+Proof.
+  intros * GET_BK.
+  unfold denote_bks.
+  rewrite KTreeFacts.unfold_iter_ktree.
+  rewrite GET_BK; cbn.
+  rewrite bind_ret_l.
+  reflexivity.
+Qed.
+
