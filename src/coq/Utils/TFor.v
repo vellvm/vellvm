b2eb3f0c552c27b0f6190a3a23d3304001dd545d
euisuny <ey222@cornell.edu>
2020-12-14 17:26:14 -0500

    Generalize tfor bounds check

--- src/coq/Utils/TFor.v
+++ src/coq/Utils/TFor.v
@@ -40,20 +40,30 @@ Section TFor.
    *)
   Definition tfor {E X} (body : nat -> X -> itree E X) (from to : nat) : X -> itree E X :=
     fun x => ITree.iter (fun '(p,x) =>
-                        if beq_nat p to
+                        if to <=? p
                         then Ret (inr x)
                         else
                           y <- (body p x);; (Ret (inl (S p,y)))
                      ) (from,x).
 
+
   (* [tfor] excludes the upper bound, hence [tfor body k k] does nothing *)
-  Lemma tfor_0: forall {E A} k (body : nat -> A -> itree E A) a0,
-      tfor body k k a0 ≈ Ret a0.
+  Lemma tfor_le: forall {E A} j k (body : nat -> A -> itree E A) a0,
+      k <= j ->
+      tfor body j k a0 ≈ Ret a0.
   Proof.
     intros; unfold tfor; cbn.
     unfold iter, CategoryKleisli.Iter_Kleisli, Basics.iter, MonadIter_itree.
-    rewrite unfold_iter, Nat.eqb_refl, bind_ret_l.
+    rewrite unfold_iter. setoid_rewrite <- Nat.leb_le in H. rewrite H.
+    rewrite bind_ret_l.
     reflexivity.
+  Qed.
+
+  (* [tfor] excludes the upper bound, hence [tfor body k k] does nothing *)
+  Lemma tfor_0: forall {E A} j (body : nat -> A -> itree E A) a0,
+      tfor body j j a0 ≈ Ret a0.
+  Proof.
+    intros; rewrite tfor_le; [reflexivity | lia]. 
   Qed.
 
   (* One step unrolling of the combinator *)
@@ -63,9 +73,9 @@ Section TFor.
            a <- body i a0;; tfor body (S i) j a.
   Proof.
     intros; unfold tfor; cbn.
-    unfold iter, CategoryKleisli.Iter_Kleisli, Basics.iter, MonadIter_itree.
+    unfold iter. unfold CategoryKleisli.Iter_Kleisli, Basics.iter, MonadIter_itree.
     rewrite unfold_iter at 1.
-    pose proof Nat.eqb_neq i j as [_ EQ].
+    assert (EQ: not (j <= i)) by lia. rewrite <- Nat.leb_nle in EQ.
     rewrite EQ; try lia.
     rewrite bind_bind.
     apply eutt_eq_bind; intros ?; rewrite bind_ret_l, tau_eutt.
@@ -83,7 +93,7 @@ Section TFor.
     remember (j - i) as p; revert a0 i LE1 Heqp.
     induction p as [| p IH]; intros ? ? LE1 EQ.
     - replace i with j by lia.
-      rewrite tfor_0, bind_ret_l.
+      rewrite tfor_0 , bind_ret_l.
       reflexivity.
     - rewrite tfor_unroll; [| lia].
       rewrite tfor_unroll; [| lia].
@@ -120,19 +130,19 @@ Section TFor.
       tfor body (S i) (S j) a0 ≈ tfor body i j a0.
   Proof.
     intros; unfold tfor; cbn.
+
     unfold iter, CategoryKleisli.Iter_Kleisli, Basics.iter, MonadIter_itree.
     apply eutt_iter'' with (RI1:=fun '(a,x) '(b, y) => a = S b /\ x = y) (RI2:=fun '(a,x) '(b, y) => a = S b /\ x = y); auto.
     intros [j1 acc1] [j2 acc2] H1.
+
     destruct H1. subst.
     cbn.
-    pose proof (Nat.eq_dec j2 j) as [EQ | NEQ].
+    destruct (j <=? j2) as [EQ | NEQ].
 
-    - subst. rewrite Nat.eqb_refl.
+    - subst.
       apply eutt_Ret.
       constructor; auto.
-    - apply Nat.eqb_neq in NEQ.
-      rewrite NEQ.
-      eapply eutt_clo_bind.
+    - eapply eutt_clo_bind.
       rewrite H.
       reflexivity.
       intros; subst.
