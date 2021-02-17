054cce37858589736e3d961543f114eb0f405334
Yannick <y.zakowski@gmail.com>
2021-02-17 21:29:17 +0100

    Quickly fixing topleverrefinements, this file si fairly old it could use some reviewing

--- src/coq/Theory/TopLevelRefinements.v
+++ src/coq/Theory/TopLevelRefinements.v
@@ -18,6 +18,7 @@ From Vellvm Require Import
      Syntax.LLVMAst
      Semantics.LLVMEvents
      Semantics.TopLevel
+     Semantics.InterpretationStack
      Handlers.Handlers
      Theory.Refinement
      Theory.InterpreterMCFG
@@ -42,6 +43,7 @@ Require Import Paco.paco.
 
 Import ListNotations.
 Import ITree.Basics.Basics.Monads.
+Import SemNotations.
 
 Module R := Refinement.Make Memory.Addr LLVMEvents.
 Import R. 
@@ -138,62 +140,62 @@ Qed.
 
 Section REFINEMENT.
   
-(** We first prove that the [itree] refinement at level [i] entails the
+  (** We first prove that the [itree] refinement at level [i] entails the
     refinement at level [i+1] after running the [i+1] level of interpretation
- *)
+   *)
 
-Lemma refine_01: forall t1 t2 g,
+  Lemma refine_01: forall t1 t2 g,
     refine_L0 t1 t2 -> refine_L1 (interp_global t1 g) (interp_global t2 g).
-Proof.
-  intros t1 t2 g H.
-  apply eutt_tt_to_eq_prod, eutt_interp_state; auto.
-Qed.
+  Proof.
+    intros t1 t2 g H.
+    apply eutt_tt_to_eq_prod, eutt_interp_state; auto.
+  Qed.
 
-Lemma refine_12 : forall t1 t2 l,
-    refine_L1 t1 t2 -> refine_L2 (interp_local_stack (handle_local (v:=uvalue)) t1 l) (interp_local_stack (handle_local (v:=uvalue)) t2 l).
-Proof.
-  intros t1 t2 l H.
-  apply eutt_tt_to_eq_prod, eutt_interp_state; auto.
-Qed.
+  Lemma refine_12 : forall t1 t2 l,
+      refine_L1 t1 t2 -> refine_L2 (interp_local_stack t1 l) (interp_local_stack t2 l).
+  Proof.
+    intros t1 t2 l H.
+    apply eutt_tt_to_eq_prod, eutt_interp_state; auto.
+  Qed.
 
-Lemma refine_23 : forall t1 t2 m,
-    refine_L2 t1 t2 -> refine_L3 (interp_memory t1 m) (interp_memory t2 m).
-Proof.
-  intros t1 t2 m H.
-  apply eutt_tt_to_eq_prod, eutt_interp_state; auto.
-Qed.
+  Lemma refine_23 : forall t1 t2 m,
+      refine_L2 t1 t2 -> refine_L3 (interp_memory t1 m) (interp_memory t2 m).
+  Proof.
+    intros t1 t2 m H.
+    apply eutt_tt_to_eq_prod, eutt_interp_state; auto.
+  Qed.
 
-(* Things are different for L4 and L5: we get into the [Prop] monad. *)
-Lemma refine_34 : forall t1 t2,
-    refine_L3 t1 t2 -> refine_L4 (model_undef refine_res3 t1) (model_undef refine_res3 t2).
-Proof.
-  intros t1 t2 H t Ht.
-  exists t; split.
-  - unfold model_undef in *.
-    unfold L3 in *.
-    match goal with |- PropT.interp_prop ?x _ _ _ _ => remember x as h end.
-    eapply interp_prop_Proper_eq in Ht.
-    apply Ht.
-    + apply prod_rel_refl; typeclasses eauto.
-    + apply prod_rel_trans; typeclasses eauto.
-    + assumption.
-    + reflexivity.
-  - reflexivity.
-Qed.
+  (* Things are different for L4 and L5: we get into the [Prop] monad. *)
+  Lemma refine_34 : forall t1 t2,
+      refine_L3 t1 t2 -> refine_L4 (model_undef refine_res3 t1) (model_undef refine_res3 t2).
+  Proof.
+    intros t1 t2 H t Ht.
+    exists t; split.
+    - unfold model_undef in *.
+      unfold L3 in *.
+      match goal with |- PropT.interp_prop ?x _ _ _ _ => remember x as h end.
+      eapply interp_prop_Proper_eq in Ht.
+      apply Ht.
+      + apply prod_rel_refl; typeclasses eauto.
+      + apply prod_rel_trans; typeclasses eauto.
+      + assumption.
+      + reflexivity.
+    - reflexivity.
+  Qed.
 
-Lemma refine_45 : forall Pt1 Pt2,
-    refine_L4 Pt1 Pt2 -> refine_L5 (model_UB refine_res3 Pt1) (model_UB refine_res3 Pt2).
-Proof.
-  intros Pt1 Pt2 HR t2 HM.
-  exists t2; split; [| reflexivity].
-  destruct HM as (t2' & HPt2 & HPT2).
-  apply HR in HPt2; destruct HPt2 as (t1' & HPt1 & HPT1).
-  exists t1'; split; auto.
-  match type of HPT2 with | PropT.interp_prop ?h' ?t _ _ _ => remember h' as h end.
-  eapply interp_prop_Proper_eq with (RR := refine_res3); eauto.
-  - typeclasses eauto.
-  - typeclasses eauto.
-Qed.
+  Lemma refine_45 : forall Pt1 Pt2,
+      refine_L4 Pt1 Pt2 -> refine_L5 (model_UB refine_res3 Pt1) (model_UB refine_res3 Pt2).
+  Proof.
+    intros Pt1 Pt2 HR t2 HM.
+    exists t2; split; [| reflexivity].
+    destruct HM as (t2' & HPt2 & HPT2).
+    apply HR in HPt2; destruct HPt2 as (t1' & HPt1 & HPT1).
+    exists t1'; split; auto.
+    match type of HPT2 with | PropT.interp_prop ?h' ?t _ _ _ => remember h' as h end.
+    eapply interp_prop_Proper_eq with (RR := refine_res3); eauto.
+    - typeclasses eauto.
+    - typeclasses eauto.
+  Qed.
 
 
   Variable ret_typ : dtyp.
@@ -202,113 +204,111 @@ Qed.
 
   Definition denote_vellvm_init := denote_vellvm ret_typ entry args.
   
-(**
+  (**
    In particular, we can therefore define top-level models
    short-circuiting the interpretation early.
- *)
+   *)
 
-Definition model_to_L1  (prog: mcfg dtyp) :=
-  let L0_trace := denote_vellvm_init prog in
-  interp1 L0_trace [].
+  Definition model_to_L1  (prog: mcfg dtyp) :=
+    let L0_trace := denote_vellvm_init prog in
+    ℑs1 L0_trace [].
 
-Definition model_to_L2 (prog: mcfg dtyp) :=
-  let L0_trace := denote_vellvm_init prog in
-  interp2 L0_trace [] ([],[]).
+  Definition model_to_L2 (prog: mcfg dtyp) :=
+    let L0_trace := denote_vellvm_init prog in
+    ℑs2 L0_trace [] ([],[]).
 
-Definition model_to_L3 (prog: mcfg dtyp) :=
-  let L0_trace := denote_vellvm_init prog in
-  interp3 L0_trace [] ([],[]) empty_memory_stack.
+  Definition model_to_L3 (prog: mcfg dtyp) :=
+    let L0_trace := denote_vellvm_init prog in
+    ℑs3 L0_trace [] ([],[]) empty_memory_stack.
 
-Definition model_to_L4 (prog: mcfg dtyp) :=
-  let L0_trace := denote_vellvm_init prog in
-  interp4 (refine_res3) L0_trace [] ([],[]) empty_memory_stack.
+  Definition model_to_L4 (prog: mcfg dtyp) :=
+    let L0_trace := denote_vellvm_init prog in
+    ℑs4 (refine_res3) L0_trace [] ([],[]) empty_memory_stack.
 
-Definition model_to_L5 (prog: mcfg dtyp) :=
-  let L0_trace := denote_vellvm_init prog in
-  interp5 (refine_res3) L0_trace [] ([],[]) empty_memory_stack.
+  Definition model_to_L5 (prog: mcfg dtyp) :=
+    let L0_trace := denote_vellvm_init prog in
+    ℑs5 (refine_res3) L0_trace [] ([],[]) empty_memory_stack.
 
-(**
+  (**
    Which leads to five notion of equivalence of [mcfg]s.
    Note that all reasoning is conducted after conversion to [mcfg] and
    normalization of types.
- *)
-Definition refine_mcfg_L1 (p1 p2: mcfg dtyp): Prop :=
-  R.refine_L1 (model_to_L1 p1) (model_to_L1 p2).
+   *)
+  Definition refine_mcfg_L1 (p1 p2: mcfg dtyp): Prop :=
+    R.refine_L1 (model_to_L1 p1) (model_to_L1 p2).
 
-Definition refine_mcfg_L2 (p1 p2: mcfg dtyp): Prop :=
-  R.refine_L2 (model_to_L2 p1) (model_to_L2 p2).
+  Definition refine_mcfg_L2 (p1 p2: mcfg dtyp): Prop :=
+    R.refine_L2 (model_to_L2 p1) (model_to_L2 p2).
 
-Definition refine_mcfg_L3 (p1 p2: mcfg dtyp): Prop :=
-  R.refine_L3 (model_to_L3 p1) (model_to_L3 p2).
+  Definition refine_mcfg_L3 (p1 p2: mcfg dtyp): Prop :=
+    R.refine_L3 (model_to_L3 p1) (model_to_L3 p2).
 
-Definition refine_mcfg_L4 (p1 p2: mcfg dtyp): Prop :=
-  R.refine_L4 (model_to_L4 p1) (model_to_L4 p2).
+  Definition refine_mcfg_L4 (p1 p2: mcfg dtyp): Prop :=
+    R.refine_L4 (model_to_L4 p1) (model_to_L4 p2).
 
-Definition refine_mcfg  (p1 p2: mcfg dtyp): Prop :=
-  R.refine_L5 (model_to_L5 p1) (model_to_L5 p2).
+  Definition refine_mcfg  (p1 p2: mcfg dtyp): Prop :=
+    R.refine_L5 (model_to_L5 p1) (model_to_L5 p2).
 
-(**
+  (**
    The chain of refinements is monotone, legitimating the ability to
    conduct reasoning before interpretation when suitable.
- *)
-Lemma refine_mcfg_L1_correct: forall p1 p2,
-    refine_mcfg_L1 p1 p2 -> refine_mcfg p1 p2.
-Proof.
-  intros p1 p2 HR.
-  apply refine_45, refine_34, refine_23, refine_12, HR.
-Qed.
+   *)
+  Lemma refine_mcfg_L1_correct: forall p1 p2,
+      refine_mcfg_L1 p1 p2 -> refine_mcfg p1 p2.
+  Proof.
+    intros p1 p2 HR.
+    apply refine_45, refine_34, refine_23, refine_12, HR.
+  Qed.
 
-Lemma refine_mcfg_L2_correct: forall p1 p2,
-    refine_mcfg_L2 p1 p2 -> refine_mcfg p1 p2.
-Proof.
-  intros p1 p2 HR.
-  apply refine_45, refine_34, refine_23, HR.
-Qed.
+  Lemma refine_mcfg_L2_correct: forall p1 p2,
+      refine_mcfg_L2 p1 p2 -> refine_mcfg p1 p2.
+  Proof.
+    intros p1 p2 HR.
+    apply refine_45, refine_34, refine_23, HR.
+  Qed.
 
-Lemma refine_mcfg_L3_correct: forall p1 p2,
-    refine_mcfg_L3 p1 p2 -> refine_mcfg p1 p2.
-Proof.
-  intros p1 p2 HR.
-  apply refine_45, refine_34, HR.
-Qed.
+  Lemma refine_mcfg_L3_correct: forall p1 p2,
+      refine_mcfg_L3 p1 p2 -> refine_mcfg p1 p2.
+  Proof.
+    intros p1 p2 HR.
+    apply refine_45, refine_34, HR.
+  Qed.
 
-Lemma refine_mcfg_L4_correct: forall p1 p2,
-    refine_mcfg_L4 p1 p2 -> refine_mcfg p1 p2.
-Proof.
-  intros p1 p2 HR.
-  apply refine_45, HR.
-Qed.
+  Lemma refine_mcfg_L4_correct: forall p1 p2,
+      refine_mcfg_L4 p1 p2 -> refine_mcfg p1 p2.
+  Proof.
+    intros p1 p2 HR.
+    apply refine_45, HR.
+  Qed.
 
-(* MOVE *)
-Ltac flatten_goal :=
-  match goal with
-  | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
-  end.
+  (* MOVE *)
+  Ltac flatten_goal :=
+    match goal with
+    | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
+    end.
 
-Ltac flatten_hyp h :=
-  match type of h with
-  | context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
-  end.
+  Ltac flatten_hyp h :=
+    match type of h with
+    | context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
+    end.
 
-Ltac flatten_all :=
-  match goal with
-  | h: context[match ?x with | _ => _ end] |- _ => let Heq := fresh "Heq" in destruct x eqn:Heq
-  | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
-  end.
+  Ltac flatten_all :=
+    match goal with
+    | h: context[match ?x with | _ => _ end] |- _ => let Heq := fresh "Heq" in destruct x eqn:Heq
+    | |- context[match ?x with | _ => _ end] => let Heq := fresh "Heq" in destruct x eqn:Heq
+    end.
 
+  Lemma UB_handler_correct: handler_correct UB_handler UB_exec.
+  Proof.
+    unfold UB_handler. unfold UB_exec.
+    unfold handler_correct.
+    intros. auto.
+  Qed.  
 
-Lemma UB_handler_correct: handler_correct UB_handler UB_exec.
-Proof.
-  unfold UB_handler. unfold UB_exec.
-  unfold handler_correct.
-  intros. auto.
-Qed.  
-
-
-Lemma refine_UB
-  : forall E F `{LLVMEvents.FailureE -< E +' F} T TT (HR: Reflexive TT)
-                      (x : _ -> Prop)
-                      (y : itree (E +' LLVMEvents.UBE +' F) T),
+  Lemma refine_UB
+    : forall E F `{LLVMEvents.FailureE -< E +' F} T TT (HR: Reflexive TT)
+        (x : _ -> Prop)
+        (y : itree (E +' LLVMEvents.UBE +' F) T),
       x y -> model_UB TT x (exec_UB y).
   Proof.
     intros E F H T TT HR x y H0.
@@ -322,52 +322,52 @@ Lemma refine_UB
     apply UB_handler_correct.
     unfold handler_correct. intros. reflexivity.
     assumption. reflexivity.
-Qed.
+  Qed.
 
-Lemma Pick_handler_correct :
-  forall E `{LLVMEvents.FailureE -< E} `{LLVMEvents.UBE -< E},
-    handler_correct (@Pick_handler E _ _) concretize_picks.
-Proof.  
-  unfold handler_correct.
-  intros.
-  destruct e.
-  cbn. apply PickD with (res := concretize_uvalue u).
-  - apply Pick.concretize_u_concretize_uvalue.
-  - reflexivity.
-Qed.
+  Lemma Pick_handler_correct :
+    forall E `{LLVMEvents.FailureE -< E} `{LLVMEvents.UBE -< E},
+      handler_correct (@Pick_handler E _ _) concretize_picks.
+  Proof.  
+    unfold handler_correct.
+    intros.
+    destruct e.
+    cbn. apply PickD with (res := concretize_uvalue u).
+    - apply Pick.concretize_u_concretize_uvalue.
+    - reflexivity.
+  Qed.
   
-Lemma refine_undef
-  : forall (E F:Type -> Type) T TT (HR: Reflexive TT)  `{LLVMEvents.UBE -< F} `{LLVMEvents.FailureE -< F}
-                      (x : itree _ T),
+  Lemma refine_undef
+    : forall (E F:Type -> Type) T TT (HR: Reflexive TT)  `{LLVMEvents.UBE -< F} `{LLVMEvents.FailureE -< F}
+        (x : itree _ T),
       model_undef TT x (@exec_undef E F _ _ _ x).
-Proof.
-  intros E F H H0 T TT HR x.
-  cbn in *.
-  unfold model_undef.
-  unfold exec_undef.
-  apply interp_prop_correct_exec.
-  apply case_prop_handler_correct.
-  unfold handler_correct. intros. reflexivity.
-  apply case_prop_handler_correct.
-  apply Pick_handler_correct.
+  Proof.
+    intros E F H H0 T TT HR x.
+    cbn in *.
+    unfold model_undef.
+    unfold exec_undef.
+    apply interp_prop_correct_exec.
+    apply case_prop_handler_correct.
+    unfold handler_correct. intros. reflexivity.
+    apply case_prop_handler_correct.
+    apply Pick_handler_correct.
 
-  unfold handler_correct. intros. reflexivity.
-  assumption. reflexivity.
-Qed.
+    unfold handler_correct. intros. reflexivity.
+    assumption. reflexivity.
+  Qed.
 
-(**
+  (**
    We prove that the interpreter belongs to the model.
- *)
-Theorem interpreter_sound: forall p, model p (interpreter p).
-Proof.
-  intros p.
-  unfold model, model_user.
-  unfold interpreter, interpreter_gen.
-  unfold interp5.
-  unfold interp5_exec.
-  apply refine_UB.  auto.
-  apply refine_undef. auto.
-Qed.
+   *)
+  Theorem interpreter_sound: forall p, model p (interpreter p).
+  Proof.
+    intros p.
+    unfold model, model_gen.
+    unfold interpreter, interpreter_gen.
+    unfold ℑs5.
+    unfold interp_mcfg5_exec.
+    apply refine_UB.  auto.
+    apply refine_undef. auto.
+  Qed.
 
 End REFINEMENT.
 
@@ -379,20 +379,20 @@ End REFINEMENT.
 
 Lemma interp2_bind:
   forall {R S} (t: itree L0 R) (k: R -> itree L0 S) s1 s2,
-    interp2 (ITree.bind t k) s1 s2 ≈
-                 (ITree.bind (interp2 t s1 s2) (fun '(s1',(s2',x)) => interp2 (k x) s2' s1')).
+    ℑs2 (ITree.bind t k) s1 s2 ≈
+            (ITree.bind (ℑs2 t s1 s2) (fun '(s1',(s2',x)) => ℑs2 (k x) s2' s1')).
 Proof.
   intros.
-  unfold interp2.
+  unfold ℑs2.
   rewrite interp_intrinsics_bind, interp_global_bind, interp_local_stack_bind.
   apply eutt_clo_bind with (UU := Logic.eq); [reflexivity | intros ? (? & ? & ?) ->; reflexivity].
 Qed.
 
 Lemma interp2_ret:
   forall (R : Type) s1 s2 (x : R),
-    interp2 (Ret x) s1 s2 ≈ Ret (s2, (s1, x)).
+    ℑs2 (Ret x) s1 s2 ≈ Ret (s2, (s1, x)).
 Proof.
-  intros; unfold interp2.
+  intros; unfold ℑs2.
   rewrite interp_intrinsics_ret, interp_global_ret, interp_local_stack_ret; reflexivity.
 Qed.
 
@@ -401,16 +401,14 @@ Definition interp_cfg {R: Type} (trace: itree instr_E R) g l m :=
   let L1_trace       := interp_global uvalue_trace g in
   let L2_trace       := interp_local L1_trace l in
   let L3_trace       := interp_memory L2_trace m in
-  let L4_trace       := model_undef Logic.eq L3_trace in
-  let L5_trace       := model_UB Logic.eq L4_trace in
+  let L4_trace       := model_undef eq L3_trace in
+  let L5_trace       := model_UB eq L4_trace in
   L5_trace.
 
 Definition model_to_L5_cfg (prog: cfg dtyp) :=
-  let trace := D.denote_cfg prog in
+  let trace := denote_cfg prog in
   interp_cfg trace [] [] empty_memory_stack.
 
 Definition refine_cfg_ret: relation (PropT L5 (memory_stack * (local_env * (global_env * uvalue)))) :=
   fun ts ts' => forall t, ts t -> exists t', ts' t' /\ eutt  (TT × (TT × (TT × refine_uvalue))) t t'.
 
-(* Definition refine_cfg  (p1 p2: cfg dtyp): Prop := *)
-(*   refine_cfg_ret (model_to_L5_cfg p1) (model_to_L5_cfg p2). *)
