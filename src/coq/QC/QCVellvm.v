d30d776f2256b16674e83d874c311eea77db6479
Calvin Beck <hobbes@ualberta.ca>
2021-03-01 16:22:21 -0500

    Make QC quieter.

--- src/coq/QC/QCVellvm.v
+++ src/coq/QC/QCVellvm.v
@@ -52,7 +52,7 @@ Definition run_llc (prog : list (toplevel_entity typ (block typ * list (block ty
 
 Definition vellvm_agrees_with_clang (prog : list (toplevel_entity typ (block typ * list (block typ)))) : Checker
   := 
-    collect (show prog)
+    (* collect (show prog) *)
             match interpret prog, run_llc prog with
             | MlOk (UVALUE_I8 x), UVALUE_I8 y => checker (equ x y)
             | _, _ => checker true
