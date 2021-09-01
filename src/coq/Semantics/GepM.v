6d1f086c7d4b05b997dc6b7e91f62a0c5de1a8d4
Calvin Beck <hobbes@ualberta.ca>
2021-09-01 09:48:50 -0400

    Add GetElementPtr module.

---
+++ src/coq/Semantics/GepM.v
@@ -0,0 +1,25 @@
+Require Import ZArith String.
+
+From ExtLib Require Import
+     Structures.Functor.
+
+From Vellvm Require Import
+     DynamicTypes
+     LLVMEvents
+     Utils.Error.
+
+Module Type GEPM(Addr:MemoryAddress.ADDRESS)(LLVMEvents:LLVM_INTERACTIONS(Addr)).
+  Import LLVMEvents.
+  Import DV.
+
+  (* TODO: should this be here? *)
+  Parameter handle_gep_h : dtyp -> Z -> list dvalue -> err Z.
+
+  Parameter handle_gep_addr : dtyp -> Addr.addr -> list dvalue -> err Addr.addr.
+
+  Definition handle_gep (t:dtyp) (dv:dvalue) (vs:list dvalue) : err dvalue :=
+    match dv with
+    | DVALUE_Addr a => fmap DVALUE_Addr (handle_gep_addr t a vs)
+    | _ => failwith "non-address"
+    end.
+End GEPM.
