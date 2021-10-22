34ac44bd6c53996cda134f0250953752d86d95d9
Calvin Beck <hobbes@ualberta.ca>
2021-10-22 11:19:18 -0400

    Get interpreter compiling and tests passing 🎉.

--- src/ml/libvellvm/interpreter.ml
+++ src/ml/libvellvm/interpreter.ml
@@ -8,7 +8,7 @@
  *   3 of the License, or (at your option) any later version.                 *
  ---------------------------------------------------------------------------- *)
 
-open Handlers.MemTheory.Mem
+open Handlers.MEMORY_THEORY.Mem
 open Handlers.Local
 open Handlers.Stack
 open Handlers.Global
