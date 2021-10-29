87fec4b53916d023ab2dea6c94594b1e6fe1315e
Calvin Beck <hobbes@ualberta.ca>
2021-10-29 11:03:11 -0400

    Change Lang to module type...

--- src/coq/Semantics/Lang.v
+++ src/coq/Semantics/Lang.v
@@ -1,8 +1,10 @@
 From Vellvm Require Import
      Semantics.MemoryAddress
      Semantics.Memory.Sizeof
+     Semantics.Memory.MemBytes
      Semantics.GepM
      Semantics.LLVMEvents
+     Semantics.Denotation
 
      Handlers.Global
      Handlers.Local
@@ -13,7 +15,37 @@ From Vellvm Require Import
      Handlers.FiniteMemory
      Handlers.FiniteMemoryTheory.
 
-Module Lang (ADDR : ADDRESS) (IP : INTPTR) (SIZEOF : Sizeof) (PTOI : PTOI ADDR) (PROV : PROVENANCE ADDR) (ITOP : ITOP ADDR PROV).
+Module Type Lang (ADDR : ADDRESS) (IP : INTPTR) (SIZEOF : Sizeof) (PTOI : PTOI ADDR) (PROV : PROVENANCE ADDR) (ITOP : ITOP ADDR PROV).
+  (* Events *)
+  Declare Module Events : LLVMEvents.LLVM_INTERACTIONS ADDR IP SIZEOF.
+
+  (* Handlers *)
+  Module Global     := Global.Make ADDR IP SIZEOF Events.
+  Module Local      := Local.Make ADDR IP SIZEOF Events.
+  Module Stack      := Stack.Make ADDR IP SIZEOF Events.
+  Module Intrinsics := Intrinsics.Make ADDR IP SIZEOF Events.
+
+  (* Memory *)
+  Module GEP  := GepM.Make ADDR IP SIZEOF Events PTOI PROV ITOP.
+  Declare Module Byte : ByteImpl ADDR IP SIZEOF Events.
+
+  (* Pick handler (depends on memory) *)
+  Module Pick := Pick.Make ADDR IP SIZEOF Events PTOI PROV ITOP GEP Byte.
+
+  Module MEM  := FiniteMemory.Make ADDR IP SIZEOF Events PTOI PROV ITOP GEP Byte.
+  Declare Module MEMORY_THEORY : FiniteMemoryTheory.MEMORY_THEORY ADDR IP SIZEOF Events PTOI PROV ITOP GEP Byte.
+
+  (* Serialization *)
+  Module SER := Serialization.Make ADDR IP SIZEOF Events PTOI PROV ITOP GEP Byte.
+
+  (* Denotation *)
+  Module D := Denotation ADDR IP SIZEOF Events PTOI PROV ITOP GEP Byte.
+
+  Export Events Events.DV Global Local Stack Pick Intrinsics
+         MEM MEMORY_THEORY UndefinedBehaviour SER D.
+End Lang.
+
+Module Make (ADDR : ADDRESS) (IP : INTPTR) (SIZEOF : Sizeof) (PTOI : PTOI ADDR) (PROV : PROVENANCE ADDR) (ITOP : ITOP ADDR PROV) <: Lang ADDR IP SIZEOF PTOI PROV ITOP.
   (* Events *)
   Module Events := LLVMEvents.Make ADDR IP SIZEOF.
 
@@ -36,6 +68,15 @@ Module Lang (ADDR : ADDRESS) (IP : INTPTR) (SIZEOF : Sizeof) (PTOI : PTOI ADDR) 
   (* Serialization *)
   Module SER := Serialization.Make ADDR IP SIZEOF Events PTOI PROV ITOP GEP Byte.
 
+  (* Denotation *)
+  Module D := Denotation ADDR IP SIZEOF Events PTOI PROV ITOP GEP Byte.
+
   Export Events Events.DV Global Local Stack Pick Intrinsics
-         MEM MEMORY_THEORY UndefinedBehaviour.
-End Lang.
+         MEM MEMORY_THEORY UndefinedBehaviour SER D.
+End Make.
+
+(* LLVM Instance with big integers for intptr *)
+Module LLVMLang := Make FiniteMemory.Addr FiniteMemory.BigIP FiniteMemory.FinSizeof FiniteMemory.FinPTOI FiniteMemory.FinPROV FiniteMemory.FinITOP.
+
+(* LLVM Instance with 64-bit intptr values *)
+Module LLVMLang64 := Make FiniteMemory.Addr FiniteMemory.IP64Bit FiniteMemory.FinSizeof FiniteMemory.FinPTOI FiniteMemory.FinPROV FiniteMemory.FinITOP.
