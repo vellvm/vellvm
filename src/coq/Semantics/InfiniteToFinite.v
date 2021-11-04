f6da6bf5fd12af6c9486f0a39d9edf4c14505cde
Calvin Beck <hobbes@ualberta.ca>
2021-11-04 08:38:06 -0400

    Update Makefile.

--- src/coq/Semantics/InfiniteToFinite.v
+++ src/coq/Semantics/InfiniteToFinite.v
@@ -5,8 +5,9 @@ From Vellvm Require Import
      Semantics.MemoryAddress
      Semantics.Memory.Sizeof
      Semantics.Lang
-     Semantics.InterpretationStack.
+     Semantics.InterpretationStack
+     Semantics.TopLevel.
 
-Module Type InfiniteToFinite (LLVM1 : InterpreterStack) (LLVM2 : InterpreterStack).
+Module InfiniteToFinite (IS1 : InterpreterStack) (IS2 : InterpreterStack) (LLVM1 : LLVMTopLevel IS1) (LLVM2 : LLVMTopLevel IS2).
   (* Parameter oom_refine : LLVM1. *)
 End InfiniteToFinite.
