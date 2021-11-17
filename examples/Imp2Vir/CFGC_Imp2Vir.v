bd21843c2309ea5a5a3b9902f1dff1e3f0988a24
Bastien Rousseau <bastien.rousseau@ens-rennes.fr>
2021-11-17 11:36:04 +0100

    Compiler IMP to VIR using OptionT + fix sketch theorems WF/correctness compiler

--- examples/Imp2Vir/CFGC_Imp2Vir.v
+++ examples/Imp2Vir/CFGC_Imp2Vir.v
@@ -23,11 +23,11 @@ Section Imp2Vir.
 Notation ocfg := (ocfg typ).
 
 
-Variable (name : nat -> block_id).
-Hypothesis (nameInj_eq : forall n n', n = n' -> name n = name n' ).
-Hypothesis (nameInj_neq : forall n n', n <> n' -> name n <> name n' ).
-(* Definition mk_anon (n : nat) := Anon (Z.of_nat n). *)
-(* Definition name := mk_anon. *)
+(* Variable (name : nat -> block_id). *)
+(* Hypothesis (nameInj_eq : forall n n', n = n' -> name n = name n' ). *)
+(* Hypothesis (nameInj_neq : forall n n', n <> n' -> name n <> name n' ). *)
+Definition mk_anon (n : nat) := Anon (Z.of_nat n).
+Definition name := mk_anon.
 
 
 
@@ -35,114 +35,93 @@ Hypothesis (nameInj_neq : forall n n', n <> n' -> name n <> name n' ).
 Record FST :=
   mk_FST
     {
-      counter_reg : Z ;
       counter_bid : nat ;
-      env : StringMap.t int
     }.
 
-Definition fresh_init : FST := mk_FST 0%Z 0 (StringMap.empty int).
+Definition fresh_init : FST := mk_FST 0.
 
 Definition fresh : Type -> Type := fun X => FST -> (FST * X).
 
 #[global] Instance freshM : Monad fresh :=
  {|
-  ret := fun _ x s => (s,x);
-  bind := fun _ _ c k s => let '(s',x) := c s in k x s'
+   ret := fun _ x s => (s,x);
+   bind := fun _ _ c k s => let '(s',x) := c s in k x s'
  |}.
 
-Definition freshLab : fresh block_id :=
-  fun '(mk_FST reg bid env) => (mk_FST reg (S bid) env , name bid).
+Definition freshLabel : fresh block_id :=
+  fun '(mk_FST bid) => (mk_FST (S bid) , name bid).
 
-Definition getReg : fresh Z :=
-  fun '(mk_FST reg bid env) => (mk_FST reg bid env , reg).
+(* TODO it is probable already defined somewhere in Vellvm *)
+Definition OptionT (m : Type -> Type) : Type -> Type :=
+  fun x => m (option x).
 
-Definition setReg (nreg : Z) : fresh _ :=
-  fun '(mk_FST reg bid env) => (mk_FST nreg bid env , tt).
+#[global] Instance optionT m `{Monad m} : Monad (OptionT m) :=
+  {|
+    ret := fun _ x => ret (Some x) ;
+    bind :=  fun x y (c : m (option x)) (k : x -> m (option y)) =>
+               t <- c ;;
+               match t with
+               | None => ret None
+               | Some v => k v
+               end
+  |}.
 
-Definition getEnv : fresh (StringMap.t int) :=
-  fun '(mk_FST reg bid env) => (mk_FST reg bid env , env).
 
-Definition setEnv (nenv : StringMap.t int) : fresh _ :=
-  fun '(mk_FST reg bid env) => (mk_FST reg bid nenv , tt).
-
-Fixpoint compile_imp' (s : stmt) : fresh (option (ocfg * block_id * block_id)).
-  refine (
-      match s with
-      | Skip => _
-      | Assign x e => _
-      | Seq l r => _
-      | If e l r => _
-      | While e b => _
-      end).
-  (* Assign *)
-  refine (ret None).
-  (* Seq *)
-  refine (ret None).
-  (* If *)
-  refine (ret None).
-  (* While *)
-  refine (ret None).
-  (* Skip *)
-  refine (
-      input <- freshLab ;;
-      output <- freshLab ;;
-      let g := cfg_block [] input output in
-      ret (Some (g, input, output))
-    ).
-Admitted.
-
-Fixpoint compile_imp (next_reg : int) (next_lab : nat) (s : stmt) (env: StringMap.t int)
-  :  option (int * nat * (StringMap.t int) * (ocfg * block_id * block_id))
-  :=
+Fixpoint compile_imp (next_reg : int) (s : stmt) (env: StringMap.t int) :
+  OptionT fresh ( int * (StringMap.t int) * ocfg * block_id * block_id) :=
   match s with
   | Skip =>
-      let input := name next_lab in
-      let output := name (next_lab+1) in
-      let g := cfg_block [] input output in
-      Some (next_reg, (next_lab+2)%nat, env, (g, input, output))
-
+      (input <- freshLabel ;;
+       output <- freshLabel ;;
+       let g := cfg_block [] input output in
+       ret (Some (next_reg, env, g, input, output)))
   | Assign x e =>
-      '(next_reg, env, c) <- compile_assign next_reg x e env ;;
-      let input := name next_lab in
-      let output := name (next_lab+1) in
-      let g := cfg_block c input output in
-      ret (next_reg, (next_lab+2)%nat, env , (cfg_block c input output, input , output))
-
+      (match compile_assign next_reg x e env with
+       | None => (ret None)
+       | Some (next_reg, env, c) =>
+           input <- freshLabel ;;
+           output <- freshLabel ;;
+           let g := cfg_block c input output in
+           ret (Some (next_reg, env, g, input, output))
+       end)
   | Seq l r =>
-      '(next_reg, next_lab, env, ir1) <- compile_imp next_reg next_lab l env;;
-      '(next_reg, next_lab, env, ir2) <- compile_imp next_reg next_lab r env;;
-      let '(g1, in1, out1) := ir1 in
-      let '(g2, in2, out2) := ir2 in
-      ret (next_reg, next_lab, env, ((cfg_seq g1 g2 out1 in2), in1, out2))
-
+      ( '(next_reg1, env1, g1, in1, out1) <- compile_imp next_reg l env;;
+        '(next_reg2, env2, g2, in2, out2) <- compile_imp next_reg1 r env1;;
+        ret (next_reg2,env2,(cfg_seq g1 g2 out1 in2), in1, out2))
   | If e l r =>
-    '(cond_reg, expr_code) <- compile_cond next_reg e env;;
-    '(next_reg, next_lab, _, irT) <- compile_imp (cond_reg + 1) next_lab l env;;
-    '(next_reg, next_lab, _, irF) <- compile_imp next_reg next_lab r env;;
-    let '(gT, inT, outT) := irT in
-    let '(gF, inF, outF) := irF in
-    let input := name next_lab in
-    let out0 := name (next_lab+1) in
-    let output := name (next_lab+2) in
-    let g0 := cfg_block expr_code input out0 in
-    let g := cfg_branch (texp_i1 cond_reg) gT gF out0 inT inF in
-    let g := cfg_join g output outT outF in
-      ret (next_reg, (next_lab+2)%nat, env, (g, input, output))
-
+      (match compile_cond next_reg e env with
+       | None => ret None
+       | Some (cond_reg, expr_code) =>
+           '(next_reg1, env1, gT, inT, outT) <- compile_imp (cond_reg + 1) l env;;
+           '(next_reg2, env2, gF, inF, outF) <- compile_imp next_reg1 r env1;;
+           input <- freshLabel ;;
+           input_body <- freshLabel ;;
+           out_code <- freshLabel ;;
+           output <- freshLabel ;;
+           let g_code := cfg_block expr_code input out_code in
+           let g_body := cfg_branch (texp_i1 cond_reg) gT gF input_body inT inF in
+           let g_body := cfg_join g_body output outT outF in
+           let g := cfg_seq g_code g_body out_code input_body in
+           ret (Some (next_reg2, env2, g, input, output))
+       end)
   | While e b =>
-    '(cond_reg, expr_code) <- compile_cond next_reg e env;;
-    '(next_reg, next_lab, _, irB) <- compile_imp (cond_reg + 1) next_lab b env;;
-    let '(gB, inB, outB) := irB in
-    let input := name next_lab in
-    let out0 := name (next_lab+1) in
-    let output := name (next_lab+2) in
-    let g := cfg_while_loop expr_code (texp_i1 cond_reg) gB input inB output outB in
-    ret (next_reg, (next_lab+2)%nat, env, (g, input, output))
+      (match compile_cond next_reg e env with
+       | None => ret None
+       | Some (cond_reg, expr_code) =>
+           '(next_reg1, env1, gB, inB, outB) <- compile_imp (cond_reg + 1) b env ;;
+           input <- freshLabel ;;
+           out0 <- freshLabel ;;
+           output <- freshLabel ;;
+           let g := cfg_while_loop expr_code (texp_i1 cond_reg) gB input inB output outB in
+           ret (Some (next_reg1, env1, g, input, output))
+       end)
   end.
 
 Definition compile (s : stmt) :=
-  '(_, _, (g, _, _)) <- compile_imp 0 0 s (StringMap.empty int);;
-  ret g.
+  snd (
+      ('(_, _, g, _, _) <- compile_imp 0 s (StringMap.empty int);;
+       (ret g))
+        fresh_init).
 
 
 (** Examples *)
@@ -154,20 +133,20 @@ Definition nested_if_ir := (compile (nested_if "a" "b" 42 43)).
 Definition nested_if2_ir := (compile (nested_if2 "a" "b" 42 43)).
 
 Compute infinite_loop_ir.
-Compute seq_ir.
+Compute fact_ir.
 Compute if_ir.
+Compute seq_ir.
 Compute nested_if_ir.
 Compute nested_if2_ir.
-Compute fact_ir.
 
 
 
 (** WF compiler *)
 From Vellvm Require Import Utils.Tactics.
 
-Theorem wf_compiler_aux : forall s reg label env input output cfg nreg nlabel nenv ,
-  compile_imp reg label s env =
-      Some (nreg, nlabel, nenv, (cfg,input,output)) -> wf_ocfg_bid cfg.
+Theorem wf_compiler_aux : forall s reg env σ σ' input output cfg nreg nenv ,
+  (compile_imp reg s env) σ =
+      (σ', Some (nreg, nenv, cfg, input, output)) -> wf_ocfg_bid cfg.
 Proof.
   intros s.
   induction s ; intros.
@@ -175,28 +154,30 @@ Proof.
   cbn in H.
   repeat flatten_all ;
   inv H ; inv Heq ; inv Heq4 ;
-  apply wf_cfg_block.
+  apply wf_bid_cfg_block.
   - (* Seq *)
     cbn in H; repeat flatten_all ; try discriminate ; inv H.
     apply IHs1 in Heq.
-    apply IHs2 in Heq3.
-    apply wf_cfg_seq ; try assumption.
+    apply IHs2 in Heq5.
+    apply wf_bid_cfg_seq ; try assumption.
     admit. admit. admit. (* I need an invariant on the compiler *)
   - (* If *)
     simpl in H. repeat (flatten_hyp H) ; try discriminate.
     inv H.
-    apply wf_cfg_join ; try assumption.
+    apply wf_bid_cfg_seq ; try assumption.
+    admit. 2 :{ admit. } 2 :{ admit. } 2 :{ admit. }
+    apply wf_bid_cfg_join ; try assumption.
     admit. admit. admit. (* I need an invariant on the compiler *)
-    apply IHs1 in Heq1. apply IHs2 in Heq5.
-    apply wf_cfg_branch ; try assumption.
+    apply IHs1 in Heq1. apply IHs2 in Heq7.
+    apply wf_bid_cfg_branch ; try assumption.
     admit. admit. admit. (* I need an invariant on the compiler *)
   - (* While *)
     simpl in H; repeat flatten_all ; try discriminate ; inv H.
     apply IHs in Heq1.
-    apply wf_cfg_while_loop ; try assumption.
+    apply wf_bid_cfg_while_loop ; try assumption.
     admit. admit. admit. (* I need an invariant on the compiler *)
   - (* Skip *)
-    cbn in H ; inv H ; apply wf_cfg_block.
+    cbn in H ; inv H ; repeat flatten_all ; inv H1 ; apply wf_bid_cfg_block.
 Admitted.
 
 
@@ -234,16 +215,16 @@ Definition Rimpvir
   Prop :=
   Rmem vmap (fst env1) (fst (snd env2)) (fst env2).
 
-Theorem compile_correct : forall (next_reg : int) (next_label : nat) env (p : stmt)
-                            (next_reg' : int) (next_label' : nat)
+Theorem compile_correct : forall (next_reg : int) (σ : FST) env (p : stmt)
+                            (next_reg' : int) (σ' : FST)
                             env' (o : ocfg) (input output : block_id)
                             mem from to genv lenv vmem,
 
   (* The environments of Imp and Vir are related *)
   Rmem env mem lenv vmem ->
   (* The compilation of p with env produce a new env' and an ir *)
-  compile_imp next_reg next_label p env = Some(next_reg', next_label', env', (o,input,output)) ->
-
+  (compile_imp next_reg p env) σ =
+      (σ', Some (next_reg', env', o, input, output))  ->
   eutt (Rimpvir env')
        (interp_imp (denote_imp p) mem)
        (interp_cfg3 (denote_cfg o from to) genv lenv vmem).
@@ -254,10 +235,10 @@ Proof.
     simpl in *.
     repeat (flatten_all) ; [| discriminate H0].
     inv H0.
-    assert ( I_to : to = (name next_label) ) by admit. (* I need to introduce an invariant here *)
-    rewrite I_to ;
+    assert ( I_to : to = input ) by admit. (* I need to introduce an invariant here *)
+    rewrite I_to ; clear I_to.
     rewrite denote_cfg_block.
-    2: {apply nameInj_neq ; lia. }
+    2: {  admit. }
     rewrite interp_imp_bind.
     unfold compile_assign in Heq.
     repeat (flatten_all).
@@ -270,22 +251,31 @@ Proof.
     admit.
     admit.
     admit.
-  - (* Seq *) admit.
+  - (* Seq *)
+    simpl in *.
+    repeat (flatten_all) ; try discriminate.
+    inv H0.
+    rewrite denote_cfg_seq.
+    admit.
+    admit.
+    admit.
   - (* If *) admit.
   - (* While *) admit.
   - (* Skip *)
     simpl in *.
+    repeat flatten_all.
     inv H0.
-    assert ( I_to : to = (name next_label) ) by admit. (* I need to introduce an invariant here *)
-    rewrite I_to ;
+    assert ( I_to : to = input ) by admit.
+    (* I probably need to introduce an invariant here *)
+    rewrite I_to ; clear I_to.
     rewrite denote_cfg_block.
-    2: { apply nameInj_neq ; lia. }
+    2: { admit. }
     rewrite interp_imp_ret.
     rewrite denote_code_nil.
-      setoid_rewrite bind_ret_l.
-      simpl.
-      rewrite interp_cfg3_ret.
-      apply eutt_Ret.
+    setoid_rewrite bind_ret_l.
+    simpl.
+    rewrite interp_cfg3_ret.
+    apply eutt_Ret.
     constructor.
       * intros.
         simpl in H0. apply H,H0.
