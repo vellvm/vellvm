10fbcf75aeeee8d50d506e02e2a6de7d953ec30c
Yannick Zakowski <yannick.zakowski@inria.fr>
2022-02-06 16:12:55 +0100

    fixed compilation of rellog

--- src/coq/Utils/RelLog.v
+++ src/coq/Utils/RelLog.v
@@ -5,6 +5,7 @@ From ITree Require Import
      Eq.Eq.
 
 Import ITreeNotations.
+Open Scope itree.
 (* end hide *)
 
 Section Pure.
@@ -18,11 +19,11 @@ Section Pure.
     forall {A1 A2 B1 B2}
       (R: rel A1 B1) (Q: rel A2 B2)
       (p1: prog A1) (p2: prog A2) (q1: prog B1) (q2: prog B2),
-      
+
       (p1,q1){{R}} ->
       (p2,q2){{Q}} ->
       (* --------------- *)
-      (p1;;p2,q1;;q2){{Q}}. 
+      (p1;;p2,q1;;q2){{Q}}.
 
   Proof.
     intros.
@@ -33,18 +34,18 @@ Section Pure.
     forall {A1 A2 B1 B2}
       (R: rel A1 B1) (Q: rel A2 B2)
       (p1: prog A1) (p2: A1 -> prog A2) (q1: prog B1) (q2: B1 -> prog B2),
-      
+
       (p1,q1){{R}} ->
       (forall a b, R a b -> (p2 a,q2 b){{Q}}) ->
       (* ----------------------------- *)
-      (x <- p1;;p2 x, x <- q1;;q2 x){{Q}}. 
+      (x <- p1;;p2 x, x <- q1;;q2 x){{Q}}.
 
   Proof.
     intros.
     eapply eutt_clo_bind; eauto.
   Qed.
 
-  
+
 End Pure.
 
 Section State.
@@ -56,7 +57,7 @@ Section State.
   Notation rel A B := (A -> B -> Prop).
   Notation "'{{' P '}}' '(' p ',' q ')' '{{' Q '}}'" :=
     (forall s1 s2, P s1 s2 -> eutt Q (p (fst s1)) (q (fst s2))).
-  Notation prog σ := (stateT σ (itree E)). 
+  Notation prog σ := (stateT σ (itree E)).
 
   Definition lift_state_rel (R: rel σ1 σ2): rel (σ1 * unit) (σ2 * unit) :=
     fun '(s1,_) '(s2,_) => R s1 s2.
@@ -88,7 +89,7 @@ Section State.
 
   Definition REL {A B} (Q : rel (σ1 * A) (σ2 * B)) (a:A) (b:B) : rel (σ1 * A) (σ2 * B) :=
     fun '(s1, a') '(s2, b') => a = a' /\ b = b' /\ Q (s1, a) (s2, b).
-  
+
   Lemma consequence_state_bind:
     forall {A1 A2 B1 B2}
       (R: rel σ1 σ2)
@@ -96,7 +97,7 @@ Section State.
       (S: rel (σ1 * A2) (σ2 * B2))
       (p1: prog σ1 A1) (p2: A1 -> prog σ1 A2) (q1: prog σ2 B1) (q2: B1 -> prog σ2 B2),
       {{↑R}}(p1,q1){{Q}} ->
-      (forall a b, {{REL Q a b}}(p2 a, q2 b){{S}}) -> 
+      (forall a b, {{REL Q a b}}(p2 a, q2 b){{S}}) ->
       {{↑R}}
         (fun s1 => '(s2,a1) <- p1 s1;; p2 a1 s2,
                 fun s1 => '(s2,b1) <- q1 s1;; q2 b1 s2)
@@ -114,7 +115,7 @@ Section State.
   (* Alternative solution for the general bind case. Looks needlessly more complex at first glance *)
   Notation "'[[' P ']]' '(' p ',' q ')' '[[' Q ']]'" :=
     (forall s1 s2, P s1 s2 -> eutt Q (p (snd s1) (fst s1)) (q (snd s2) (fst s2))).
-  
+
   Lemma consequence_state_bind':
     forall {A1 A2 B1 B2}
       (R: rel σ1 σ2)
@@ -138,4 +139,3 @@ Section State.
   Qed.
 
 End State.
-
