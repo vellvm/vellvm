7732c193f8e2b4fa87d2f4fab1a80476178ca527
Calvin Beck <hobbes@ualberta.ca>
2022-03-19 10:48:43 -0400

    Update nix flake so Coq will build on M1 Macs.
    The nixos-21.11 release of nixpkgs doesn't have an update to the
    ocaml-cairo2 library that is necessary for building it on M1
    Macs. This library is needed by CoqIDE.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Vellvm, a formal specification and interpreter for LLVM";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -13,9 +13,28 @@
         lib = pkgs.lib;
         coq = pkgs.coq;
 
+        coqPkgs = pkgs.coqPackages.overrideScope'
+          (self: super:
+            { ITree = super.ITree.overrideAttrs
+              (s : { version = "DeepSpec:78bf12c326e52f4d64bf03c43b937f7397559cbf";
+                     src = fetchTarball {
+                       url = "https://github.com/DeepSpec/InteractionTrees/archive/78bf12c326e52f4d64bf03c43b937f7397559cbf.zip";
+                       sha256 = "0sfrrkrhr51qnc6rpxr2mq4fvhvwbgp453r5q4nafba32lhfima2";
+                     };
+                   });
+
+              coq-ext-lib = super.coq-ext-lib.overrideAttrs
+              (s : { version = "0.11.3";
+                     src = fetchTarball {
+                       url = "https://github.com/coq-community/coq-ext-lib/archive/refs/tags/v0.11.3.zip";
+                       sha256 = "1w99nzpk72lffxis97k235axss5lmzhy5z3lga2i0si95mbpil42";
+                     };
+                   });
+            });
+
         version = "vellvm:master";
       in {
         defaultPackage =
-          (pkgs.callPackage ./release.nix (coq.ocamlPackages // pkgs.coqPackages // { inherit coq version; })).vellvm;
+          (pkgs.callPackage ./release.nix (coq.ocamlPackages // coqPkgs // { inherit coq version; })).vellvm;
       });
 }
