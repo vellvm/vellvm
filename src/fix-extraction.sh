bc772c05f289002f8cf01f7f2662ac1928552072
Steve Zdancewic <stevez@cis.upenn.edu>
2022-05-31 15:01:02 -0400

    saner fix extraction code, leaves no backup files

--- src/fix-extraction.sh
+++ src/fix-extraction.sh
@@ -1,30 +1,46 @@
 #!/bin/sh
 EXTRACT_DIR=ml/extracted
 
-# BigIntptr mismatches.
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParamsBigIntptr.Events.DV/LP.Events.DV/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParamsBigIntptr.Events.DV/LP.Events.DV/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParamsBigIntptr.ADDR.addr/LP.ADDR.addr/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParamsBigIntptr.ADDR.addr/LP.ADDR.addr/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParamsBigIntptr.IP/LP.IP/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParamsBigIntptr.IP/LP.IP/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParamsBigIntptr.PROV/LP.PROV/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParamsBigIntptr.PROV/LP.PROV/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParamsBigIntptr.Events/LP.Events/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParamsBigIntptr.Events/LP.Events/g" {} \;
+#Files in which to rewrite
+FILENAMES=("InterpretationStack.ml" "InterpretationStack.mli" "TopLevel.ml" "TopLevel.mli")
 
-# 64BitIntptr mismatches
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParams64BitIntptr.Events.DV/LP.Events.DV/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParams64BitIntptr.Events.DV/LP.Events.DV/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParams64BitIntptr.ADDR.addr/LP.ADDR.addr/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParams64BitIntptr.ADDR.addr/LP.ADDR.addr/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParams64BitIntptr.IP/LP.IP/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParams64BitIntptr.IP/LP.IP/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParams64BitIntptr.PROV/LP.PROV/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParams64BitIntptr.PROV/LP.PROV/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/MemoryModelImplementation.LLVMParams64BitIntptr.Events/LP.Events/g" {} \;
-find $EXTRACT_DIR -type f \( -name "InterpretationStack*" -or -name "TopLevel*" \) -exec sed -i -e "s/LLVMParams64BitIntptr.Events/LP.Events/g" {} \;
+function replace () {
+    sed -i.bak -e $1 $EXTRACT_DIR/$2
+    rm -rf $EXTRACT_DIR/*.bak
+}
+
+for f in "${FILENAMES[@]}"
+do
+    # BigIntptr mismatches
+    replace "s/MemoryModelImplementation.LLVMParamsBigIntptr.Events.DV/LP.Events.DV/g" $f
+    replace "s/LLVMParamsBigIntptr.Events.DV/LP.Events.DV/g" $f			      
+    replace "s/MemoryModelImplementation.LLVMParamsBigIntptr.ADDR.addr/LP.ADDR.addr/g" $f 
+    replace "s/LLVMParamsBigIntptr.ADDR.addr/LP.ADDR.addr/g" $f			      
+    replace "s/MemoryModelImplementation.LLVMParamsBigIntptr.IP/LP.IP/g" $f		      
+    replace "s/LLVMParamsBigIntptr.IP/LP.IP/g" $f					          
+    replace "s/MemoryModelImplementation.LLVMParamsBigIntptr.PROV/LP.PROV/g" $f	          
+    replace "s/LLVMParamsBigIntptr.PROV/LP.PROV/g" $f				          
+    replace "s/MemoryModelImplementation.LLVMParamsBigIntptr.Events/LP.Events/g" $f
+    replace "s/LLVMParamsBigIntptr.Events/LP.Events/g" $f                                 
+
+    # 64BitIntptr mismatches
+    replace "s/MemoryModelImplementation.LLVMParams64BitIntptr.Events.DV/LP.Events.DV/g" $f   
+    replace "s/LLVMParams64BitIntptr.Events.DV/LP.Events.DV/g" $f				    
+    replace "s/MemoryModelImplementation.LLVMParams64BitIntptr.ADDR.addr/LP.ADDR.addr/g" $f   
+    replace "s/LLVMParams64BitIntptr.ADDR.addr/LP.ADDR.addr/g" $f				    
+    replace "s/MemoryModelImplementation.LLVMParams64BitIntptr.IP/LP.IP/g" $f		    
+    replace "s/LLVMParams64BitIntptr.IP/LP.IP/g" $f					    
+    replace "s/MemoryModelImplementation.LLVMParams64BitIntptr.PROV/LP.PROV/g" $f		    
+    replace "s/LLVMParams64BitIntptr.PROV/LP.PROV/g" $f					    
+    replace "s/MemoryModelImplementation.LLVMParams64BitIntptr.Events/LP.Events/g" $f	    
+    replace "s/LLVMParams64BitIntptr.Events/LP.Events/g" $f                                   
+done
 
 # Polymorphism issue
-find $EXTRACT_DIR -type f -exec sed -i -e "s/('a1, __) coq_MemPropT coq_Monad/(__, __) coq_MemPropT coq_Monad/g" {} \;
-find $EXTRACT_DIR -type f -exec sed -i -e "s/('a1, ('a1, __) coq_MemPropT) coq_MonadMemState/(__, (__, __) coq_MemPropT) coq_MonadMemState/g" {} \;
+find $EXTRACT_DIR -type f -exec sed -i.bak -e "s/('a1, __) coq_MemPropT coq_Monad/(__, __) coq_MemPropT coq_Monad/g" {} \;
+rm -rf $EXTRACT_DIR/*.bak
+find $EXTRACT_DIR -type f -exec sed -i.bak -e "s/('a1, ('a1, __) coq_MemPropT) coq_MonadMemState/(__, (__, __) coq_MemPropT) coq_MonadMemState/g" {} \;
+rm -rf $EXTRACT_DIR/*.bak
+
+
+
