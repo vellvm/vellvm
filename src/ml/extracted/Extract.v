c3317ff26a5383dd8a0883594f4c41395c9f980f
Steve Zdancewic <stevez@cis.upenn.edu>
2022-06-17 10:44:33 -0400

    fix extraction

--- src/ml/extracted/Extract.v
+++ src/ml/extracted/Extract.v
@@ -41,12 +41,10 @@ Extract Inductive ascii => char
 "(* If this appears, you're using Ascii internals. Please don't *) (fun (b0,b1,b2,b3,b4,b5,b6,b7) -> let f b i = if b then 1 lsl i else 0 in Char.chr (f b0 0 + f b1 1 + f b2 2 + f b3 3 + f b4 4 + f b5 5 + f b6 6 + f b7 7))"
 ]
 "(* If this appears, you're using Ascii internals. Please don't *) (fun f c -> let n = Char.code c in let h i = (n land (1 lsl i)) <> 0 in f (h 0) (h 1) (h 2) (h 3) (h 4) (h 5) (h 6) (h 7))".
-
 Extract Constant zero => "'\000'".
 Extract Constant one => "'\001'".
 Extract Constant shift =>
  "fun b c -> Char.chr (((Char.code c) lsl 1) land 255 + if b then 1 else 0)".
-
 Extract Inlined Constant ascii_dec => "(=)".
 Extract Inductive string => "string" [ "str_nil" "str_cons" ].
 *)
@@ -65,17 +63,11 @@ in fun msg -> print_string (camlstring_of_coqstring msg ^ ""\n"")".
 (* Extract Inlined Constant LLVMAst.float => "float". *)
 
 (* Cutting the dependency to R. *)
-Extract Inlined Constant Defs.F2R => "fun _ -> assert false".
-Extract Inlined Constant Binary.FF2R => "fun _ -> assert false".
-Extract Inlined Constant Binary.B2R => "fun _ -> assert false".
-Extract Inlined Constant BinarySingleNaN.round_mode => "fun _ -> assert false".
-Extract Inlined Constant BinarySingleNaN.SF2R => "fun _ -> assert false".
-Extract Inlined Constant BinarySingleNaN.B2R => "fun _ -> assert false".
-(* Extract Inlined Constant Binary.BSN.round_mode => "fun _ -> assert false". *)
-(* Extract Inlined Constant Binary.BSN.SF2R => "fun _ -> assert false". *)
-(* Extract Inlined Constant Binary.BSN.B2R => "fun _ -> assert false". *)
-Extract Inlined Constant Bracket.inbetween_loc => "fun _ -> assert false".
-Extract Inlined Constant Generic_fmt.round => "fun _ -> assert false".
+Extract Inlined Constant Flocq.Core.Defs.F2R => "(fun _ -> assert false)".
+Extract Inlined Constant Flocq.IEEE754.Binary.FF2R => "(fun _ -> assert false)".
+Extract Inlined Constant Flocq.IEEE754.Binary.B2R => "(fun _ -> assert false)".
+Extract Inlined Constant Flocq.IEEE754.Binary.round_mode => "(fun _ -> assert false)".
+Extract Inlined Constant Flocq.Calc.Bracket.inbetween_loc => "(fun _ -> assert false)".
 
 Extract Inlined Constant Archi.ppc64 => "false".
 
