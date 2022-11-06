d3c98b10e08081595c75bdbe37b9fe7f9a0e0874
euisuny <ey222@cornell.edu>
2022-11-06 18:30:17 -0500

    refactor proof about itreemap

--- src/coq/Utils/Raise.v
+++ src/coq/Utils/Raise.v
@@ -314,29 +314,3 @@ Section UB.
       rbm_raise_ret_inv := raiseUB_ret_inv_itree;
     }.
 End UB.
-
-(* TODO: Move this? *)
-Lemma itree_map_ret_inv :
-  forall Eff A B (f : A -> B) (t : itree Eff A) b,
-    ITree.map f t ≈ ret b ->
-    exists a, t ≈ ret a /\ f a = b.
-Proof.
-  intros * HM.
-  punfold HM.
-  cbn in *.
-  red in HM.
-  dependent induction HM.
-  - setoid_rewrite (itree_eta t).
-    unfold ITree.map,observe in x; cbn in x.
-    destruct (observe t) eqn:EQ'; try now inv x.
-    cbn in *; exists r; inv x; split; reflexivity.
-  - unfold ITree.map,observe in x; cbn in x.
-    setoid_rewrite (itree_eta t).
-    destruct (observe t) eqn:EQ'; try now inv x.
-    cbn in x.
-    inv x.
-    edestruct IHHM as (? & ? & ?).
-    all: try reflexivity.
-    exists x; split; auto.
-    rewrite H, tau_eutt; reflexivity.
-Qed.
