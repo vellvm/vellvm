8128daa05f1c7743c6f251e4cfd3aa162cf8cd34
Hanxi Chen <garychen2000@outlook.com>
2023-06-15 11:27:11 -0400

    Add integer uvalue generator

--- src/coq/QC/GenAlive2.v
+++ src/coq/QC/GenAlive2.v
@@ -43,11 +43,24 @@ Module Type GEN_ALIVE2 (ADDR : MemoryAddress.ADDRESS) (IP:MemoryAddress.INTPTR) 
   Import LLVMEvents.
   Import DV.
 
+  Search G.
+  
   Fixpoint gen_uvalue (t : typ): G uvalue :=
     match t with
     | TYPE_I i =>
         match i with
-        | 1%N => returnGen (UVALUE_None)
+        | 1%N =>
+            x <- choose (0,1);;
+            returnGen (UVALUE_I1 (repr x)) 
+        | 8%N =>
+            x <- choose (0,2 ^ 8);;
+            returnGen (UVALUE_I8 (repr x))
+        | 32%N =>
+            x <- choose (0, 2 ^ 32);;
+            returnGen (UVALUE_I32 (repr x))
+        | 64%N =>
+            x <- choose (0, 2 ^ 63);;
+            returnGen (UVALUE_I64 (repr x))
         | _ => returnGen UVALUE_None
         end
     | _ => returnGen UVALUE_None
