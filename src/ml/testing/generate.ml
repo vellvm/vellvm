331a58d0b03e2172ff53be5403a9483f1927c4ee
Hanxi Chen <garychen2000@outlook.com>
2023-07-19 11:27:29 -0400

    Wrote basic generator function that replace gen_uvalue. Successfully compiled. Needs to edit parameters in gen_uvalue' to make sure generator actually works

--- src/ml/testing/generate.ml
+++ src/ml/testing/generate.ml
@@ -7,11 +7,12 @@ open MemoryModelImplementation
 open MemoryAddress
 module GA = GenAlive2.GEN_ALIVE2(MemoryModelImplementation.InfAddr)(MemoryModelImplementation.BigIP)(MemoryModelImplementation.FinSizeof)
 module GL = GenAlive2.GenLow
+open Buffer
 
-
-
-let rec gen_uvalue : LL.typ -> DV.uvalue GL.coq_G = function
-  | _ -> failwith "Unimplemented"
+let string_of_char_list : char list -> string = fun l ->
+  let buf = Buffer.create (List.length l) in
+  List.iter (Buffer.add_char buf) l;
+  Buffer.contents buf
 
 
 let small_gen =
@@ -35,27 +36,41 @@ let g_si32 = G.map
 
 let g_si64 = G.map (fun v -> DV.UVALUE_I64 (Z.of_sint v)) small_gen
 
-let rec gen_uvalue'' : LL.typ -> DV.uvalue GL.coq_G = (fun t -> GA.run_GenALIVE2 (GA.gen_uvalue t))
+let gen_uvalue'' (t : LL.typ) : DV.uvalue GL.coq_G =
+  let ran = GA.run_GenALIVE2 (GA.gen_uvalue t) in
+  GL.bindGen
+    (ran)
+    (fun x ->
+       begin match x with
+         | GenAlive2.Inl a -> failwith (string_of_char_list a)
+         | GenAlive2.Inr b -> GL.returnGen b
+       end)
 
-let rec gen_uvalue : LL.typ -> DV.uvalue G.t = function
-  | LL.TYPE_I i ->
-     begin match Camlcoq.N.to_int i with
-     | 1 -> g_i1 | 8 -> g_si8 | 16 | 32 -> g_si32 | 64 -> g_si64
-     | _ -> failwith "generating integer values of this type is not implemented"
-     end
-  | LL.TYPE_Void -> g_const DV.UVALUE_None
-  | LL.TYPE_Vector (sz, ty) ->
-     (* print_endline "generating vector value"; *)
-     let open G in
-     let gen_t = gen_uvalue ty in
-     let list_ts = G.list_size (g_const (sz |> Camlcoq.N.to_int32 |> Int32.to_int)) gen_t in
-     list_ts >>= (fun l -> G.return @@ DV.UVALUE_Vector l)
-  | LL.TYPE_Array (sz, ty) ->  
-     let open G in
-     let gen_t = gen_uvalue ty in
-     let list_ts = G.list_size (g_const (sz |> Camlcoq.N.to_int32 |> Int32.to_int)) gen_t in
-     list_ts >>= (fun l -> G.return @@ DV.UVALUE_Vector l)
-  | _ -> failwith "generating values of this type is not implemented"
+let gen_uvalue' (t : LL.typ) : DV.uvalue G.t =
+  let uv = GL.run (gen_uvalue'' t) O GenAlive2.newRandomSeed in (* This needs to be edited. Cannot afford to have random seed everytime*)
+  G.return uv
+
+let gen_uvalue : LL.typ -> DV.uvalue G.t = gen_uvalue'
+
+(* let rec gen_uvalue : LL.typ -> DV.uvalue G.t = function
+ *   | LL.TYPE_I i ->
+ *      begin match Camlcoq.N.to_int i with
+ *      | 1 -> g_i1 | 8 -> g_si8 | 16 | 32 -> g_si32 | 64 -> g_si64
+ *      | _ -> failwith "generating integer values of this type is not implemented"
+ *      end
+ *   | LL.TYPE_Void -> g_const DV.UVALUE_None
+ *   | LL.TYPE_Vector (sz, ty) ->
+ *      (\* print_endline "generating vector value"; *\)
+ *      let open G in
+ *      let gen_t = gen_uvalue ty in
+ *      let list_ts = G.list_size (g_const (sz |> Camlcoq.N.to_int32 |> Int32.to_int)) gen_t in
+ *      list_ts >>= (fun l -> G.return @@ DV.UVALUE_Vector l)
+ *   | LL.TYPE_Array (sz, ty) ->  
+ *      let open G in
+ *      let gen_t = gen_uvalue ty in
+ *      let list_ts = G.list_size (g_const (sz |> Camlcoq.N.to_int32 |> Int32.to_int)) gen_t in
+ *      list_ts >>= (fun l -> G.return @@ DV.UVALUE_Vector l)
+ *   | _ -> failwith "generating values of this type is not implemented" *)
 
 
 let gen_args : LL.typ list -> (DV.uvalue) list G.t = fun ts -> ts |> List.map (fun t -> gen_uvalue t) |> G.flatten_l 
