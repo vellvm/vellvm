1450b764c69ee607aaa80d29c7b514f28bc3dd40
Hanxi Chen <garychen2000@outlook.com>
2023-07-26 11:56:00 -0400

    Checkpoint: Partially implemented gen_instr and exp

--- src/coq/QC/GenAlive2.v
+++ src/coq/QC/GenAlive2.v
@@ -23,7 +23,7 @@ Import ListNotations.
 Import MonadNotation.
 Import ApplicativeNotation.
 
-(* Import Floats. *)
+Import Floats.
 From Coq Require Import
   ZArith Bool.Bool String.
 
@@ -44,34 +44,50 @@ Module GEN_ALIVE2 (ADDR : MemoryAddress.ADDRESS) (IP:MemoryAddress.INTPTR) (SIZE
   Definition var_context := list (ident * typ).
   Record GenState :=
     mkGenState
-    {
-      num_raw : N
-    ; gen_local_ctx : var_context
-    }.
+      {
+        num_void : N
+      ; num_raw : N
+      ; gen_local_ctx : var_context
+                          (* ; backtrack_instrs : list (instr typ) *)
+      }.
 
   Definition init_GenState : GenState
     :=
     {|
-      num_raw := 0
+      num_void := 0
+    ; num_raw := 0
     ; gen_local_ctx := []
     |}.
 
+  Definition increment_void (gs : GenState) : GenState
+    :=
+    {|
+      num_void := N.succ gs.(num_void)
+    ; num_raw := gs.(num_raw)
+    ; gen_local_ctx := gs.(gen_local_ctx)
+    |}.
+  
   Definition increment_raw (gs : GenState) : GenState
     :=
     {|
-      num_raw := N.succ gs.(num_raw)
+      num_void := gs.(num_void)
+    ; num_raw := N.succ gs.(num_raw)
     ; gen_local_ctx := gs.(gen_local_ctx)
     |}.
 
   Definition replace_local_ctx (ctx : var_context) (gs : GenState) : GenState
     :=
     {|
-      num_raw := gs.(num_raw)
+      num_void := gs.(num_void)
+    ;  num_raw := gs.(num_raw)
     ; gen_local_ctx := ctx
     |}.
     
   Definition GenALIVE2 := (eitherT string (stateT GenState G)).
 
+  Definition get_void (gs : GenState) : N
+    := gs.(num_void).
+  
   Definition get_raw (gs : GenState) : N
     := gs.(num_raw).
   
@@ -80,6 +96,11 @@ Module GEN_ALIVE2 (ADDR : MemoryAddress.ADDRESS) (IP:MemoryAddress.INTPTR) (SIZE
     apply Monad_stateT. typeclasses eauto.
   Defined.
 
+  Definition new_void_id : GenALIVE2 instr_id
+    := n <- gets get_void;;
+       modify increment_void;;
+       ret (IVoid (Z.of_N n)).
+  
   Definition new_raw_id : GenALIVE2 raw_id
     := n <- gets get_raw;;
        modify increment_raw;;
@@ -124,13 +145,27 @@ Module GEN_ALIVE2 (ADDR : MemoryAddress.ADDRESS) (IP:MemoryAddress.INTPTR) (SIZE
     exact (ret (inl (s), stack)).
   Defined.
 
-  Definition add_to_local_ctx (var : ident * typ) (gs: GenState) : GenALIVE2 unit
+  Definition add_to_local_ctx (var : ident * typ): GenALIVE2 unit
     := ctx <- get_local_ctx;;
        set_local_ctx (var :: ctx).
 
-  Definition append_to_local_ctx (vars : list (ident * typ)) (gs : GenState) : GenALIVE2 unit
+  Definition append_to_local_ctx (vars : list (ident * typ)): GenALIVE2 unit
     := ctx <- get_local_ctx;;
        set_local_ctx (vars ++ ctx).
+
+  Fixpoint remove_fst_id_var_context (id : ident) (l : var_context) : var_context
+    := match l with
+       | nil => nil 
+       | hd::tl => match Ident.eq_dec id (fst hd) with
+                 | left _ => tl
+                 | right _ => hd:: remove_fst_id_var_context id tl
+                 end
+       end.
+           
+  Definition remove_fst_from_local_ctx (var : ident * typ) : GenALIVE2 unit
+    := ctx <- get_local_ctx;;
+       set_local_ctx (remove_fst_id_var_context (fst var) ctx);;
+         ret tt.
 
   Definition reset_local_ctx : GenALIVE2 unit
     := set_local_ctx [].
@@ -178,57 +213,209 @@ Module GEN_ALIVE2 (ADDR : MemoryAddress.ADDRESS) (IP:MemoryAddress.INTPTR) (SIZE
 
   Definition gen_float_exp : GenALIVE2 (exp typ) :=
     ret EXP_Float <*> gen_float32.
+
+  (* size is the max depth of the data structure
+     int, float, double -> 0
+     ptr x -> size(x) + 1
+     vector n t -> size(t) + 1
+     struct -> max(size(l)) + 1
+   *)
+  Fixpoint depth_of_typ (t : typ) : nat :=
+    match t with
+    | TYPE_Array n t
+    | TYPE_Vector n t => depth_of_typ (t) + 1
+    | TYPE_Pointer t => depth_of_typ (t) + 1
+    | TYPE_Struct vars
+    | TYPE_Packed_struct vars => fold_right (fun x acc => max (depth_of_typ x) acc) 0%nat vars
+    | _ => 0
+    end.  
+
+  (* (* *)
+  (*   exp that directly link to types should be generated when a flag is up *)
+  (*   exp that are not related to types should be generated when a flag is down *)
+  (*  *) *)
+  (* Fixpoint gen_exp_size (depth: nat) (t : typ) {struct depth}: GenALIVE2 (exp typ) := *)
+  (*   local_ctx <- get_local_ctx;; *)
+  (*   match t with *)
+  (*   | TYPE_I sz => *)
+  (*       match depth with *)
+  (*       | O => *)
+  (*           ret sz >>= gen_int_exp *)
+  (*       | S _ => *)
+  (*           ret (OP_IBinop (LLVMAst.Add false false)) <*> ret t <*> gen_exp_size O t <*> ret (EXP_Integer 0) *)
+  (*       end *)
+  (*   | TYPE_Float => *)
+  (*       match depth with *)
+  (*       | O => *)
+  (*           gen_float_exp *)
+  (*       | S _ => *)
+  (*           ret (OP_FBinop (LLVMAst.FAdd) []) <*> ret t <*> gen_exp_size O t <*> ret (EXP_Float Float32.zero) *)
+  (*       end *)
+  (*   | TYPE_Double => *)
+  (*       match depth with *)
+  (*       | O => *)
+  (*           f32 <- gen_float32;;               *)
+  (*           ret (EXP_Double (Float.of_single f32)) *)
+  (*       | S _ => *)
+  (*           ret (OP_FBinop (LLVMAst.FAdd) []) <*> ret t <*> gen_exp_size O t <*> ret (EXP_Double Float.zero) *)
+  (*       end *)
+  (*   | TYPE_Array n t => *)
+  (*       match depth with *)
+  (*       | O => *)
+  (*           es <- vectorOf_ALIVE2 (N.to_nat n) (gen_exp_size O t);; *)
+  (*           ret (EXP_Array (map (fun e => (t, e)) es)) *)
+  (*       | S z => *)
+  (*           (* First loop through all and create instructions that add into backtrack_instrs *) *)
+  (*           (* Pick up last instrs and return *) *)
+  (*           failGen "Unimplemented" *)
+  (*       end *)
+  (*   | TYPE_Vector n t => *)
+  (*       match depth with *)
+  (*       | O => *)
+  (*           es <- vectorOf_ALIVE2 (N.to_nat n) (gen_exp_size O t);; *)
+  (*           ret (EXP_Vector (map (fun e => (t, e)) es)) *)
+  (*       | S z => *)
+  (*           (* First loop through all and create instructions that add into backtrack_instrs *) *)
+  (*           (* Pick up last instrs and return *) *)
+  (*           failGen "Unimplemented" *)
+  (*       end *)
+  (*   | TYPE_Struct vars *)
+  (*   | TYPE_Packed_struct vars => failGen "Unimplemented" *)
+  (*   | _ => failGen "Unimplemented" *)
+  (*   end *)
+  (* with *)
+  (* gen_instr (depth : nat) (t : typ) {struct depth}: GenALIVE2 (instr typ) *)
+  (* := *)
+  (*   let fix propogate (t : list typ) : GenALIVE2 (list (instr typ)) := *)
+  (*     failGen "Unimplemented" *)
+  (*   in  *)
+  (*   match t with *)
+  (*   | TYPE_Array n t => *)
+  (*   (* Iterate the array from the start. *)
+  (*      For each iteration, do: *)
+  (*        generate an insertelement instruction *)
+  (*        Propogate the new vector id to the next *) *)
+  (*       failGen "Unimplemented" *)
+  (*   | _ => failGen "Unimplemented" *)
+  (*   end *)
+  (* . *)
+
+
   
-  Fixpoint gen_exp_size (sz : nat) (t : typ) {struct sz} : GenALIVE2 (exp typ) :=
+  Fixpoint gen_exp_size (sz : nat) (t : typ) {struct sz}: GenALIVE2 (exp typ) :=
     local_ctx <- get_local_ctx;;
-    match sz with
-    | 0%nat =>
-        let fix gen_size_0 (ty : typ) : GenALIVE2 (exp typ) :=
+    let fix gen_size_0 (ty : typ) : GenALIVE2 (exp typ) :=
           match ty with
           | TYPE_I sz =>
               ret sz >>= gen_int_exp
           | TYPE_Float =>
               gen_float_exp
-          | TYPE_Array n t =>
-              es <- vectorOf_ALIVE2 (N.to_nat n) (gen_exp_size 0 t);;
-              ret (EXP_Array (map (fun e => (t, e)) es))
           | TYPE_Double =>
+              f32 <- gen_float32;;              
+              ret (EXP_Double (Float.of_single f32))
+          | TYPE_Array n t
+          | TYPE_Vector n t =>
+              es <- vectorOf_ALIVE2 (N.to_nat n) (gen_exp_size 0 t);;
+              ret (EXP_Array (map (fun e => (t, e)) es))    
+          | TYPE_Struct vars =>
+              failGen "Unimplemented"
+          | TYPE_Packed_struct vars =>
               failGen "Unimplemented"
           | _ => failGen "Unimplemented"
           end in
+    match sz with
+    | 0%nat =>
         gen_size_0 t
     | (S z)%nat =>
         match t with
         | TYPE_I sz =>
             ret (OP_IBinop (LLVMAst.Add false false)) <*> ret t <*> gen_exp_size 0 t <*> ret (EXP_Integer 0)
         | TYPE_Float =>
-            ret (OP_FBinop (LLVMAst.FAdd) []) <*> ret t <*> gen_exp_size 0 t <*> ret (EXP_Integer 0)
+            ret (OP_FBinop (LLVMAst.FAdd) []) <*> ret t <*> gen_exp_size 0 t <*> ret (EXP_Float Float32.zero)
+        | TYPE_Double =>
+            ret (OP_FBinop (LLVMAst.FAdd) []) <*> ret t <*> gen_exp_size 0 t <*> ret (EXP_Double Float.zero)
         | _ => failGen "Unimplemented"
         end
     end
+  with gen_exp_ident (t : typ): GenALIVE2 (exp typ) :=
+         (* Remove from local ctx *)
+        failGen "Unimplemented"
   (* Need to think about this. *)
   .
 
-  
-  Fixpoint gen_instr (t : typ) : GenALIVE2 (typ * instr typ) :=
-    match t with
+  Definition add_id_to_instr (t_instr : typ * instr typ) : GenALIVE2 (instr_id * instr typ)
+    :=
+    match t_instr with
+    | (TYPE_Void, instr) =>
+        vid <- new_void_id;;
+        ret (vid, instr)
+    | (t, instr) =>
+        i <- new_raw_id;;
+        add_to_local_ctx (ID_Local i, t);;
+        ret (IId i, instr)
+    end.
+
+  Fixpoint gen_instr (index : nat) (tgt : typ) {struct index}: GenALIVE2 (instr_id * instr typ) :=
+    match tgt with
     | TYPE_I _ =>
-        exp <- gen_exp_size 1 t;;
-        ret (t, INSTR_Op exp)
+        exp <- gen_exp_size 1 tgt;;
+        (add_id_to_instr (tgt, INSTR_Op exp))
     | TYPE_Float =>
-        exp <- gen_exp_size 1 t;;
-        ret (t, INSTR_Op exp)
-    | TYPE_Double => failGen "Unimplemented"
-    | TYPE_Array sz t => failGen "Unimplemented"
-    | TYPE_Vector sz t => failGen "Unimplemented"
-    | TYPE_Struct fields => failGen "Unimplemented"
-    | TYPE_Packed_struct fields => failGen "Unimplemented"
+        exp <- gen_exp_size 1 tgt;;
+        add_id_to_instr (tgt, INSTR_Op exp)
+    | TYPE_Double =>
+        exp <- gen_exp_size 1 tgt;;
+        add_id_to_instr (tgt, INSTR_Op exp)
+    | TYPE_Vector sz t' =>
+        e_src <- gen_exp_size 0 tgt;;
+        e_input <- gen_exp_size 0 t';;
+        let e_index := EXP_Integer (Z.of_nat index) in
+        let exp := OP_InsertElement (tgt, e_src) (t', e_input) (TYPE_I 8, e_index) in
+        add_id_to_instr (tgt, INSTR_Op exp)
+    | TYPE_Array sz t' =>
+        (* Assumption is that src have already been created, either undef or not *)
+        e_src <- gen_exp_ident tgt;;
+        e_input <- gen_exp_size 0 t';;
+        let exp := OP_InsertValue (tgt, e_src) (t', e_input) [Z.of_nat index] in
+        add_id_to_instr (tgt, INSTR_Op exp)
+    | TYPE_Struct fields =>
+        (* Assumption is that src have already been created, either undef or not *)
+        e_src <- gen_exp_ident tgt;;
+        t' <-  match nth_error fields index with
+            | Some t => ret t
+              | _ => failGen "Out of Bounds"
+              end;;
+        e_input <- gen_exp_size 0 t';;
+        let exp := OP_InsertValue (tgt, e_src) (t', e_input) [Z.of_nat index] in
+        add_id_to_instr (tgt, INSTR_Op exp)
+    | TYPE_Packed_struct fields =>
+        e_src <- gen_exp_size 0 tgt;;
+        t' <-  match nth_error fields index with
+            | Some t => ret t
+              | _ => failGen "Out of Bounds"
+              end;;
+        e_input <- gen_exp_size 0 t';;
+        let exp := OP_InsertValue (tgt, e_src) (t', e_input) [Z.of_nat index] in
+        add_id_to_instr (tgt, INSTR_Op exp)
     | TYPE_Pointer t => failGen "Unimplemented"
     | _ => failGen "Unimplemented"
     end.
 
-  Definition add_id_to_instr (t_instr : typ * instr typ) : GenALIVE2 (instr_id * instr typ)
-    := failGen "Unimplemented".
+  Fixpoint gen_instrs (depth : nat) (t : typ) {struct depth} : GenALIVE2 (list (instr_id * instr typ))
+    := match t with
+       | TYPE_I _ =>
+           inst <- gen_instr 0 t;;
+           ret [inst]
+       | TYPE_Float
+       | TYPE_Double =>
+           inst <- gen_instr 0 t;;
+           ret [inst]
+       | TYPE_Vector sz t' =>
+       (* Put an undef vector in there *)
+           
+       | _ => failGen "Unimplemented"
+       end.
+
 
   Definition gen_instr_id (t : typ): GenALIVE2 (instr_id * instr typ)
     := t_instr <- gen_instr t;; add_id_to_instr t_instr.
