3ba75e18c0b58b4fed2d433dd9247b026d9c72a8
Calvin Beck <hobbes@ualberta.ca>
2023-11-15 12:09:33 -0500

    Proof of all_bytes_from_uvalue_fin_inf_helper_Some.

--- src/coq/Semantics/InfiniteToFinite.v
+++ src/coq/Semantics/InfiniteToFinite.v
@@ -23056,11 +23056,44 @@ cofix CIH
       (BYTES : sbytes_refine bytes_inf bytes_fin),
       DVC1.uvalue_refine_strict parent_inf parent_fin ->
       Memory64BitIntptr.MMEP.MMSP.MemByte.all_bytes_from_uvalue_helper idx sid parent_fin bytes_fin = Some uv_fin ->
-      exists uv_inf,
-        MemoryBigIntptr.MMEP.MMSP.MemByte.all_bytes_from_uvalue_helper idx sid parent_inf bytes_inf = Some uv_inf /\
-          DVC1.uvalue_refine_strict uv_inf uv_fin.
+      MemoryBigIntptr.MMEP.MMSP.MemByte.all_bytes_from_uvalue_helper idx sid parent_inf bytes_inf = Some parent_inf.
   Proof.
-  Admitted.
+    intros bytes_fin bytes_inf parent_fin parent_inf idx sid uv_fin BYTES REF ALL.
+    revert parent_fin parent_inf idx sid uv_fin REF ALL.
+    induction BYTES; intros parent_fin parent_inf idx sid uv_fin REF ALL.
+    - cbn in *; inv ALL.
+      split; eauto.
+    - cbn in ALL.
+      repeat break_match_hyp_inv.
+      destruct u0, u1, u2.
+      eapply IHBYTES in H2; eauto.
+      Transparent MemoryBigIntptr.MMEP.MMSP.MemByte.all_bytes_from_uvalue_helper.
+      cbn.
+      destruct x.
+      red in H.
+      unfold convert_SByte in H.
+      cbn in H.
+      break_match_hyp_inv.
+      rewrite FinLLVM.MEM.Byte.sbyte_to_extractbyte_of_uvalue_sbyte in Hequ.
+      inv Hequ.
+      rewrite InfLLVM.MEM.Byte.sbyte_to_extractbyte_of_uvalue_sbyte.
+      rewrite Heqo.
+      unfold OptionUtil.guard_opt in *.
+      repeat break_match_hyp_inv.
+      pose proof RelDec.rel_dec_correct u parent_fin.
+      apply H in Heqb0; subst.
+      break_inner_match_goal.
+      * eauto.
+      * red in REF.
+        pose proof DVC1.uvalue_refine_strict_R2_injective.
+        red in H0.
+        specialize (H0 _ _ _ _ Heqo2 REF).
+        destruct H0.
+        forward H1; auto.
+        subst.
+        rewrite Util.eq_dec_eq in Heqb0.
+        inv Heqb0.        
+  Qed.
 
   Lemma all_bytes_from_uvalue_fin_inf_Some :
     forall {bytes_fin : list Memory64BitIntptr.MP.BYTE_IMPL.SByte}
