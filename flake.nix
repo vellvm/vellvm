429d447008cb9517019183e4529cf4b2b74b1ebe
Calvin Beck <hobbes@ualberta.ca>
2023-12-01 12:44:33 -0500

    Add getopt to nix for vellvm-reduce.

--- flake.nix
+++ flake.nix
@@ -104,7 +104,13 @@
           # Include a fixed version of clang in the development environment for testing.
           default = pkgs.mkShell {
             inputsFrom = [ packages.default ];
-            buildInputs = [ pkgs.clang_13 pkgs.llvmPackages_13.libllvm ocamlPkgs.utop ocamlPkgs.bisect_ppx ocamlPkgs.ppxlib ];
+            buildInputs = [ pkgs.clang_13
+                            pkgs.llvmPackages_13.libllvm
+                            ocamlPkgs.utop
+                            ocamlPkgs.bisect_ppx
+                            ocamlPkgs.ppxlib
+                            pkgs.util-linux
+                          ];
           };
         };
 
