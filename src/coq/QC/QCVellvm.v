ea2edf88cc2b1f965b8b6864c4253ca72c6ee42b
Calvin Beck <hobbes@ualberta.ca>
2024-02-02 17:37:39 -0500

    Fix QCVellvm.v

--- src/coq/QC/QCVellvm.v
+++ src/coq/QC/QCVellvm.v
@@ -21,8 +21,8 @@ From ITree Require Import
 (* Require Import ExtrOcamlString. *)
 
 (* From QuickChick Require Import QuickChick. *)
-From QuickChick Require Import Show Checker.
-From Vellvm Require Import ShowAST ReprAST (* GenAST  *)TopLevel LLVMAst DynamicValues.
+From QuickChick Require Import Show Checker Generators Producer Test.
+From Vellvm Require Import ShowAST ReprAST GenAST TopLevel LLVMAst DynamicValues.
 
 Extraction Blacklist String List Char Core Z Format.
 
@@ -39,8 +39,8 @@ Proof.
   exact
     (fun res =>
        match res with
-       | MlOk a => ("Ok " ++ show a)%string
-       | MlError e => ("Error " ++ show e)%string
+       | MlOk _ _ a => ("Ok " ++ show a)%string
+       | MlError _ _ e => ("Error " ++ show e)%string
        end).
 Defined.
 
@@ -61,16 +61,16 @@ Fixpoint step (t : ITreeDefinition.itree L4 res_L4) : MlResult dvalue string
   := match observe t with
      | RetF (_,(_,(_,(_,x)))) => MlOk _ string x
      | TauF t => step t
-     | VisF _ (inl1 e) k =>
+     | VisF (inl1 e) k =>
          MlError _ string "Uninterpreted external call"
-     | VisF _ (inr1 (inl1 (ThrowOOM msg))) k =>
-         MlError _ string ("OOM: " ++ msg)%string
-     | VisF _ (inr1 (inr1 (inl1 (ThrowUB msg)))) k =>
-         MlError _ string ("UB: " ++ msg)%string
-     | VisF _ (inr1 (inr1 (inr1 (inl1 (Debug msg))))) k =>
-         MlError _ string ("Debug: " ++ msg)%string
-     | VisF _ (inr1 (inr1 (inr1 (inr1 (LLVMEvents.Throw msg))))) k =>
-         MlError _ string ("Failure: " ++ msg)%string
+     | VisF (inr1 (inl1 (ThrowOOM msg))) k =>
+         MlError _ string ("OOM")%string
+     | VisF (inr1 (inr1 (inl1 (ThrowUB msg)))) k =>
+         MlError _ string ("UB")%string
+     | VisF (inr1 (inr1 (inr1 (inl1 (Debug msg))))) k =>
+         MlError _ string ("Debug")%string
+     | VisF (inr1 (inr1 (inr1 (inr1 (LLVMEvents.Throw msg))))) k =>
+         MlError _ string ("Failure")%string
      end.
 Set Guard Checking.
 
@@ -183,7 +183,7 @@ Definition vellvm_agrees_with_clang_parallel (p : PROG) : Checker
     let vellvm_res := interpret prog in
     let clang_res := snd (waitpid nil pid) in
     match vellvm_res, clang_res with
-    | MlOk (DVALUE_I8 x), (WEXITED ocaml_y) =>
+    | MlOk _ _ (DVALUE_I8 x), (WEXITED ocaml_y) =>
         let y := repr (oint_to_Z ocaml_y) in
         if equ x y
         then checker true
@@ -215,7 +215,7 @@ Definition vellvm_agrees_with_clang (p : string + PROG) : Checker
       let clang_res := run_llc prog in
       let vellvm_res := interpret prog in
       match clang_res, vellvm_res with
-      | DVALUE_I8 y, MlOk (DVALUE_I8 x) =>
+      | DVALUE_I8 y, MlOk _ _ (DVALUE_I8 x) =>
           if equ x y
           then checker true
           else whenFail ("Vellvm: " ++ show (unsigned x) ++ " | Clang: " ++ show (unsigned y) ++ " | Ast: " ++ ReprAST.repr prog) false
@@ -224,7 +224,10 @@ Definition vellvm_agrees_with_clang (p : string + PROG) : Checker
       end
   end.
 
-(* (* Definition agrees := (forAll (run_GenLLVM gen_llvm) vellvm_agrees_with_clang). *)
+Definition gen_PROG : GenLLVM PROG
+  := fmap Prog gen_llvm.
+
+(* Definition agrees := (forAll (run_GenLLVM gen_llvm) vellvm_agrees_with_clang). *)
 
 Extract Constant defNumTests    => "1000".
 
@@ -240,4 +243,4 @@ QCInclude "ml/libvellvm/*".
 (* QCInclude "../../ml/extracted/*ml". *)
 Extract Inlined Constant Error.failwith => "(fun _ -> raise)".
 QuickChick (forAll (run_GenLLVM gen_PROG) vellvm_agrees_with_clang).
-(*! QuickChick agrees. *) *)
+(*! QuickChick agrees. *)
