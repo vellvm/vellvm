1cdab6aa332cba9188d5cbd4dd4d9cac848a0545
Calvin Beck <hobbes@ualberta.ca>
2024-02-12 19:44:05 -0500

    InsertElement in concretize_ub_fin_inf

--- src/coq/Semantics/InfiniteToFinite/LangRefine.v
+++ src/coq/Semantics/InfiniteToFinite/LangRefine.v
@@ -13927,6 +13927,19 @@ Qed.
     }
   Qed.
 
+  (* TODO: Move this / generalize monad? *)
+  (* TODO: Prove this *)
+  Lemma insert_into_vec_dv_no_ub_fin_inf :
+    forall dv1_fin dv2_fin dv3_fin ub_msg t,
+      @insert_into_vec_dv err_ub_oom (@Monad_err_ub_oom IdentityMonad.ident IdentityMonad.Monad_ident)
+        (@RAISE_ERROR_err_ub_oom IdentityMonad.ident IdentityMonad.Monad_ident)
+        t dv1_fin dv2_fin dv3_fin = raise_ub ub_msg -> False.
+  Proof.
+    intros dv1_fin dv2_fin dv3_fin ub_msg t INSERT.
+    unfold insert_into_vec_dv in INSERT.
+    repeat break_match_hyp_inv.
+  Qed.
+
   Lemma concretize_ub_fin_inf :
     forall uv_inf uv_fin ub_msg
       (REF : uvalue_refine_strict uv_inf uv_fin)
@@ -14865,7 +14878,97 @@ Qed.
       symmetry in Heqres.
       eapply index_into_vec_dv_no_ub in Heqres; contradiction.
     - (* UVALUE_InsertElement *)
-      admit.
+      unfold_uvalue_refine_strict_in REF.
+      repeat break_match_hyp_inv.
+
+      repeat red.
+      rewrite IS1.LLVM.MEM.CP.CONCBASE.concretize_uvalueM_equation.
+
+      repeat red in UB.
+      rewrite IS2.LLVM.MEM.CP.CONCBASE.concretize_uvalueM_equation in UB.
+
+      repeat red in UB.
+      destruct UB as (?&?&?&?&?).
+      destruct_err_ub_oom x; inv H0.
+      { (* UB when concretizing first operand *)
+        eapply IHuv_inf1 in H; eauto.
+        repeat red.
+        exists (UB_unERR_UB_OOM ub_msg).
+        exists (fun _ => UB_unERR_UB_OOM ub_msg).
+        split; cbn; eauto.
+      }
+
+      exists (ret (fin_to_inf_dvalue x1)).
+      exists (fun _ => UB_unERR_UB_OOM ub_msg).
+      split; eauto.
+      eapply uvalue_concretize_strict_concretize_inclusion; eauto.
+      split; eauto.
+      right.
+      intros a H0.
+      inv H0.
+
+      (* No UB on first operand. *)
+      destruct H1 as [[] | H1].
+      specialize (H1 x1).
+      forward H1; [cbn; auto|].
+      repeat red in H1.
+      destruct H1 as (?&?&?&?&?).
+      rewrite <- H1 in H3.
+      destruct_err_ub_oom x; inv H3.
+
+      { (* UB uv_inf3 *)
+        eapply IHuv_inf3 in H0; eauto.
+        repeat red.
+        exists (UB_unERR_UB_OOM ub_msg).
+        exists (fun _ => UB_unERR_UB_OOM ub_msg).
+        split; cbn; eauto.
+      }
+
+      (* No UB on uv_inf3 *)
+      exists (ret (fin_to_inf_dvalue x3)).
+      exists (fun _ => UB_unERR_UB_OOM ub_msg).
+      split; eauto.
+      eapply uvalue_concretize_strict_concretize_inclusion; eauto.
+      split; eauto.
+      right.
+      intros a ?.
+      inv H3.
+
+      destruct H2 as [[] | H2].
+      specialize (H2 x3).
+      forward H2; [cbn; auto|].
+      repeat red in H2.
+      destruct H2 as (?&?&?&?&?).
+      rewrite <- H3 in H5.
+      destruct_err_ub_oom x; inv H5.
+
+      { (* UB in uv_inf2 *)
+        eapply IHuv_inf2 in H2; eauto.
+        repeat red.
+        exists (UB_unERR_UB_OOM ub_msg).
+        exists (fun _ => UB_unERR_UB_OOM ub_msg).
+        split; cbn; eauto.
+      }
+
+      (* No UB on uv_inf2 *)
+      exists (ret (fin_to_inf_dvalue x5)).
+      exists (fun _ => UB_unERR_UB_OOM ub_msg).
+      split; eauto.
+      eapply uvalue_concretize_strict_concretize_inclusion; eauto.
+      split; eauto.
+      right.
+      intros a ?.
+      inv H5.
+
+      (* UB in evaluating... *)
+      destruct H4 as [[] | H4].
+      specialize (H4 x5).
+      forward H4; [cbn; auto|].
+      rewrite <- H4 in H7.
+      remember (insert_into_vec_dv t x1 x5 x3) as res.
+      destruct_err_ub_oom res; inv H7.
+      symmetry in Heqres.
+      eapply insert_into_vec_dv_no_ub_fin_inf in Heqres; contradiction.
     - (* UVALUE_ExtractValue *)
       admit.
     - (* UVALUE_InsertValue *)
