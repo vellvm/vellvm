4f702c3c6e928edf4d443e100c78089d5eb7da6c
Calvin Beck <hobbes@ualberta.ca>
2024-02-28 22:09:53 -0500

    Cleanup for submission.

--- ci/keep_alive.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Stolen from fiat-crypto: https://github.com/mit-plv/fiat-crypto/blob/78656baa06ed1daeb7ba90c2d429228516dc8475/etc/ci/keep_alive.sh
-
-while [ 1 ]
-do
-    echo ""
-    echo "Travis keep-alive spew"
-    sleep 5m
-done
