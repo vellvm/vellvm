b008cd8fcf172437a2820088c142d34c8c5a388b
Calvin Beck <hobbes@ualberta.ca>
2024-02-29 00:34:08 -0500

    Remove licenses.

--- src/ml/libvellvm/ast_printer.ml
+++ src/ml/libvellvm/ast_printer.ml
@@ -1,11 +1,3 @@
-(*  ------------------------------------------------------------------------- *)
-(* Adapted for use in Vellvm by Steve Zdancewic (c) 2017                      *)
-(*  ------------------------------------------------------------------------- *)
-
-
-(*
-  TODO: The ReprAST.repr_tle (and related "repr" type classes) stack overflow on large inputs.
-*)
 let toplevel_entities (fmt : Format.formatter) (tles: (LLVMAst.typ , (LLVMAst.typ LLVMAst.block) * ((LLVMAst.typ LLVMAst.block) list)) LLVMAst.toplevel_entities) : unit =
   List.iter
     (fun tle ->
