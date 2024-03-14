784a1d753ee2aee05e939776aa45f46cee4f3f42
Calvin Beck <hobbes@ualberta.ca>
2024-03-14 18:50:25 -0400

    Messing with ECS QC.

---
+++ src/coq/QC/ECS.v
@@ -0,0 +1,581 @@
+From Coq Require Import List.
+From Ltac2 Require Import Ltac2.
+Require Import ZArith String.
+From Vellvm.Utils Require Import
+  IntMaps
+  Monads
+  Default.
+
+From Vellvm.Syntax Require Import
+  LLVMAst.
+
+From Vellvm.QC Require Import Lens.
+Import LensNotations.
+Local Open Scope lens.
+
+Require Import ExtLib.Data.Monads.StateMonad.
+Require Import ExtLib.Structures.Monads.
+Require Import ExtLib.Structures.Functor.
+Require Import ExtLib.Structures.Applicative.
+Require Import ExtLib.Structures.Traversable.
+Import MonadNotation.
+Import ApplicativeNotation.
+Import FunctorNotation.
+Import ListNotations.
+
+Definition Position := (Z * Z)%type.
+
+Variant ComponentType :=
+  | Field
+  | Unique.
+
+Variant StorageType :=
+  | FieldOf
+  | WorldOf : (Type -> Type) -> StorageType
+  | SetterOf.
+
+Variant Update (a : Type) :=
+  | Keep
+  | Unset
+  | SetValue : a -> Update a.
+
+Instance Functor_Update : Functor Update.
+split.
+intros A B f ua.
+destruct ua eqn:Hua.
+- apply Keep.
+- apply Unset.
+- apply (SetValue _ (f a)).
+Defined.
+
+(* TODO: Move to intmap library *)
+Instance Functor_IntMap : Functor IntMap.
+split.
+intros A B f ma.
+eapply IntMaps.IM.map. apply f.
+apply ma.
+Defined.
+
+(* TODO: Move to intmap library *)
+Instance Functor_IntMapRaw : Functor IM.Raw.t.
+split.
+intros A B f ma.
+eapply IM.Raw.map. apply f.
+apply ma.
+Defined.
+
+
+Record Ent := mkEnt { unEnt : Z }.
+
+Definition Component (s : StorageType) (c : ComponentType) (a : Type) : Type :=
+  match s with
+  | FieldOf => option a
+  | SetterOf => Update a
+  | WorldOf m =>
+      match c with
+      | Field => IM.Raw.t a
+      | Unique => option (Z * a)
+      end
+  end.
+
+Instance Functor_Component {s c} : Functor (Component s c).
+split.
+intros A B f.
+destruct s.
+- apply (fmap f).
+- destruct c; cbn.
+  + apply (fmap f).
+  + intros o.
+    destruct o eqn:Ho.
+    * destruct p.
+      apply (Some (z, f a)).
+    * apply None.
+- cbn.
+  apply (fmap f).
+Defined.
+
+Record Metadata s :=
+  mkMetadata
+    { name : Component s Field ident
+    ; type_alias : Component s Field typ
+    ; variable_type : Component s Field typ
+    ; is_local : Component s Field unit
+    ; is_global : Component s Field unit
+    ; is_non_deterministic : Component s Field unit
+    ; from_pointer : Component s Field Ent
+    }.
+
+Instance Default_Metadata {m} : Default (Metadata (WorldOf m)).
+split.
+apply mkMetadata; apply IM.empty.
+Defined.
+
+Record SystemState w m :=
+  mkSystemState
+  { systemStateNextEnt : Z
+  ; systemStateEntities : IS.t
+  ; systemStateMetadata : w (WorldOf m)
+  }.
+
+Instance Default_IntSet : Default IS.t
+  := { def := IS.empty }.
+
+Instance Default_SystemState {w m} `{Default (w (WorldOf m))} : Default (SystemState w m)
+  := { def := mkSystemState w m def def def }.
+
+Definition nextEnt {w m} : Lens' (SystemState w m) Z.
+  red.
+  intros f F afa w'.
+
+  refine
+    open_constr:
+      ((fun x => _) <$> afa (_ w')); try typeclasses_eauto.
+  - constructor;
+      Control.dispatch
+        [ (fun _ => apply x)
+          ; (fun _ => eapply systemStateEntities)
+          ; (fun _ => eapply systemStateMetadata)
+        ];
+      apply w'.
+  - intros w''.
+    destruct w''.
+    apply systemStateNextEnt0.
+Defined.
+
+Definition entities {w m} : Lens' (SystemState w m) IS.t.
+  red.
+  intros f F afa w'.
+
+  refine
+    open_constr:
+      ((fun x => _) <$> afa (_ w')); try typeclasses_eauto.
+  - constructor;
+      Control.dispatch
+        [ (fun _ => eapply systemStateNextEnt)
+          ; (fun _ => eapply x)
+          ; (fun _ => eapply systemStateMetadata)
+        ];
+      apply w'.
+  - intros w''.
+    destruct w''.
+    apply systemStateEntities0.
+Defined.
+
+Definition metadata {w m} : Lens' (SystemState w m) (w (WorldOf m)).
+  red.
+  intros f F afa w'.
+
+  refine
+    open_constr:
+      ((fun x => _) <$> afa (_ w')); try typeclasses_eauto.
+  - constructor;
+      Control.dispatch
+        [ (fun _ => eapply systemStateNextEnt)
+          ; (fun _ => eapply systemStateEntities)
+          ; (fun _ => eapply x)
+        ];
+      apply w'.
+  - intros w''.
+    destruct w''.
+    apply systemStateMetadata0.
+Defined.
+
+Definition SystemT W M A := stateT (SystemState W M) M A.
+Instance Monad_SystemT {w} {m} `{Monad m} : Monad (SystemT w m).
+unfold SystemT.
+apply Monad_stateT; auto.
+Defined.
+
+Instance MonadState_SystemT {w} {m} `{Monad m} : MonadState (SystemState w m) (SystemT w m).
+unfold SystemT.
+apply MonadState_stateT; auto.
+Defined.
+
+Instance MonadT_SystemT {w} {m} `{Monad m} : MonadT (SystemT w m) m.
+unfold SystemT.
+apply MonadT_stateT; auto.
+Defined.
+
+Ltac2 metadataConstructors :=
+  [ (fun _ => apply name)
+  ; (fun _ => apply type_alias)
+  ; (fun _ => apply variable_type)
+  ; (fun _ => apply is_local)
+  ; (fun _ => apply is_global)
+  ; (fun _ => apply is_non_deterministic)
+  ; (fun _ => apply from_pointer)
+  ].
+
+Ltac2 applyMetadataConstructors (tac : unit -> unit) :=
+  Control.dispatch
+    [ (fun _ => tac (); apply name)
+      ; (fun _ => tac (); apply type_alias)
+      ; (fun _ => tac (); apply variable_type)
+      ; (fun _ => tac (); apply is_local)
+      ; (fun _ => tac (); apply is_global)
+      ; (fun _ => tac (); apply is_non_deterministic)
+      ; (fun _ => tac (); apply from_pointer)
+    ].  
+
+Definition getEntity {m} `{Monad m} (entity : Ent) : SystemT Metadata m (Metadata FieldOf).
+  refine open_constr:(let entity_id := unEnt entity in
+                      storage <- use metadata;;
+                      ret (_)); try typeclasses_eauto.
+  apply (mkMetadata FieldOf);
+    refine open_constr:(IM.Raw.find entity_id (_ storage));
+    Control.dispatch metadataConstructors.
+Defined.
+
+Definition updateIntMap {a} (u : Update a) k (m : IntMap a) : IntMap a
+  := match u with
+     | Keep => m
+     | Unset => delete k m
+     | SetValue x => add k x m
+     end.
+
+Definition updateIntMapRaw {a} (u : Update a) k (m : IM.Raw.t a) : IM.Raw.t a
+  := match u with
+     | Keep => m
+     | Unset => IM.Raw.remove k m
+     | SetValue x => IM.Raw.add k x m
+     end.
+
+Definition blankSetter : (Metadata SetterOf).
+  constructor.
+  all: apply Keep.
+Defined.
+
+Definition defFields : (Metadata FieldOf).
+  constructor.
+  all: apply None.
+Defined.
+
+Definition setEntity
+  {m} `{Monad m} (entity : Ent) (setter : Metadata SetterOf) : SystemT Metadata m unit.
+  refine
+    open_constr:(let entity_id := unEnt entity in
+                 metadata _ _ %= (fun storage => _);;
+                 ret tt);
+    try typeclasses_eauto.
+  apply (mkMetadata (WorldOf m));
+    applyMetadataConstructors (fun _ => refine open_constr:(updateIntMapRaw (_ setter) entity_id (_ storage))).
+Defined.
+
+Definition name' {s : StorageType} : Lens' (Metadata s) (Component s Field ident).
+  red.
+  intros f F afa w.
+  refine open_constr:((fun x => _) <$> afa (_ w)); try typeclasses_eauto.
+  - apply mkMetadata;
+      Control.dispatch
+        [ (fun _ => apply x)
+          ; (fun _ => apply type_alias)
+          ; (fun _ => apply variable_type)
+          ; (fun _ => apply is_local)
+          ; (fun _ => apply is_global)
+          ; (fun _ => apply is_non_deterministic)
+          ; (fun _ => apply from_pointer)
+        ];
+      apply w.
+  - apply name.
+Defined.
+
+Definition is_global' {s : StorageType} : Lens' (Metadata s) (Component s Field unit).
+  red.
+  intros f F afa w.
+  refine open_constr:((fun x => _) <$> afa (_ w)); try typeclasses_eauto.
+  - apply mkMetadata;
+      Control.dispatch
+        [ (fun _ => apply name)
+          ; (fun _ => apply type_alias)
+          ; (fun _ => apply variable_type)
+          ; (fun _ => apply is_local)
+          ; (fun _ => apply x)
+          ; (fun _ => apply is_non_deterministic)
+          ; (fun _ => apply from_pointer)
+        ];
+      apply w.
+  - apply is_global.
+Defined.
+
+Definition ident_to_raw_id (i : ident) :=
+  match i with
+  | ID_Global id => id
+  | ID_Local id => id
+  end.
+
+(* Virtual lens for setting global identifiers *)
+Definition global' {s : StorageType} : Lens' (Metadata s) (Component s Field raw_id).
+  red.
+  intros f F afa w.
+  refine open_constr:((fun x => _) <$> afa (_ w)); try typeclasses_eauto.
+  - apply mkMetadata;
+      Control.dispatch
+        [ (fun _ => apply (fmap ID_Global x))
+          ; (fun _ => apply type_alias)
+          ; (fun _ => apply variable_type)
+          ; (fun _ => apply is_local)
+          ; (fun _ => apply (fmap (fun _ => tt) x))
+          ; (fun _ => apply is_non_deterministic)
+          ; (fun _ => apply from_pointer)
+        ];
+      apply w.
+  - intros w'.
+    apply (fmap ident_to_raw_id (name _ w')).
+Defined.
+
+Definition globals' {m} (w : Metadata (WorldOf m)) : list Ent.
+  apply is_global in w.
+  red in w.
+  apply (fmap (fun e => mkEnt (fst e)) (IM.Raw.elements w)).
+Defined.
+
+Definition traverseWithKeyRaw {t} `{Applicative t} {a b} (f : IM.Raw.key -> a -> t b) (m : IM.Raw.t a) : t (IM.Raw.t b)
+  :=
+  let fix go x :=
+    match x with
+    | IM.Raw.Leaf => pure (IM.Raw.Leaf _)
+    | IM.Raw.Node l k v r h =>
+        if Z.eqb 1%Z h
+        then (fun v' => IM.Raw.Node (IM.Raw.Leaf _) k v' (IM.Raw.Leaf _) 1%Z) <$> f k v
+        else (fun l' v' r' => IM.Raw.Node l' k v' r' h) <$> go l <*> f k v <*> go r
+    end
+  in
+  go m.
+
+Instance Traversable_RawIntMap : Traversable IM.Raw.t.
+split.
+intros F Ap A B X X0.
+eapply traverseWithKeyRaw.
+intros k.
+apply X.
+apply X0.
+Defined.
+
+Definition traverseWithKey {t} `{Applicative t} {a b} (f : IM.key -> a -> t b) (m : IM.t a) : t (IM.t b).
+  eapply @IM.fold.
+  3: apply (pure (IM.empty _)).
+  2: apply m.
+  - intros k e acc.
+    refine open_constr:(let res := f k e in _).
+    eapply ap.
+    2: apply acc.
+    eapply fmap.
+    2: apply res.
+    apply (IM.add k).
+Defined.
+
+Instance Traversable_IntMap : Traversable IntMap.
+split.
+intros F Ap A B X X0.
+eapply traverseWithKey.
+intros k.
+apply X.
+apply X0.
+Defined.
+
+Instance Traversable_option : Traversable option.
+split.
+intros F Ap A B X X0.
+destruct X0 eqn:Hx.
+- apply (Some <$> X a).
+- apply (pure None).
+Defined.
+
+Instance Traversable_Update : Traversable Update.
+split.
+intros F Ap A B X X0.
+destruct X0 eqn:Hx.
+- apply (pure (Keep _)).
+- apply (pure (Unset _)).
+- apply (SetValue _ <$> X a).
+Defined.
+
+Instance Traversable_Component {s c} : Traversable (Component s c).
+split.
+intros F Ap A B X X0.
+destruct s; cbn in *.
+- eapply mapT; eauto.
+- destruct c.
+  + eapply mapT; eauto.
+  + eapply mapT.
+    2: apply X0.
+    intros X1.
+    eapply ap.
+    2: apply (X (snd X1)).
+    apply (pure (fun b => (fst X1, b))).
+- eapply mapT; eauto.
+Defined.
+
+Definition optSetMap {a} (mv : option a) k (m : IntMap a) : IntMap a
+  := match mv with
+     | Some v => add k v m
+     | None => delete k m
+     end.
+
+Definition optSetMapRaw {a} (mv : option a) k (m : IM.Raw.t a) : IM.Raw.t a
+  := match mv with
+     | Some v => IM.Raw.add k v m
+     | None => IM.Raw.remove k m
+     end.
+
+Definition entl {m} `{HM: Monad m} (e : Ent) : Lens' (Metadata (WorldOf m)) (Metadata FieldOf).
+  red.
+  intros f F afa w.
+
+  refine
+    open_constr:
+      (let eid := unEnt e in
+       ((fun x => _) <$> afa (_ w))); try typeclasses_eauto.
+  - apply mkMetadata; red;
+      applyMetadataConstructors (fun _ => refine open_constr:(optSetMapRaw (_ x) eid (_ w))).
+  - intros w'.
+    apply (mkMetadata FieldOf);
+      refine open_constr:(IM.Raw.find eid (_ w'));
+      Control.dispatch metadataConstructors.
+Defined.
+
+Definition newEntity {m} `{Monad m} : SystemT Metadata m Ent
+  := i <- use nextEnt;;
+     nextEnt _ _ .= Z.succ i;;
+     entities _ _ %= (fun ents => IS.add i ents);;
+     ret (mkEnt i).
+
+Definition runSystemT {m} `{Monad m} {a} (w : Metadata (WorldOf m)) (sys : SystemT Metadata m a) : m a
+  := evalStateT sys def.
+
+Definition test_system {m} `{HM: Monad m} : SystemT Metadata m _
+  := e <- newEntity;;
+     e2 <- newEntity;;
+     (metadata .@ entl e .@ global') _ _ .= ret (Name "blah");;
+     ent <- use (metadata .@ entl e);;
+     ret ent.
+
+Definition ecs_test :=
+  IdentityMonad.unIdent (runSystemT def test_system).
+
+(* What if I want to find an entity that's not in the set? *)
+Eval compute in ecs_test.
+
+Definition firstAndThird {a x b} : Traversal (a * x * a) (b * x * b) a b.
+  red.
+  intros f F focus X0.
+  destruct X0 as [[a' b'] c'].
+  apply (pure (fun a b c => (a, b, c)) <*> focus a' <*> pure b' <*> focus c').
+Defined.
+
+Definition ixs {a} `{Default a} (ns : list nat) : Traversal (list a) (list a) a a.
+  red.
+  intros f F focus xs.
+  refine constr:(let fix go (n : nat) (ns : list nat) (xs : list a) :=
+            match xs with
+            | nil => pure nil
+            | cons a0 l =>
+                let rest := go (S n) ns l in
+                if (existsb (Nat.eqb n) ns)
+                then (pure cons <*> focus a0 <*> rest)
+                else (pure cons <*> pure a0 <*> rest)
+            end
+          in
+          go 0 ns xs).
+Defined.
+
+Eval compute in (over (ixs [1; 4] _ _) (fun x => 2 * x) [1; 2; 3; 4; 5]).
+Eval compute in (over (firstAndThird _ _) length ("one", 2, "three")).
+Goal Lens (string * nat * string) (nat * nat * nat) string nat.
+  red.
+  intros f F X H.
+  eapply firstAndThird.
+  assert open_constr:(over _ length ("one", 2, "three") = (3, 2, 5)).
+  red.
+  intros f F X H.
+
+  eapply firstAndThird; try typeclasses_eauto.
+  pose open_constr:(over _ _ firstAndThird length ("one", 2, "three")).
+Eval compute in .
+
+Definition names'' {m} : Traversal (Metadata (WorldOf m)) (Metadata (WorldOf m)) (list ident) (list ident).
+  red.
+  intros f F focus meta.
+  cbn in *.
+  remember meta.
+  clear Heqm0.
+  apply name in m0.
+  red in m0.
+  eapply mapT in m0.
+  2: intros x; apply (focus [x]).
+  eapply fmap.
+  2: apply m0.
+  intros ?.
+  eapply ap.
+
+    
+  eapply mapT in m0.
+  2: apply pure.
+  eapply ap.
+  2: apply m0.
+  eapply (@fmap f _ _ _ _).
+  apply m0.
+  Unshelve.
+  
+  
+  eapply mapT in focus.
+  eapply fmap.
+  2: apply focus.
+  eapply ap.
+  2: apply focus.
+  2: apply (name _ meta).
+  eapply fmap.
+  2: apply (pure tt).
+  intros H X.
+  apply meta.
+Defined.
+
+  
+  destruct s.
+  admit.
+  2: admit.
+  remember meta.
+  destruct meta.
+  cbn in *.
+  eapply ap.
+  2: apply focus.
+  2: apply name0.
+  eapply fmap.
+  intros X X0.
+  apply m.
+  apply (pure tt).
+
+  2: apply pure.
+  2: eapply mapT.
+  2: eapply fmap. mapT.
+  2: apply focus.
+  eapply fmap.
+  
+  2: intros; apply focus.
+  a
+  2: {
+
+  }
+  destruct meta.
+  destruct s; cbn in *.
+
+Definition globals'' {s : StorageType} : Traversal (Metadata s) (Metadata s) (Component s Field Ent) (Component s Field Ent).
+  red.
+  intros f F.
+  apply traversal.
+  intros focus meta.
+  destruct meta.
+  destruct s; cbn in *.
+  - 
+  eapply mapT in is_global0.
+  
+  destruct s.
+  cbn in *.
+  
+  Check @mapT list _ f F _ _ focus.
+  
+Check mapT.
+
+  eapply mapT.
+  refine open_constr:((fun x => _) <$> afa (_ w)); try typeclasses_eauto.
+Defined.
---
+++ src/coq/QC/Lens.v
@@ -0,0 +1,83 @@
+From Vellvm.Utils Require Import
+  Monads.
+
+Require Import ExtLib.Data.Monads.StateMonad.
+Require Import ExtLib.Structures.Monads.
+Require Import ExtLib.Structures.Functor.
+Require Import ExtLib.Structures.Applicative.
+Require Import ExtLib.Structures.Traversable.
+Import MonadNotation.
+Import ApplicativeNotation.
+Import FunctorNotation.
+
+Record Const (a b : Type) : Type := mkConst { getConst : a }.
+Arguments mkConst {_ _}.
+Arguments getConst {_ _}.
+
+Instance Const_Functor {a} : Functor (Const a).
+split.
+intros A B X X0.
+destruct X0.
+apply (mkConst getConst0).
+Defined.
+
+Definition ASetter s t a b := (a -> IdentityMonad.ident b) -> s -> IdentityMonad.ident t.
+Definition LensLike (f : Type -> Type) s t a b := (a -> f b) -> s -> f t.
+Definition Lens s t a b := forall {f} `{F: Functor f}, (a -> f b) -> s -> f t.
+Definition Lens' s a := forall {f} `{F: Functor f}, (a -> f a) -> s -> f s.
+Definition Traversal s t a b := forall {f} `{F: Applicative f}, (a -> f b) -> s -> f t.
+
+Definition lift_Lens' {a b} (l : Lens' a b) : Lens a a b b := l.
+
+Definition view {s t a b : Type} (l : Lens s t a b) (w : s) : a
+  := (getConst (l _ _ mkConst w)).
+
+Definition over {s t a b : Type} (l : ASetter s t a b) (f : a -> b) (w : s) : t
+  := IdentityMonad.unIdent (l (fun x => IdentityMonad.mkIdent (f x)) w).
+
+Definition setl {s t a b : Type} (l : ASetter s t a b) (v : b) (w : s) : t
+  := IdentityMonad.unIdent (l (fun _ => IdentityMonad.mkIdent v) w).
+
+Definition assign {m : Type -> Type} {s a b : Type} `{HM : Monad m} `{ST : @MonadState s m}
+  (l : ASetter s s a b) (v : b) : m s
+  := modify (setl l v).
+
+Definition modifying {m : Type -> Type} {s a b : Type} `{HM : Monad m} `{ST : @MonadState s m}
+  (l : ASetter s s a b) (f : a -> b) : m s
+  := modify (over l f).
+
+Definition use {m : Type -> Type} {s a : Type} `{HM : Monad m} `{ST : @MonadState s m}
+  (l : Lens' s a) : m a
+  := gets (view l).
+
+Definition comp' {a b c} (l1 : Lens' a b) (l2 : Lens' b c) : Lens' a c.
+  red in l1, l2.
+  red.
+  intros f F afa x.
+  specialize (l1 f F).
+  specialize (l2 f F).
+  apply l1.
+  - apply l2.
+    apply afa.
+  - apply x.
+Defined.
+
+Definition traversal {f s t a b} (tr : (a -> f b) -> s -> f t) : LensLike f s t a b
+  := tr.
+
+Module LensNotations.
+  Declare Scope lens_scope.
+  Delimit Scope lens_scope with lens.
+  Bind Scope lens_scope with Lens.
+
+  Notation "X -l> Y" := (Lens X X Y Y)
+    (at level 99, Y at level 200, right associativity) : type_scope.
+  Notation "a & b" := (b a) (at level 50, only parsing, left associativity) : lens_scope.
+  Notation "a %~ f" := (Lens.over a f) (at level 49, left associativity) : lens_scope.
+  Notation "a .~ b" := (Lens.setl a b) (at level 49, left associativity) : lens_scope.
+  Notation "a %= f" := (Lens.modifying a f) (at level 49, left associativity) : lens_scope.
+  Notation "a .= b" := (Lens.assign a b) (at level 49, left associativity) : lens_scope.
+  Notation "a .^ f" := (Lens.view f a) (at level 45, left associativity) : lens_scope.
+  (* level 19 to be compatible with Iris .@ *)
+  Notation "a .@ b" := (comp' a b) (at level 19, left associativity) : lens_scope.
+End LensNotations.
