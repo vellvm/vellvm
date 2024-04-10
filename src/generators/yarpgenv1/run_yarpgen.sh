ee370c6f678ca963e1bb7428a578a317fbe6c561
Hanxi Chen <garychen2000@outlook.com>
2024-04-09 22:50:26 -0400

    add threshold

--- src/generators/yarpgenv1/run_yarpgen.sh
+++ src/generators/yarpgenv1/run_yarpgen.sh
@@ -38,8 +38,14 @@ gen_yarpgen() {
 
 # Run the function in parallel for arguments 1 to 1000
 max=1000
+pids=()
 for i in $(seq 1 10000); do
     gen_yarpgen $i &
+    pids+=($!)
+    if (($i % 400 == 0)); then
+	wait ${pids[@]}
+	pids=()
+    fi
 done
 
 # Wait for all background processes to finish
