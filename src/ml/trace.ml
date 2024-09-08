245e11440eefb8d0542224a81c5d2c8c4a178b4d
Hanxi Chen <garychen2000@outlook.com>
2024-09-08 00:21:05 -0400

    Progress: functions seems to work for most cases. There is a bug at funptr.ll

--- src/ml/trace.ml
+++ src/ml/trace.ml
@@ -6,7 +6,16 @@ open OrderedType
 open DynamicTypes
 open CFG
 
+let print_dblk dblk : unit =
+  Printf.printf "%s" (ShowAST.dshowBlock ShowAST.dshowDtyp (dblk) |> DList.coq_DString_to_string |> Camlcoq.camlstring_of_coqstring)
+
+let print_log_entry (le : log_entry) =
+  Printf.printf "%s" (Log.dshow_log_entry le |> DList.coq_DString_to_string |> Camlcoq.camlstring_of_coqstring)
+
+let print_log () : unit =
+  Printf.printf "%s\n" (Log.dstring_of_log_stream !Log.log |> DList.coq_DString_to_string |> Camlcoq.camlstring_of_coqstring)
 (** Modules **)
+
 type raw_id = LLVMAst.raw_id
 
 module type OrdPrintT =
@@ -126,14 +135,31 @@ type code = (instr_id * dtyp instr) list
 
 let gensym : string -> string =
   let c = ref 0 in
-  fun (s:string) -> incr c; Printf.sprintf "_%s%d" s (!c)
+  fun (s:string) -> incr c; Printf.sprintf "_temp%s%d" s (!c)
+
+let gensym_int : int -> int =
+  let c = ref 0 in
+  fun (_:int) -> incr c; !c
 
 let gensym_raw_id : raw_id -> raw_id = function
   | Name id ->
     let id' = Camlcoq.camlstring_of_coqstring id |> gensym |> Camlcoq.coqstring_of_camlstring in
     Name id'
-  | Anon i -> Anon i
-  | Raw i -> Raw i
+  | Anon i ->
+    let i' = Camlcoq.Z.to_int i |> gensym_int |> Camlcoq.Z.of_uint in
+    Anon i'
+    (* let temp_name = Printf.sprintf "anon%d" (Camlcoq.Z.to_int i) in *)
+    (* let id' = *)
+    (*   temp_name |> gensym |> Camlcoq.coqstring_of_camlstring in *)
+    (* Name id' *)
+  | Raw i ->
+    let i' = Camlcoq.Z.to_int i |> gensym_int |> Camlcoq.Z.of_uint in
+    Raw i'
+    (* let temp_name = Printf.sprintf "anon%d" (Camlcoq.Z.to_int i) in *)
+    (* let id' = *)
+    (*   temp_name |> gensym |> Camlcoq.coqstring_of_camlstring in *)
+    (* Name id' *)
+    (* let id' = Campcoq.camlstring_of_coqstring "raw" |> gensym |> Camlcoq.coqstring_of_camlstring in *)
 
 (* Substitution r2 using r1 *)
 let subst_raw_id_opt (ctx : ctx) (s : raw_id) (d : dtyp exp) =
@@ -186,116 +212,63 @@ let add_term dblk (term : dtyp terminator) : dblk =
 (* TODO: How to deal with rightmost terminator when it is not well-formed *)
 
 (* TODO: Substitution needed *)
-let normalize_exp (ctx : ctx) (lid : raw_id) (op : dtyp exp) : ctx * code =
+let normalize_exp (ctx : ctx) (op : dtyp exp) : dtyp exp =
   match op with
   | OP_IBinop (ibinop, t, exp1, exp2) ->
     let exp1' = subst_exp ctx exp1 in
     let exp2' = subst_exp ctx exp2 in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_IBinop (ibinop, t, exp1', exp2')))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_IBinop (ibinop, t, exp1', exp2')
   | OP_ICmp (icmp, t, exp1, exp2) -> 
     let exp1' = subst_exp ctx exp1 in
     let exp2' = subst_exp ctx exp2 in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_ICmp (icmp, t, exp1', exp2')))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_ICmp (icmp, t, exp1', exp2')
   | OP_FBinop (fbinop, fm, t, exp1, exp2) ->
     let exp1' = subst_exp ctx exp1 in
     let exp2' = subst_exp ctx exp2 in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_FBinop (fbinop, fm, t, exp1', exp2')))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_FBinop (fbinop, fm, t, exp1', exp2')
   | OP_FCmp (fcmp, t, exp1, exp2) ->
     let exp1' = subst_exp ctx exp1 in
     let exp2' = subst_exp ctx exp2 in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_FCmp (fcmp, t, exp1', exp2')))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_FCmp (fcmp, t, exp1', exp2')
   | OP_Conversion (conv, t1, exp, t2) ->
     let exp' = subst_exp ctx exp in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_Conversion (conv, t1, exp', t2)))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_Conversion (conv, t1, exp', t2)
   | OP_GetElementPtr (t, texp, texps) ->
     let texp' = subst_texp ctx texp in
     let texps' = subst_texps ctx texps in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_GetElementPtr (t, texp', texps')))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_GetElementPtr (t, texp', texps')
   | OP_ExtractElement (texp1, texp2) ->
     let texp1' = subst_texp ctx texp1 in
     let texp2' = subst_texp ctx texp2 in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_ExtractElement (texp1', texp2')))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_ExtractElement (texp1', texp2')
   | OP_InsertElement (texp1, texp2, texp3) -> 
     let texp1' = subst_texp ctx texp1 in
     let texp2' = subst_texp ctx texp2 in
     let texp3' = subst_texp ctx texp3 in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_InsertElement (texp1', texp2', texp3')))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_InsertElement (texp1', texp2', texp3')
   | OP_ShuffleVector (texp1, texp2, texp3) -> 
     let texp1' = subst_texp ctx texp1 in
     let texp2' = subst_texp ctx texp2 in
     let texp3' = subst_texp ctx texp3 in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_InsertElement (texp1', texp2', texp3')))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_ShuffleVector (texp1', texp2', texp3')
   | OP_ExtractValue (texp, il) -> 
     let texp' = subst_texp ctx texp in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_ExtractValue (texp', il)))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_ExtractValue (texp', il)
   | OP_InsertValue (texp1, texp2, il) -> 
     let texp1' = subst_texp ctx texp1 in
     let texp2' = subst_texp ctx texp2 in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_InsertValue (texp1', texp2', il)))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_InsertValue (texp1', texp2', il)
   | OP_Select (texp1, texp2, texp3) -> 
     let texp1' = subst_texp ctx texp1 in
     let texp2' = subst_texp ctx texp2 in
     let texp3' = subst_texp ctx texp3 in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_Select (texp1', texp2', texp3')))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_Select (texp1', texp2', texp3')
   | OP_Freeze texp ->
     let texp' = subst_texp ctx texp in
-    let lid' = gensym_raw_id lid in
-    let code = [(IId lid', INSTR_Op (OP_Freeze texp'))] in
-    let exp = EXP_Ident (ID_Local lid') in
-    let ctx' = RawidM.update_or exp (fun _ -> exp) lid ctx in
-    ctx', code
+    OP_Freeze texp'
   | EXP_Ident ident ->
     let op' = subst_ident_opt ctx ident op in
-    let ctx' = RawidM.update_or op' (fun _ -> op') lid ctx in
-        ctx', []
+    op'
   | EXP_Integer _ | EXP_Float _ | EXP_Double _
   | EXP_Hex _ | EXP_Bool _ | EXP_Null | EXP_Zero_initializer
   | EXP_Cstring _ | EXP_Undef | EXP_Poison | EXP_Struct _
@@ -303,10 +276,13 @@ let normalize_exp (ctx : ctx) (lid : raw_id) (op : dtyp exp) : ctx * code =
     (* let lid' = gensym_raw_id lid in *)
     (* let code = [(IId lid', INSTR_Op op)] in *)
     (* let exp = EXP_Ident (ID_Local lid') in *)
-    let ctx' = RawidM.update_or op (fun _ -> op) lid ctx in
-    ctx', []
+    op
 
-let normalize_phi (ctx : ctx) (lid : raw_id) (phi : dtyp phi) (bid_from : raw_id) : ctx * code =
+let ctx_unit_to_string (r : raw_id) (d : dtyp exp) : string =
+  Printf.sprintf "%s->%s" (ShowAST.dshow_raw_id r |> DList.coq_DString_to_string |> Camlcoq.camlstring_of_coqstring)
+    (ShowAST.dshowExp ShowAST.dshowDtyp d |> DList.coq_DString_to_string |> Camlcoq.camlstring_of_coqstring)
+
+let normalize_phi (ctx : ctx) (id : raw_id) (phi : dtyp phi) (bid_from : raw_id) : ctx =
   match phi with
   | Phi (_, args) -> 
     match Util.assoc AstLib.eq_dec_raw_id bid_from args with
@@ -315,7 +291,9 @@ let normalize_phi (ctx : ctx) (lid : raw_id) (phi : dtyp phi) (bid_from : raw_id
        If op is some values -> then need to case analysis on that {tempid / phi}
        If exp is some operations, then assign this id with that operations, and {id / phiid}
  *)
-    normalize_exp ctx lid op
+      let exp = normalize_exp ctx op in
+      let ctx' = RawidM.update_or exp (fun _ -> exp) id ctx in
+      ctx'
     | None -> failwith (Printf.sprintf "No block match phi node")
 
 let list_to_map l1 l2 =
@@ -325,13 +303,14 @@ let normalize_definition ctx (mcfg : DynamicTypes.dtyp CFG.mcfg) (f : dtyp exp) 
   match f with
   | EXP_Ident (ID_Global id) ->
     begin match List.find_opt (fun x -> RawidOrdPrint.compare x.df_prototype.dc_name id == 0) mcfg.m_definitions with
-    | None -> 
-      None
-    | Some def ->
-      let args = List.map (fun (_, arg) -> subst_exp ctx arg) targs in
-      let ctx' = List.combine def.df_args args |> RawidM.of_list in
-      Some ctx'
-    (* Need to zip local_id with raw_id, If the length is the same will proceed the above, otherwise error *)
+      | None ->
+        None
+      | Some def ->
+        let args = List.map (fun (_, arg) -> subst_exp ctx arg) targs in
+        let ctx' = List.combine def.df_args args |> RawidM.of_list in
+        Printf.printf "ctx: %s\n" (RawidM.to_string ctx_unit_to_string ctx');
+        Some ctx'
+        (* Need to zip local_id with raw_id, If the length is the same will proceed the above, otherwise error *)
     end
   | _ -> None
 
@@ -341,26 +320,38 @@ let rec normalize_log
     (dblk : dblk)
     (stack : log_stream) : ctx * log_stream * dblk * dtyp texp option =
   match stack with
-  | nil ->
-    ctx, nil, dblk, None
+  | [] ->
+    ctx, [], dblk, None
   | log::stack' ->
+    print_log_entry log;
     begin match log with
       | Phi_node (lid, phi, bid) ->
-        let (ctx', code) = normalize_phi ctx lid phi bid in
-        let dblk' = add_code dblk code in
-        normalize_log ctx' mcfg dblk' stack'
-      | Ret term ->
-        let dblk' = add_term dblk (TERM_Ret term) in
-        ctx, stack', dblk', Some term
+        let ctx'= normalize_phi ctx lid phi bid in
+        normalize_log ctx' mcfg dblk stack'
+      | Ret texp ->
+        let texp' = subst_texp ctx texp in
+        let dblk' = add_term dblk (TERM_Ret texp') in
+        ctx, stack', dblk', Some texp' 
       | Instr (id, ins) ->
         begin match ins with
           | INSTR_Comment _ ->
             let dblk' = add_code dblk [(id, ins)] in
             normalize_log ctx mcfg dblk' stack'
           | INSTR_Op exp ->
-            
+            let exp' = normalize_exp ctx exp in
+            begin match id with
+            | IVoid _ ->
+              let dblk' = add_code dblk [(id, INSTR_Op exp')] in
+              normalize_log ctx mcfg dblk' stack'
+            | IId id ->
+              let id' = gensym_raw_id id in
+              let e = EXP_Ident (ID_Local id') in
+              let ctx' = RawidM.update_or e (fun _ -> e) id ctx in
+              let dblk' = add_code dblk [(IId id', INSTR_Op exp')] in
+              normalize_log ctx' mcfg dblk' stack'
+            end
           | INSTR_Call ((_, f), targs, _) ->
-
+        
             (* 1. Need to analyze the targs. Match them with the function signatures from mcfg
                2. Recursively call normalize_log
                3. continue with the rest of the stack
@@ -394,22 +385,143 @@ let rec normalize_log
                     begin match texp with
                       | Some (_, exp) -> 
                         let ctx2 = RawidM.update_or exp (fun _ -> exp) id ctx in
+                        Printf.printf "ctx: %s\n" (RawidM.to_string ctx_unit_to_string ctx);
+                        Printf.printf "ctx': %s\n" (RawidM.to_string ctx_unit_to_string ctx');
+                        Printf.printf "ctx2: %s\n" (RawidM.to_string ctx_unit_to_string ctx2);
                         normalize_log ctx2 mcfg dblk2 stack2
                       | None ->
                         failwith "Should return something"
                     end
+                  | None ->
+                    print_dblk dblk;
+                    failwith "function takes in no parameter?"
                 end
             end
-          | _ ->
-            failwith "unimplemented"
+          | INSTR_Alloca (t, tann) ->
+            begin match id with
+              | IVoid _ -> failwith "Alloca must have id"
+              | IId id ->
+                let id' = gensym_raw_id id in
+                let exp = EXP_Ident (ID_Local id') in
+                let ctx' = RawidM.update_or exp (fun _ -> exp) id ctx in
+                let dblk' = add_code dblk [(IId id', INSTR_Alloca (t, tann))] in
+                normalize_log ctx' mcfg dblk' stack'
+            end
+          | INSTR_Load (t, texp, tann) ->
+            begin match id with
+              | IVoid _ -> failwith "Load must have id"
+              | IId id ->
+                let texp' = subst_texp ctx texp in
+                let id' = gensym_raw_id id in
+                let exp = EXP_Ident (ID_Local id') in
+                let ctx' = RawidM.update_or exp (fun _ -> exp) id ctx in
+                let dblk' = add_code dblk [(IId id', INSTR_Load (t, texp', tann))] in
+                normalize_log ctx' mcfg dblk' stack'
+            end
+          | INSTR_Store (texp1, texp2, tann) ->
+            let texp1' = subst_texp ctx texp1 in
+            let texp2' = subst_texp ctx texp2 in
+            let dblk' = add_code dblk [(id, INSTR_Store (texp1', texp2', tann))] in
+            normalize_log ctx mcfg dblk' stack'
+          | INSTR_Fence (co, o) ->
+            let dblk' = add_code dblk [(id, INSTR_Fence (co, o))] in
+            normalize_log ctx mcfg dblk' stack'
+          | INSTR_AtomicCmpXchg cmpxchg ->
+            let cmpxchg' = {c_weak=cmpxchg.c_weak;
+                            c_volatile=cmpxchg.c_volatile;
+                            c_ptr=subst_texp ctx cmpxchg.c_ptr;
+                            c_cmp=cmpxchg.c_cmp;
+                            c_cmp_type=cmpxchg.c_cmp_type;
+                            c_new=subst_texp ctx cmpxchg.c_new;
+                            c_syncscope=cmpxchg.c_syncscope;
+                            c_success_ordering=cmpxchg.c_success_ordering;
+                            c_failure_ordering=cmpxchg.c_failure_ordering;
+                            c_align=cmpxchg.c_align
+                           } in
+            begin match id with
+              | IVoid _ -> failwith "cmpxchg must have id"
+              | IId id -> 
+                let id' = gensym_raw_id id in
+                let exp = EXP_Ident (ID_Local id') in
+                let ctx' = RawidM.update_or exp (fun _ -> exp) id ctx in
+                let dblk' = add_code dblk [(IId id', INSTR_AtomicCmpXchg (cmpxchg'))] in
+                normalize_log ctx' mcfg dblk' stack'
+            end
+          | INSTR_AtomicRMW atomicrmw ->
+            let atomicrmw' = {a_volatile=atomicrmw.a_volatile;
+                              a_operation=atomicrmw.a_operation;
+                              a_ptr=subst_texp ctx atomicrmw.a_ptr;
+                              a_val=subst_texp ctx atomicrmw.a_val;
+                              a_syncscope=atomicrmw.a_syncscope;
+                              a_ordering=atomicrmw.a_ordering;
+                              a_align=atomicrmw.a_align;
+                              a_type=atomicrmw.a_type
+                             } in
+            begin match id with
+              | IVoid _ -> failwith "atomicrmw must have id"
+              | IId id -> 
+                let id' = gensym_raw_id id in
+                let exp = EXP_Ident (ID_Local id') in
+                let ctx' = RawidM.update_or exp (fun _ -> exp) id ctx in
+                let dblk' = add_code dblk [(IId id', INSTR_AtomicRMW (atomicrmw'))] in
+                normalize_log ctx' mcfg dblk' stack'
+            end
+          | INSTR_VAArg (texp, t) ->
+            let texp' = subst_texp ctx texp in
+            begin match id with
+              | IVoid _ -> failwith "va_arg must have id"
+              | IId id ->
+                let id' = gensym_raw_id id in
+                let exp = EXP_Ident (ID_Local id') in
+                let ctx' = RawidM.update_or exp (fun _ -> exp) id ctx in
+                let dblk' = add_code dblk [(IId id', INSTR_VAArg (texp', t))] in
+                normalize_log ctx' mcfg dblk' stack'
+            end
+          | INSTR_LandingPad ->
+            begin match id with
+              | IVoid _ -> failwith "va_arg must have id"
+              | IId id ->
+                let id' = gensym_raw_id id in
+                let exp = EXP_Ident (ID_Local id') in
+                let ctx' = RawidM.update_or exp (fun _ -> exp) id ctx in
+                let dblk' = add_code dblk [(IId id', INSTR_LandingPad)] in
+                normalize_log ctx' mcfg dblk' stack'
+            end
         end
     end
+
+let normalize_code 
+    (mcfg : DynamicTypes.dtyp CFG.mcfg)
+    (stack : log_stream) : dblk =
+  let ctx = RawidM.empty in
+  (* Printf.printf "%s" (Log.dstring_of_log_stream stack |> DList.coq_DString_to_string |> Camlcoq.camlstring_of_coqstring); *)
+  let dblk : dtyp block = {blk_id=Name (['0']);
+              blk_phis=[];
+              blk_code=[];
+              blk_term=(TERM_Ret (DTYPE_Void, EXP_Undef));
+              blk_comments= None
+             } in
+  let (_, _ , dblk', _) = normalize_log ctx mcfg dblk stack in
+  {blk_id=dblk'.blk_id;
+   blk_phis=dblk'.blk_phis;
+   blk_code=List.rev dblk'.blk_code;
+   blk_term=dblk'.blk_term;
+   blk_comments=dblk'.blk_comments
+  }
+
+let get_mcfg ll_ast =
+  (TypToDtyp.convert_types (mcfg_of_tle (TopLevel.TopLevelBigIntptr.link TopLevel.TopLevelBigIntptr.coq_PREDEFINED_FUNCTIONS ll_ast)))
+
+
 
 (** Printing trace **)
 let output_channel = ref stdout
 
-let print_log () : unit =
-  Printf.printf "%s" (Log.dstring_of_log_stream !Log.log |> DList.coq_DString_to_string |> Camlcoq.camlstring_of_coqstring)
+
+let print_normalized_log ll_ast =
+  let mcfg = get_mcfg ll_ast in
+  let dblk = normalize_code mcfg (List.rev !Log.log) in
+  print_dblk dblk
 
 
 
