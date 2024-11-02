d3a861c8d17097c21f2dce4aab8036b001828470
Hanxi Chen <garychen2000@outlook.com>
2024-11-02 00:11:36 -0400

    Progress: Need to figure out how to do function argument substitution

--- src/ml/extracted/log.ml
+++ src/ml/extracted/log.ml
@@ -4,44 +4,44 @@ open ShowAST
 open DList
 open CFG
 
-type log_entry =
-  | Instr of instr_id * dtyp instr
-  | Phi_node of local_id * dtyp phi * block_id
-  | Term of dtyp terminator
+type 'a log_entry =
+  | Instr of instr_id * 'a instr
+  | Phi_node of local_id * 'a phi * block_id
+  | Term of 'a terminator
   | F_args of (function_id) * local_id list
 
-type log_stream = log_entry list
+type 'a log_stream = 'a log_entry list
 
-let log : log_stream ref = ref []
+let log_ref : dtyp log_stream ref = ref []
 
 let ( >@ ) x y = y @ x
 let ( >:: ) x y = y :: x
 
 let output_channel = ref stdout
 
-let dshow_log_entry (le : log_entry) : DList.coq_DString =
+let dshow_log_entry (le : 'a log_entry) ~(f : 'a -> DList.coq_DString) : DList.coq_DString =
   match le with
   | Instr (uid, ins) ->
-    ShowAST.dshow_instr_id ShowAST.dshow_dtyp (uid, ins)
+    ShowAST.dshow_instr_id f (uid, ins)
   | Phi_node (uid, phi, bid) ->
     DList.coq_DList_join
       [
       ShowAST.dshow_raw_id bid;
-      ShowAST.dshow_phi_id ShowAST.dshow_dtyp (uid, phi)
+      ShowAST.dshow_phi_id f (uid, phi)
     ]
   | Term term ->
-    ShowAST.dshowTerminator ShowAST.dshow_dtyp term 
+    ShowAST.dshowTerminator f term 
   | F_args (def, _) ->
     (* List.fold_right (fun x acc -> coq_DList_append (ShowAST.dshowRawId x) acc) args (DList.coq_EmptyDString) *)
     ShowAST.dshowRawId def
 
-let dstring_of_log_stream (log_stream : log_stream) : DList.coq_DString =
-  List.rev log_stream |> List.map dshow_log_entry |> ShowAST.dintersperse (string_to_DString ('\n' :: []))
+let dstring_of_log_stream (log_stream : 'a log_stream) ~(f : 'a -> DList.coq_DString) : DList.coq_DString =
+  List.rev log_stream |> List.map (dshow_log_entry ~f) |> ShowAST.dintersperse (string_to_DString ('\n' :: []))
 
-let clear_log : unit = log := []
+let clear_log : unit = log_ref := []
 
-let write_log_entry (le : log_entry) : unit =
-  log := !log >:: le
+let write_log_entry (le : dtyp log_entry) : unit =
+  log_ref := !log_ref >:: le
 
-let get_log_stream () : log_stream =
-  List.rev !log
+let get_log_stream () : dtyp log_stream =
+  List.rev !log_ref
