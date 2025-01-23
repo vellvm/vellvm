2671848dde0b78749a49f0656c28bda498afdd04
Hanxi Chen <garychen2000@outlook.com>
2025-01-22 21:14:09 -0700

    ptrtoint test case assembly without puts

--- qc-test-failures/ptrtoint-roundtrip/ptrtoint7.s
+++ qc-test-failures/ptrtoint-roundtrip/ptrtoint7.s
@@ -8,24 +8,15 @@
 main:                                   # @main
 	.cfi_startproc
 # %bb.0:
-	addi	sp, sp, -32
-	.cfi_def_cfa_offset 32
-	sd	ra, 24(sp)                      # 8-byte Folded Spill
-	.cfi_offset ra, -8
+	addi	sp, sp, -16
+	.cfi_def_cfa_offset 16
 	li	a0, 0
-	sb	a0, 23(sp)
-	sb	a0, 22(sp)
-	sb	a0, 21(sp)
-	sb	a0, 20(sp)
-	lbu	a0, 23(sp)
-	sd	a0, 8(sp)                       # 8-byte Folded Spill
-	sb	a0, 19(sp)
-	addi	a0, sp, 19
-	call	puts
-                                        # kill: def $x11 killed $x10
-	ld	a0, 8(sp)                       # 8-byte Folded Reload
-	ld	ra, 24(sp)                      # 8-byte Folded Reload
-	addi	sp, sp, 32
+	sb	a0, 15(sp)
+	sb	a0, 14(sp)
+	sb	a0, 13(sp)
+	sb	a0, 12(sp)
+	lbu	a0, 15(sp)
+	addi	sp, sp, 16
 	ret
 .Lfunc_end0:
 	.size	main, .Lfunc_end0-main
@@ -33,4 +24,3 @@ main:                                   # @main
                                         # -- End function
 	.section	".note.GNU-stack","",@progbits
 	.addrsig
-	.addrsig_sym puts
