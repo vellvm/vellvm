983550e0312eba809099ab238b14125c16ef811f
Calvin Beck <hobbes@ualberta.ca>
2025-10-16 11:02:38 -0400

    Disable org-linting test for now.

--- flake.nix
+++ flake.nix
@@ -46,37 +46,37 @@
               '';
             };
             
-          org-lint =
-            pkgs.stdenv.mkDerivation {
-              name = "org-linting";
-              src = ./.;
-              meta = {
-                description = "Ensure that links are still valid within some important org files for artifact submissions :).";
-              };
-              buildInputs = [pkgs.emacs];
-              installPhase = ''
- ${pkgs.emacs}/bin/emacs --batch -f package-initialize --eval "(add-hook 'org-mode-hook  
-      (lambda ()
-          (let* ((file-name (current-buffer))
-            (Col1 'Line)
-            (Col2 'Trust)
-            (Col3 'Warning)
-            (lint-report (org-lint '(link-to-local-file)))
-          )
-          (princ (format \"file: %s\n%6s%6s%8s\n\" file-name Col1 Col2 Col3))
-          (dolist (element lint-report)
-           (setq report (car (cdr element)))
-           (princ (format \"%6s%6s %7s\n\" (seq-elt report 0) (seq-elt report 1) (seq-elt report 2)))
-          )
-          (if (not (null lint-report))
-            (kill-emacs 1))
-          )))" MemoryTour.org
+ #          org-lint =
+ #            pkgs.stdenv.mkDerivation {
+ #              name = "org-linting";
+ #              src = ./.;
+ #              meta = {
+ #                description = "Ensure that links are still valid within some important org files for artifact submissions :).";
+ #              };
+ #              buildInputs = [pkgs.emacs];
+ #              installPhase = ''
+ # ${pkgs.emacs}/bin/emacs --batch -f package-initialize --eval "(add-hook 'org-mode-hook  
+ #      (lambda ()
+ #          (let* ((file-name (current-buffer))
+ #            (Col1 'Line)
+ #            (Col2 'Trust)
+ #            (Col3 'Warning)
+ #            (lint-report (org-lint '(link-to-local-file)))
+ #          )
+ #          (princ (format \"file: %s\n%6s%6s%8s\n\" file-name Col1 Col2 Col3))
+ #          (dolist (element lint-report)
+ #           (setq report (car (cdr element)))
+ #           (princ (format \"%6s%6s %7s\n\" (seq-elt report 0) (seq-elt report 1) (seq-elt report 2)))
+ #          )
+ #          (if (not (null lint-report))
+ #            (kill-emacs 1))
+ #          )))" MemoryTour.org
 
-          if [[ $? == 0 ]]; then
-             mkdir $out
-          fi
-              '';
-            };
+ #          if [[ $? == 0 ]]; then
+ #             mkdir $out
+ #          fi
+ #              '';
+ #            };
         };
 
         devShells = {
