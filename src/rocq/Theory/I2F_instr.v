12ff94b603ecbee73dfb0def107c7750f5a95750
Yannick <y.zakowski@gmail.com>
2026-07-12 18:58:21 +0200

    One tiny step away from I2F_denote_mcfg, need to massage a bit my ruttc_mcfg lemma

--- src/rocq/Theory/I2F_instr.v
+++ src/rocq/Theory/I2F_instr.v
@@ -394,4 +394,320 @@ Proof with try now (rstep; cbnn; try (easy); eauto).
   intros ?? HR; inv HR...
 Qed.
 
+Definition I2F_function_denotation
+  (ctx1 : @function_denotation PInf)
+  (ctx2 : @function_denotation PFin) : Prop :=
+  forall args1 args2,
+    Forall2 I2F_dvalue args1 args2 ->
+    I2F_refine_CFG (sum_rel I2F_dvalue I2F_dvalue) (ctx1 args1) (ctx2 args2).
 
+Lemma I2F_combine_lists_varargs : 
+  forall (d : definition dtyp (cfg dtyp)) y1 y2,
+    Forall2 I2F_dvalue y1 y2 -> 
+    I2F_EOU 
+      (prod_rel (Forall2 (prod_rel Logic.eq I2F_dvalue)) (Forall2 I2F_dvalue))
+      (combine_lists_varargs (df_args d) y1)
+      (combine_lists_varargs (df_args d) y2).
+Proof with cbn; eauto using I2F_EOU_error, I2F_EOU_ret, I2F_EOU_oom, I2F_EOU_ub.
+  intros d.
+  generalize (df_args d) as l.
+  induction l as [| x l IH]; intros * HR.
+  - dependent induction HR...
+    apply I2F_EOU_ret; constructor...
+  - dependent induction HR...
+    apply IH in HR.
+    induction HR...
+    destruct a1,a2; constructor.
+    inv H0.
+    constructor; cbn...
+Qed.
+
+Lemma Forall2_eq {A} (l1 l2 : list A) :
+  Forall2 Logic.eq l1 l2 -> l1 = l2.
+Proof.
+  intros * HR; induction HR; subst; cbn; auto.
+Qed.
+
+(** [dtyp_of_dvalue] computes in the parameter-free [EOU dtyp]: on
+    related values the two sides are literally equal (related scalars
+    carry equal payloads, and aggregates share their annotations). *)
+Lemma I2F_dtyp_of_dvalue_eq :
+  forall v v',
+    I2F_dvalue v v' ->
+    @dtyp_of_dvalue PInf v = @dtyp_of_dvalue PFin v'.
+Proof.
+  induction v using dvalue_ind; intros v' R; inversion R; subst;
+    repeat match goal with
+      | H : existT _ _ _ = existT _ _ _ |- _ =>
+          apply Eqdep_dec.inj_pair2_eq_dec in H; [| apply Pos.eq_dec]; subst
+      end;
+    clear R; cbn; auto.
+  - (* Struct *)
+    match goal with
+    | F2 : Forall2 I2F_dvalue fields ?l2 |- context [?L fields] =>
+        match goal with
+        | |- context [?R l2] =>
+            assert (GO : L fields = R l2); [| rewrite GO; reflexivity]
+        end
+    end.
+    match goal with
+    | F2 : Forall2 I2F_dvalue fields _, IH : Forall _ fields |- _ =>
+        revert IH; induction F2 as [| u u' us us' HU HUS IHUS]
+    end; intros FIH; cbn; auto.
+    inversion FIH; subst.
+    match goal with
+    | HP : forall _, I2F_dvalue u _ -> _ |- _ => rewrite (HP _ HU)
+    end.
+    rewrite IHUS by assumption; reflexivity.
+  - (* Packed struct *)
+    match goal with
+    | F2 : Forall2 I2F_dvalue fields ?l2 |- context [?L fields] =>
+        match goal with
+        | |- context [?R l2] =>
+            assert (GO : L fields = R l2); [| rewrite GO; reflexivity]
+        end
+    end.
+    match goal with
+    | F2 : Forall2 I2F_dvalue fields _, IH : Forall _ fields |- _ =>
+        revert IH; induction F2 as [| u u' us us' HU HUS IHUS]
+    end; intros FIH; cbn; auto.
+    inversion FIH; subst.
+    match goal with
+    | HP : forall _, I2F_dvalue u _ -> _ |- _ => rewrite (HP _ HU)
+    end.
+    rewrite IHUS by assumption; reflexivity.
+  - (* Array *)
+    break_match_goal; cbn; auto.
+    match goal with
+    | F2 : Forall2 I2F_dvalue elts ?l2 |- context [forallb ?f elts] =>
+        match goal with
+        | |- context [forallb ?g l2] =>
+            assert (FB : forallb f elts = forallb g l2);
+            [| assert (LEN : length elts = length l2)
+                 by (eapply Forall2_length; eauto);
+               rewrite FB, LEN; reflexivity ]
+        end
+    end.
+    match goal with
+    | F2 : Forall2 I2F_dvalue elts _, IH : Forall _ elts |- _ =>
+        revert IH; induction F2 as [| u u' us us' HU HUS IHUS]
+    end; intros FIH; cbn; auto.
+    inversion FIH; subst.
+    match goal with
+    | HP : forall _, I2F_dvalue u _ -> _ |- _ => rewrite (HP _ HU)
+    end.
+    rewrite IHUS by assumption; reflexivity.
+  - (* Vector *)
+    break_match_goal; cbn; auto.
+    match goal with
+    | F2 : Forall2 I2F_dvalue elts ?l2 |- context [forallb ?f elts] =>
+        match goal with
+        | |- context [forallb ?g l2] =>
+            assert (FB : forallb f elts = forallb g l2);
+            [| assert (LEN : length elts = length l2)
+                 by (eapply Forall2_length; eauto);
+               rewrite FB, LEN; reflexivity ]
+        end
+    end.
+    match goal with
+    | F2 : Forall2 I2F_dvalue elts _, IH : Forall _ elts |- _ =>
+        revert IH; induction F2 as [| u u' us us' HU HUS IHUS]
+    end; intros FIH; cbn; auto.
+    inversion FIH; subst.
+    match goal with
+    | HP : forall _, I2F_dvalue u _ -> _ |- _ => rewrite (HP _ HU)
+    end.
+    rewrite IHUS by assumption; reflexivity.
+Qed.
+
+Lemma I2F_dtyp_of_dvalue :
+  forall a1 a2 : dvalue,
+    I2F_dvalue a1 a2 ->
+    I2F_EOU Logic.eq (dtyp_of_dvalue a1) (dtyp_of_dvalue a2).
+Proof.
+  intros * H; erewrite I2F_dtyp_of_dvalue_eq; eauto.
+  apply I2F_EOU_refl.
+Qed.
+  
+Lemma I2F_push_call_frame d args1 args2:
+    Forall2 I2F_dvalue args1 args2 ->
+    I2F_refine_CFG I2F_Addr (push_call_frame d args1) (push_call_frame d args2).
+Proof with try now (rstep; cbnn; try (easy); eauto).
+  intros.
+  erbind; [apply I2F_refine_lift', I2F_combine_lists_varargs; auto | intros].
+  destruct r1, r2; inv H0; cbn in * |-.
+  erbind; [eapply I2F_refine_lift', I2F_EOU_map_monad2 with (RB := Logic.eq); eauto | intros ?? HEQ; apply Forall2_eq in HEQ; subst].
+  apply I2F_dtyp_of_dvalue.
+  rbind TT...
+  intros _ _ _.
+  rbind TT...
+  intros _ _ _.
+  rbind I2F_dvalue...
+  intros ?? HR.
+  rbind TT.
+  rstep; cbnn; try easy.
+  constructor; intuition.
+  intros _ _ _.
+  induction HR...
+Qed.
+
+Lemma unfold_run_exc_strong `{Params.Params} {A} (t : itree _ A) :
+  run_exc t ≅ match observe t with
+         | RetF a  => Ret (inr a)
+         | TauF u' => Tau (run_exc u')
+         | VisF X e k =>
+             match exc_of_event e with
+             | Some x => Ret (inl x)
+             | None   => Vis e (fun x => Tau (run_exc (k x)))
+             end
+    end.
+Proof.
+  unfold run_exc.
+  rewrite unfold_iter.
+  desobs t ot; cbn; auto.
+  rewrite Eqit.bind_ret_l; reflexivity.
+  rewrite Eqit.bind_ret_l; reflexivity.
+  break_goal_fast; rewrite ?Eqit.bind_ret_l; [reflexivity |].
+  rewrite bind_vis. apply eqit_Vis.
+  intros.
+  rewrite Eqit.bind_ret_l; reflexivity.
+Qed.
+
+Lemma unfold_run_exc `{Params.Params} {A} (t : itree _ A) :
+  run_exc t ≳ match observe t with
+         | RetF a  => Ret (inr a)
+         | TauF u' => run_exc u'
+         | VisF X e k =>
+             match exc_of_event e with
+             | Some x => Ret (inl x)
+             | None   => Vis e (fun x => run_exc (k x))
+             end
+    end.
+Proof.
+  unfold run_exc.
+  rewrite unfold_iter.
+  desobs t ot; cbn; auto.
+  rewrite Eqit.bind_ret_l; reflexivity.
+  rewrite Eqit.bind_ret_l, tau_euttge; reflexivity.
+  break_goal_fast; rewrite ?Eqit.bind_ret_l; [reflexivity |].
+  rewrite bind_vis. apply eqit_Vis.
+  intros.
+  rewrite Eqit.bind_ret_l, tau_euttge; reflexivity.
+Qed.
+
+(* #[global] Instance run_exc_eq_itree Pa A : Proper (eq_itree Logic.eq ==> eq_itree Logic.eq) (@run_exc Pa A). *)
+(* Proof. *)
+(*   repeat intro. *)
+(*   unfold run_exc. *)
+(*   setoid_rewrite H. *)
+(* Admitted. *)
+
+Lemma cutUB_exc_of_event `{Params.Params} {X} (e : _ X) :
+  cutUB e ->
+  exc_of_event e = None.
+Proof.
+  intros CUT.
+  induction CUT; reflexivity.
+Qed.
+
+Lemma cutOOM_exc_of_event `{Params.Params} {X} (e : _ X) :
+  cutOOM e ->
+  exc_of_event e = None.
+Proof.
+  intros CUT.
+  induction CUT; reflexivity.
+Qed.
+
+(** Related events have related exception projections: the [LLVMExcE]
+    payloads are related by [I2FE_Exc], and every other component
+    projects to [None] on both sides (mismatched injections are ruled
+    out by the computing [sum_prerel]). *)
+Lemma I2FE_CFG_exc_of_event {A B} (e1 : @CFGEtop PInf A) (e2 : @CFGEtop PFin B) :
+  I2FE_CFG e1 e2 ->
+  option_rel I2F_dvalue (exc_of_event e1) (exc_of_event e2).
+Proof.
+  intros HE.
+  repeat (destruct e1 as [e1|e1]); repeat (destruct e2 as [e2|e2]);
+    cbn in *; try easy; try (now constructor).
+  destruct e1, e2; simp I2FE_Exc in HE; constructor; auto.
+Qed.
+
+Lemma I2F_run_exc {R1 R2 : Type} {RR : R1 -> R2 -> Prop} t1 t2 :
+  ruttc cutUB cutOOM I2FE_CFG I2FA_CFG RR t1 t2 ->
+  ruttc cutUB cutOOM I2FE_CFG I2FA_CFG (sum_rel I2F_dvalue RR) (run_exc t1) (run_exc t2).
+Proof.
+  revert t1 t2.
+  ginit; gcofix cih.
+  intros * HR.
+  rewrite 2 unfold_run_exc_strong.
+  punfold HR; red in HR.
+  induction HR; pclearbot.
+  - gstep; constructor; eauto.
+  - gstep. constructor; eauto.
+    gfinal; left; apply cih; auto.
+  - pose proof (I2FE_CFG_exc_of_event _ _ H) as EXC.
+    destruct (exc_of_event e1), (exc_of_event e2); inv EXC.
+    + gstep; constructor; constructor; auto.
+    + gstep; constructor; auto.
+      intros a b HAns.
+      gstep; constructor.
+      gfinal; left; apply cih.
+      now apply H0.
+  - rewrite tau_euttge, unfold_run_exc_strong.
+    apply IHHR.
+  - rewrite tau_euttge, unfold_run_exc_strong.
+    apply IHHR.
+  - rewrite cutUB_exc_of_event; auto.
+    gstep; constructor; auto.
+  - rewrite cutOOM_exc_of_event; auto.
+    gstep; constructor; auto.
+Qed.
+
+Lemma I2F_denote_function d :
+    I2F_function_denotation (@denote_function PInf d) (@denote_function PFin d).
+Proof with try now (rstep; cbnn; try (easy); eauto).
+  unfold denote_function.
+  intros ???.
+  rbind I2F_Addr; [apply I2F_push_call_frame; auto | intros].
+  erbind...
+  eapply I2F_run_exc, I2F_denote_cfg; constructor; auto.
+  intros.
+  unfold pop_call_frame; cbn; rewrite 2 Eqit.bind_bind.
+  rbind TT...
+  intros ???.
+  rbind TT...
+  intros ???...
+Qed.
+
+(** Pointwise lifting of [I2F_function_denotation] to function contexts,
+    extensionally over [lookup] (in the style of [IntMaps.Equiv]): the two
+    maps have the same domain, and denotations found at a common key are
+    related. [lookup_defn] keys the map by [ptr_to_int], which coincides
+    on [I2F_Addr]-related addresses, so related lookups share their key. *)
+Definition I2F_ctx
+  (ctx1 : IntMap (@function_denotation PInf))
+  (ctx2 : IntMap (@function_denotation PFin)) : Prop :=
+  forall k, option_rel I2F_function_denotation (lookup k ctx1) (lookup k ctx2).
+    
+Lemma I2F_denote_mcfg :
+  forall t ctx1 ctx2 f1 f2 args1 args2,
+    I2F_dvalue f1 f2 ->
+    Forall2 I2F_dvalue args1 args2 ->
+    (* I2F_function_denotation ctx1 ctx2 -> *)
+    I2F_ctx ctx1 ctx2 ->
+    I2F_refine_MCFG
+      (sum_rel I2F_dvalue I2F_dvalue)
+      (@denote_mcfg PInf ctx1 t f1 args1)
+      (@denote_mcfg PFin ctx2 t f2 args2).
+Proof.
+  intros * HF Hargs Hctx.
+  red.
+  (* unfold denote_mcfg. *)
+  (* (* Set Printing Implicit. *) *)
+  (* epose proof *)
+  (*   @ruttc_mrec (@CallE PInf) (@CallE PFin) (@MCFGEtop PInf) (@MCFGEtop PFin) *)
+  (*   (@dvalue PInf + @dvalue PInf) (@dvalue PFin + @dvalue PFin) *)
+  (*   cutUB cutOOM I2FE_MCFG I2FA_MCFG _ _ _ _ _ _ _ _ (Call t f1 args1) (Call t f2 args2) _. *)
+  (* apply H. *)
+  
+Admitted.
